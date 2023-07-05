617cb10ab48cbddb7136b9a4bfd17e36f5e7f72f
jagaldol <yoy06056@pusan.ac.kr>
2023-07-05 12:02:47 +0900

    feat: POST orders/save mock API 구현

--- src/main/java/com/example/kakaoshop/order/OrderRestController.java
+++ src/main/java/com/example/kakaoshop/order/OrderRestController.java
@@ -7,6 +7,7 @@ import com.example.kakaoshop.order.response.ProductDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -41,6 +42,42 @@ public class OrderRestController {
 
         OrderResFindByIdDTO responseDTO = OrderResFindByIdDTO.builder()
                 .id(id)
+                .products(productDTOs)
+                .totalPrice(209000)
+                .build();
+
+        return ResponseEntity.ok(ApiUtils.success(responseDTO));
+    }
+
+    @PostMapping("/orders/save")
+    ResponseEntity<?> save() {
+        // TODO: cart_tb의 내용을 order_tb에 저장
+
+        // 성공 했다고 가정
+
+        List<ProductDTO> productDTOs = new ArrayList<>();
+
+        List<OrderItemDTO> orderItemDTOs = new ArrayList<>();
+        orderItemDTOs.add(OrderItemDTO.builder()
+                .id(4L)
+                .optionName("01. 슬라이딩 지퍼백 크리스마스에디션")
+                .quantity(10)
+                .price(100000)
+                .build());
+        orderItemDTOs.add(OrderItemDTO.builder()
+                .id(5L)
+                .optionName("02. 슬라이딩 지퍼백 플라워에디션 5종")
+                .quantity(10)
+                .price(109000)
+                .build());
+
+        productDTOs.add(ProductDTO.builder()
+                .productName("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션")
+                .items(orderItemDTOs)
+                .build());
+
+        OrderResFindByIdDTO responseDTO = OrderResFindByIdDTO.builder()
+                .id(2L)
                 .products(productDTOs)
                 .totalPrice(209000)
                 .build();
