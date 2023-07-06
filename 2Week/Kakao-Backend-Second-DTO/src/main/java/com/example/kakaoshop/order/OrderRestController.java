eecb63a34bad228f8180ab98cd2e2d91db053728
ki-met-hoon <shggm2000@gmail.com>
2023-07-06 22:43:38 +0900

    feat: 주문 결과 확인 Mock API 구현
    id 값에 따라 주문 내역을 보여주는 주문 결과 확인 API 구현

--- 2Week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/order/OrderRestController.java
+++ 2Week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/order/OrderRestController.java
@@ -50,4 +50,45 @@ public class OrderRestController {
 
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
+
+    @GetMapping("/orders/{id}")
+    public ResponseEntity<?> findById(@PathVariable int id){
+
+        if(id == 1){
+            List<OrderItemDTO> orderItemDTOList = new ArrayList<>();
+
+            OrderItemDTO orderItemDTO1 = OrderItemDTO.builder()
+                    .id(4)
+                    .optionName("01. 슬라이딩 지퍼백 크리스마스에디션 4종")
+                    .quantity(10)
+                    .price(100000)
+                    .build();
+
+            orderItemDTOList.add(orderItemDTO1);
+
+            OrderItemDTO orderItemDTO2 = OrderItemDTO.builder()
+                    .id(5)
+                    .optionName("02. 슬라이딩 지퍼백 플라워에디션 5종")
+                    .quantity(10)
+                    .price(109000)
+                    .build();
+
+            orderItemDTOList.add(orderItemDTO2);
+
+            List<OrderProductDTO> orderProductDTOList = new ArrayList<>();
+
+            orderProductDTOList.add(
+                    OrderProductDTO.builder()
+                            .productName("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전")
+                            .orderItems(orderItemDTOList)
+                            .build()
+            );
+
+            OrderDTO responseDTO = new OrderDTO(1, orderProductDTOList, 209000);
+
+            return ResponseEntity.ok(ApiUtils.success(responseDTO));
+        }
+
+        return ResponseEntity.ok(ApiUtils.success("오류입니다"));
+    }
 }
