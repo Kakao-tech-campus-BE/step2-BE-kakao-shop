5b9bcc7eca012127a98f5177bf8613d958882717
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-07-06 16:43:40 +0900

    fix : code style to remove @Setter annotation

--- week2/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/cart/CartRestController.java
+++ week2/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/cart/CartRestController.java
@@ -16,34 +16,39 @@ import java.util.List;
 @RestController
 public class CartRestController {
 
+
     @GetMapping("/carts")
     public ResponseEntity<?> findAll() {
         // 카트 아이템 리스트 만들기
         List<CartItemDTO> cartItemDTOList = new ArrayList<>();
+
+        ProductOptionDTO option1 = ProductOptionDTO.builder()
+                .id(1)
+                .optionName("01. 슬라이딩 지퍼백 크리스마스에디션 4종")
+                .price(10000)
+                .build();
 
         // 카트 아이템 리스트에 담기
         CartItemDTO cartItemDTO1 = CartItemDTO.builder()
                 .id(4)
                 .quantity(5)
                 .price(50000)
+                .option(option1)
                 .build();
-        cartItemDTO1.setOption(ProductOptionDTO.builder()
-                                .id(1)
-                                .optionName("01. 슬라이딩 지퍼백 크리스마스에디션 4종")
-                                .price(10000)
-                                .build());
         cartItemDTOList.add(cartItemDTO1);
 
+
+        ProductOptionDTO option2 = ProductOptionDTO.builder()
+                .id(2)
+                .optionName("02. 슬라이딩 지퍼백 플라워에디션 5종")
+                .price(10900)
+                .build();
         CartItemDTO cartItemDTO2 = CartItemDTO.builder()
                 .id(5)
                 .quantity(5)
                 .price(54500)
+                .option(option2)
                 .build();
-        cartItemDTO2.setOption(ProductOptionDTO.builder()
-                                .id(1)
-                                .optionName("02. 슬라이딩 지퍼백 크리스마스에디션 5종")
-                                .price(10900)
-                                .build());
         cartItemDTOList.add(cartItemDTO2);
 
         // productDTO 리스트 만들기
@@ -54,11 +59,14 @@ public class CartRestController {
                 ProductDTO.builder()
                         .id(1)
                         .productName("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전")
-                        .cartItems(cartItemDTOList)
+                        .carts(cartItemDTOList)
                         .build()
         );
 
-        CartRespFindAllDTO responseDTO = new CartRespFindAllDTO(productDTOList, 104500);
+        CartRespFindAllDTO responseDTO = CartRespFindAllDTO.builder()
+                .products(productDTOList)
+                .totalPrice(104500)
+                .build();
 
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
