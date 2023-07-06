70bd7f7580a7964c6647743e17e915df390e894f
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-07-06 14:46:26 +0900

    feat : add prefix at UserController

--- week2/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/user/UserRestController.java
+++ week2/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/user/UserRestController.java
@@ -11,12 +11,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/auth")
 public class UserRestController {
     private final UserJPARepository userRepository;
     private final PasswordEncoder passwordEncoder;
