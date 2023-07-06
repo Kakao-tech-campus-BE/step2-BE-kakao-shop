baf6c0fa56d7af4c79891aa786ca9dfdc2920317
hanju <ahj1903@naver.com>
2023-07-06 15:28:23 +0900

    Res, Req DTO API명세서 기준으로 DTO작성완료

---
+++ week2/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/cart/response/CartUpdateResDTO.java
@@ -0,0 +1,10 @@
+package com.example.kakaoshop.cart.response;
+
+import com.example.kakaoshop.product.Product;
+
+import java.util.List;
+
+public class CartUpdateResDTO {
+    private int totalPrice;
+    private List<ProductDTO> products;
+}
