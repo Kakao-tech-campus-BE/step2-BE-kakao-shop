b700c57bbbe4fa02beb20db504327f0c916eecef
BoSeung <gda05189@gmail.com>
2023-07-06 16:33:16 +0900

    refactor - ProductDTO

--- 2week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/order/response/ProductDTO.java
+++ 2week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/order/response/ProductDTO.java
@@ -1,9 +1,11 @@
 package com.example.kakaoshop.order.response;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
-
+@Getter @Setter
 public class ProductDTO {
     private int id;
     private String productName;
