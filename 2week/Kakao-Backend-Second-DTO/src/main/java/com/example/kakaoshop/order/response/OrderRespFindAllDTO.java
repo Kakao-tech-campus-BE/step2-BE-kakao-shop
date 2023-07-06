b70935b22af3b643e174c6107f9a29a171cb389b
BoSeung <gda05189@gmail.com>
2023-07-06 16:27:41 +0900

    feat - OrderRespFindAllDTO

---
+++ 2week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/order/response/OrderRespFindAllDTO.java
@@ -0,0 +1,17 @@
+package com.example.kakaoshop.order.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
+public class OrderRespFindAllDTO {
+    private List<ProductDTO> products;
+    private int totalPrice;
+
+    public OrderRespFindAllDTO(List<ProductDTO> products, int totalPrice) {
+        this.products = products;
+        this.totalPrice = totalPrice;
+    }
+}
