dfd543b41c2e30fd1afb42f253a3a57b75be7941
yunzae <dsa05037@naver.com>
2023-07-06 14:29:53 +0900

    feat: Update Cart Controller
    장바구니 컨트롤러 수정

--- 2week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/cart/CartRestController.java
+++ 2week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/cart/CartRestController.java
@@ -1,12 +1,15 @@
 package com.example.kakaoshop.cart;
 
 import com.example.kakaoshop._core.utils.ApiUtils;
-import com.example.kakaoshop.cart.response.CartItemDTO;
-import com.example.kakaoshop.cart.response.CartRespFindAllDTO;
-import com.example.kakaoshop.cart.response.ProductOptionDTO;
-import com.example.kakaoshop.cart.response.ProductDTO;
+import com.example.kakaoshop.cart.request.CartReqAddDTO;
+import com.example.kakaoshop.cart.response.*;
+import com.example.kakaoshop.cart.request.OrderReqUpdateDTO;
+import com.example.kakaoshop.cart.response.CartRespCartDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -27,10 +30,10 @@ public class CartRestController {
                 .price(50000)
                 .build();
         cartItemDTO1.setOption(ProductOptionDTO.builder()
-                                .id(1)
-                                .optionName("01. 슬라이딩 지퍼백 크리스마스에디션 4종")
-                                .price(10000)
-                                .build());
+                .id(1)
+                .optionName("01. 슬라이딩 지퍼백 크리스마스에디션 4종")
+                .price(10000)
+                .build());
         cartItemDTOList.add(cartItemDTO1);
 
         CartItemDTO cartItemDTO2 = CartItemDTO.builder()
@@ -39,10 +42,10 @@ public class CartRestController {
                 .price(54500)
                 .build();
         cartItemDTO2.setOption(ProductOptionDTO.builder()
-                                .id(1)
-                                .optionName("02. 슬라이딩 지퍼백 크리스마스에디션 5종")
-                                .price(10900)
-                                .build());
+                .id(1)
+                .optionName("02. 슬라이딩 지퍼백 크리스마스에디션 5종")
+                .price(10900)
+                .build());
         cartItemDTOList.add(cartItemDTO2);
 
         // productDTO 리스트 만들기
@@ -53,7 +56,7 @@ public class CartRestController {
                 ProductDTO.builder()
                         .id(1)
                         .productName("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전")
-                        .cartItems(cartItemDTOList)
+                        .carts(cartItemDTOList)
                         .build()
         );
 
@@ -61,4 +64,47 @@ public class CartRestController {
 
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
+
+
+    //장바구니 담기- 같은물건중복주문시 에러
+    @PostMapping("/carts")
+    public ResponseEntity<?> add(@RequestBody List<CartReqAddDTO> Items) {
+        //같은물건중복주문시 에러는 현재 디비에 연결이 되어잇지 않아서 구현X
+        //todo
+
+        return ResponseEntity.ok(ApiUtils.success(null));
+    }
+
+    //todo 주문하기 장바구니 수정(장바구니 모두 주문)
+    @PutMapping("/carts")
+    public ResponseEntity<?> update(@RequestBody List<OrderReqUpdateDTO> orderReqUpdateDTOS) {
+        //todo
+        List<CartRespCartDTO> cartRespCartDTOS = new ArrayList<>();
+
+        cartRespCartDTOS.add(CartRespCartDTO.builder()
+                .cartId(4)
+                .optionId(1)
+                .optionName("01. 슬라이딩 지퍼백 크리스마스에디션 4종")
+                .quantity(10)
+                .price(100000)
+                .build());
+
+        cartRespCartDTOS.add(CartRespCartDTO.builder()
+                .cartId(5)
+                .optionId(2)
+                .optionName("02. 슬라이딩 지퍼백 플라워에디션 5종")
+                .quantity(10)
+                .price(109000)
+                .build());
+
+
+        CartRespUpdateDTO cartRespUpdateDTO = CartRespUpdateDTO.builder()
+                .carts(cartRespCartDTOS)
+                .totalPrice(209000)
+                .build();
+
+        return ResponseEntity.ok(ApiUtils.success(cartRespUpdateDTO));
+    }
+
+
 }
