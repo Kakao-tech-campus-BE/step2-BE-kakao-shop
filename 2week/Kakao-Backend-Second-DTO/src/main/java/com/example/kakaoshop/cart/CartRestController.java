554181b83a33867354a91159a41e82d5e6b8dd82
BoSeung <gda05189@gmail.com>
2023-07-06 15:28:17 +0900

    refactor - modify cart PutMapping

--- 2week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/cart/CartRestController.java
+++ 2week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/cart/CartRestController.java
@@ -99,21 +99,8 @@ public class CartRestController {
                 .build());
         cartItemDTOList.add(cartItemDTO2);
 
-        // productDTO 리스트 만들기
-        List<ProductDTO> productDTOList = new ArrayList<>();
 
-        // productDTO 리스트에 담기
-        productDTOList.add(
-                ProductDTO.builder()
-                        .id(1)
-                        .productName("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전")
-                        .cartItems(cartItemDTOList)
-                        .build()
-        );
-
-        CartRespFindAllDTO responseDTO = new CartRespFindAllDTO(productDTOList, 209000);
-
-        return ResponseEntity.ok(ApiUtils.success(responseDTO));
+        return ResponseEntity.ok(ApiUtils.success(cartItemDTOList));
     }
 
     @DeleteMapping("/carts")
