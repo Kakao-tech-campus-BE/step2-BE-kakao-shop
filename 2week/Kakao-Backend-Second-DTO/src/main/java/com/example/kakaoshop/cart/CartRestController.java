8d20170c45d497bff1da1da69b058efae878371f
BoSeung <gda05189@gmail.com>
2023-07-06 15:01:35 +0900

    feat - add putMapping /carts

--- 2week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/cart/CartRestController.java
+++ 2week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/cart/CartRestController.java
@@ -4,10 +4,7 @@ import com.example.kakaoshop._core.utils.ApiUtils;
 import com.example.kakaoshop.cart.response.*;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -69,5 +66,53 @@ public class CartRestController {
             ...
          */
         return ResponseEntity.ok(ApiUtils.success(null));
+    }
+
+    @PutMapping("/carts")
+    public ResponseEntity<?> modifyCartList(@RequestBody List<ModifyCartItemDTO> modifyCartItemDTO){
+
+        // 카트 아이템 리스트 만들기
+        List<CartItemDTO> cartItemDTOList = new ArrayList<>();
+
+        // 카트 아이템 리스트에 담기
+        CartItemDTO cartItemDTO1 = CartItemDTO.builder()
+                .id(4)
+                .quantity(modifyCartItemDTO.get(0).getQuantity())
+                .price(50000)
+                .build();
+        cartItemDTO1.setOption(ProductOptionDTO.builder()
+                .id(1)
+                .optionName("01. 슬라이딩 지퍼백 크리스마스에디션 4종")
+                .price(10000)
+                .build());
+        cartItemDTOList.add(cartItemDTO1);
+
+        CartItemDTO cartItemDTO2 = CartItemDTO.builder()
+                .id(5)
+                .quantity(modifyCartItemDTO.get(1).getQuantity())
+                .price(54500)
+                .build();
+        cartItemDTO2.setOption(ProductOptionDTO.builder()
+                .id(1)
+                .optionName("02. 슬라이딩 지퍼백 크리스마스에디션 5종")
+                .price(10900)
+                .build());
+        cartItemDTOList.add(cartItemDTO2);
+
+        // productDTO 리스트 만들기
+        List<ProductDTO> productDTOList = new ArrayList<>();
+
+        // productDTO 리스트에 담기
+        productDTOList.add(
+                ProductDTO.builder()
+                        .id(1)
+                        .productName("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전")
+                        .cartItems(cartItemDTOList)
+                        .build()
+        );
+
+        CartRespFindAllDTO responseDTO = new CartRespFindAllDTO(productDTOList, 209000);
+
+        return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
 }
