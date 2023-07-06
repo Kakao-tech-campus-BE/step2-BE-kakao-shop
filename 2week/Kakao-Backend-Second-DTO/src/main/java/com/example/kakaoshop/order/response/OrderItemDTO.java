0afa7483344082d6a728a2ee69ecd270ba8bc4e4
MinjuKwak01 <mkwak1125@gmail.com>
2023-07-06 13:57:40 +0900

    order 폴더 수정, 추가

--- 2week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/order/response/OrderItemDTO.java
+++ 2week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/order/response/OrderItemDTO.java
@@ -1,2 +1,27 @@
-package com.example.kakaoshop.order.response;public class OrderItemDTO {
+package com.example.kakaoshop.order.response;
+
+
+import com.example.kakaoshop.cart.response.ProductDTO;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
+public class OrderItemDTO {
+
+    private int id;
+    private String optionName;
+    private int quantity;
+    private int price;
+
+    @Builder
+    public OrderItemDTO(int id, String optionName, int quantity, int price){
+        this.id=id;
+        this.optionName=optionName;
+        this.price=price;
+        this.quantity=quantity;
+    }
+
 }
--- 2week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/order/response/ProductDTO.java
+++ 2week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/order/response/ProductDTO.java
@@ -1,2 +1,21 @@
-package com.example.kakaoshop.order.response;public class ProductDTO {
+package com.example.kakaoshop.order.response;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
+public class ProductDTO {
+
+    private String productName;
+    private List<OrderItemDTO> orderItems;
+
+    @Builder
+    public ProductDTO(String productName, List<OrderItemDTO> orderItems){
+        this.productName=productName;
+        this.orderItems=orderItems;
+    }
 }
