72ca642b049903ae56485615efed082da57c7864
WoeunJung <leena09123@naver.com>
2023-07-07 01:50:29 +0900

    feat: order request

--- Kakao-Backend-Second-DTO-main/src/main/java/com/example/kakaoshop/order/OrderRestController.java
+++ Kakao-Backend-Second-DTO-main/src/main/java/com/example/kakaoshop/order/OrderRestController.java
@@ -7,6 +7,7 @@ import com.example.kakaoshop.order.response.OrderResponseFindByIdDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -42,6 +43,37 @@ public class OrderRestController {
                         .build()
         );
         OrderResponseFindByIdDTO responseDTO = new OrderResponseFindByIdDTO(id, productDTOList, 19900);
+        return ResponseEntity.ok(ApiUtils.success(responseDTO));
+    }
+
+    @PostMapping("/orders")
+    public ResponseEntity<?> order(){
+        // 주문 번호는 모두 1번이라 가정 (가짜 데이터기 때문)
+        List<OrderItemDTO> orderItemDTOList = new ArrayList<>();
+
+        // 주문 아이템 리스트에 담기
+        OrderItemDTO orderItemDTO1 = OrderItemDTO.builder()
+                .id(1)
+                .optionName("01. 슬라이딩 지퍼백 크리스마스에디션 4종")
+                .price(10000)
+                .build();
+        orderItemDTOList.add(orderItemDTO1);
+
+        OrderItemDTO orderItemDTO2 = OrderItemDTO.builder()
+                .id(1)
+                .optionName("02. 슬라이딩 지퍼백 크리스마스에디션 5종")
+                .price(9900)
+                .build();
+        orderItemDTOList.add(orderItemDTO2);
+
+        List<OrderProductDTO> productDTOList = new ArrayList<>();
+        productDTOList.add(
+                OrderProductDTO.builder()
+                        .productName("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전")
+                        .orderItems(orderItemDTOList)
+                        .build()
+        );
+        OrderResponseFindByIdDTO responseDTO = new OrderResponseFindByIdDTO(2, productDTOList, 19900);
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
 }
