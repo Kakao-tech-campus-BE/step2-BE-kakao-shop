fd31d8891dfd995c20f70d97a4a6b6ebc52571db
suuding <tngus4789@naver.com>
2023-07-07 17:39:53 +0900

    [feat] 2주차 과제 (DTO, controller 작성)

--- week2/src/main/java/com/example/kakaoshop/cart/CartRestController.java
+++ week2/src/main/java/com/example/kakaoshop/cart/CartRestController.java
@@ -1,12 +1,13 @@
 package com.example.kakaoshop.cart;
 
 import com.example.kakaoshop._core.utils.ApiUtils;
-import com.example.kakaoshop.cart.response.CartItemDTO;
-import com.example.kakaoshop.cart.response.CartRespFindAllDTO;
-import com.example.kakaoshop.cart.response.ProductOptionDTO;
-import com.example.kakaoshop.cart.response.ProductDTO;
+import com.example.kakaoshop.cart.request.CartReqAddDTO;
+import com.example.kakaoshop.cart.request.CartReqUpdateDTO;
+import com.example.kakaoshop.cart.response.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -15,6 +16,15 @@ import java.util.List;
 @RestController
 public class CartRestController {
 
+    // 카트에 요청 데이터(requestDTO) 추가
+    @PostMapping("/carts/add")
+    public ResponseEntity<?> add(@RequestBody CartReqAddDTO requestDTO) {
+
+        return ResponseEntity.ok(ApiUtils.success(null));
+    }
+
+    // 카트(장바구니) 조회
+    // CartRespFindAllDTO <- ProductDTO <- CartItemDTO <- ProductOptionDTO
     @GetMapping("/carts")
     public ResponseEntity<?> findAll() {
         // 카트 아이템 리스트 만들기
@@ -39,7 +49,7 @@ public class CartRestController {
                 .price(54500)
                 .build();
         cartItemDTO2.setOption(ProductOptionDTO.builder()
-                                .id(1)
+                                .id(2)
                                 .optionName("02. 슬라이딩 지퍼백 크리스마스에디션 5종")
                                 .price(10900)
                                 .build());
@@ -57,7 +67,39 @@ public class CartRestController {
                         .build()
         );
 
+        // 카트 조회했을 때 응답 데이터 생성
         CartRespFindAllDTO responseDTO = new CartRespFindAllDTO(productDTOList, 104500);
+
+        return ResponseEntity.ok(ApiUtils.success(responseDTO));
+    }
+
+    // 카트 업데이트
+    // CartRespUpdateDTO <- CartDTO
+    @PostMapping("/carts/update")
+    public ResponseEntity<?> update(@RequestBody List<CartReqUpdateDTO> requestDTO) {
+
+        // 카트 리스트 만들기
+        List<CartDTO> cartDTOList = new ArrayList<>();
+
+        // 카트에 아이템 담기
+        cartDTOList.add(CartDTO.builder()
+                    .cartId(4)
+                    .optionId(1)
+                    .optionName("01. 슬라이딩 지퍼백 크리스마스에디션 4종")
+                    .quantity(10)
+                    .price(100000)
+                    .build());
+
+        cartDTOList.add(CartDTO.builder()
+                    .cartId(5)
+                    .optionId(2)
+                    .optionName("02. 슬라이딩 지퍼백 플라워에디션 5종")
+                    .quantity(10)
+                    .price(109000)
+                    .build());
+
+        // 카트 업데이트 했을 때 응답 데이터 생성
+        CartRespUpdateDTO responseDTO = new CartRespUpdateDTO(cartDTOList, 209000);
 
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
