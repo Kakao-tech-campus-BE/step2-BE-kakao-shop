46561a9915a6b02e090d5f3476cce824171b9be2
kssumin <201566@jnu.ac.kr>
2023-07-07 12:28:05 +0900

    fix : products url에서 page param으로 받도록 변경

--- Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/product/ProductRestController.java
+++ Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/product/ProductRestController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -17,7 +18,7 @@ import java.util.List;
 public class ProductRestController {
 
     @GetMapping("/products")
-    public ResponseEntity<ApiUtils.ApiResult<List<ProductFindAllResponse>>> findAll() {
+    public ResponseEntity<ApiUtils.ApiResult<List<ProductFindAllResponse>>> findAll(@RequestParam(value = "page", defaultValue = "0")int page) {
         List<ProductFindAllResponse> responseDTO = new ArrayList<>();
 
         // 상품 하나씩 집어넣기
