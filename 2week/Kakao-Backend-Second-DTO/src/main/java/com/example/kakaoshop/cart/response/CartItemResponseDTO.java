0d298bd46ca6ea9c87ec433e9cf3ec3635234edb
Cheoroo <195822@jnu.ac.kr>
2023-07-07 20:06:29 +0900

    feat Cart/update 구현을 위한 DTO

--- 2week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/cart/response/CartItemResponseDTO.java
+++ 2week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/cart/response/CartItemResponseDTO.java
@@ -1,2 +1,25 @@
-package com.example.kakaoshop.cart.response;public class CartItemResponseDTO {
+package com.example.kakaoshop.cart.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CartItemResponseDTO {
+
+    private Integer cartId;
+    private int optionId;
+    private String optionName;
+    private int quantity;
+    private int price;
+
+    @Builder
+    public CartItemResponseDTO(Integer cartId, int optionId, String optionName, int quantity, int price) {
+        this.cartId = cartId;
+        this.optionId = optionId;
+        this.optionName = optionName;
+        this.quantity = quantity;
+        this.price = price;
+    }
 }
