3bbcaf76e769f42d87887b0bccf3fc8dffee9451
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-07-07 17:13:02 +0900

    feat : add order controller

--- week2/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/order/OrderRestController.java
+++ week2/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/order/OrderRestController.java
@@ -1,9 +1,8 @@
 package com.example.kakaoshop.order;
 
 import com.example.kakaoshop._core.utils.ApiUtils;
-import com.example.kakaoshop.order.response.OrderDTO;
-import com.example.kakaoshop.order.response.OrderItemDTO;
-import com.example.kakaoshop.order.response.OrderProductDTO;
+import com.example.kakaoshop.order.response.OrderFindResponseDTO;
+import com.example.kakaoshop.order.response.OrderSaveResponseDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,84 +14,84 @@ import java.util.List;
 public class OrderRestController {
     @PostMapping("/save")
     public ResponseEntity<?> save() {
-        OrderItemDTO item1 = OrderItemDTO.builder()
+        OrderFindResponseDTO.Item item1 = OrderFindResponseDTO.Item.builder()
                 .id(4)
                 .optionName("01. 슬라이딩 지퍼백 크리스마스에디션 4종")
                 .quantity(10)
                 .price(100000)
                 .build();
 
-        OrderItemDTO item2 = OrderItemDTO.builder()
+        OrderFindResponseDTO.Item item2 = OrderFindResponseDTO.Item.builder()
                 .id(5)
                 .optionName("02. 슬라이딩 지퍼백 플라워에디션 5종")
                 .quantity(10)
                 .price(109000)
                 .build();
 
-        List<OrderItemDTO> orderItemList = new ArrayList<>();
-        orderItemList.add(item1);
-        orderItemList.add(item2);
+        List<OrderFindResponseDTO.Item> itemList = new ArrayList<>();
+        itemList.add(item1);
+        itemList.add(item2);
 
-        OrderProductDTO orderProduct = OrderProductDTO.builder()
+        OrderFindResponseDTO.Product orderProduct = OrderFindResponseDTO.Product.builder()
                 .productName("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션외 주방용품 특가전")
-                .items(orderItemList)
+                .items(itemList)
                 .build();
 
-        List<OrderProductDTO> products = new ArrayList<>();
+        List<OrderFindResponseDTO.Product> products = new ArrayList<>();
         products.add(orderProduct);
 
-        OrderDTO order = OrderDTO.builder()
+        OrderFindResponseDTO.Response responseDTO = OrderFindResponseDTO.Response.builder()
                 .id(2)
                 .products(products)
                 .totalPrice(209000)
                 .build();
 
-        return ResponseEntity.ok().body(ApiUtils.success(order));
+        return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
     }
 
     @GetMapping("/{idx}")
     public ResponseEntity<?> findById(@PathVariable("idx") int idx) {
-        OrderItemDTO item1 = OrderItemDTO.builder()
+        OrderSaveResponseDTO.Item item1 = OrderSaveResponseDTO.Item.builder()
                 .id(4)
                 .optionName("01. 슬라이딩 지퍼백 크리스마스 에디션 4종")
                 .quantity(10)
                 .price(100000)
                 .build();
 
-        OrderItemDTO item2 = OrderItemDTO.builder()
+        OrderSaveResponseDTO.Item item2 = OrderSaveResponseDTO.Item.builder()
                 .id(5)
                 .optionName("02. 슬라이딩 지퍼백 플라워에디션 4종")
                 .quantity(10)
                 .price(109000)
                 .build();
 
-        List<OrderItemDTO> orderItemList = new ArrayList<>();
+        List<OrderSaveResponseDTO.Item> orderItemList = new ArrayList<>();
         orderItemList.add(item1);
         orderItemList.add(item2);
 
-        OrderProductDTO orderProduct = OrderProductDTO.builder()
+        OrderSaveResponseDTO.Product orderProduct = OrderSaveResponseDTO.Product.builder()
                 .productName("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전")
                 .items(orderItemList)
                 .build();
 
-        List<OrderProductDTO> products = new ArrayList<>();
+        List<OrderSaveResponseDTO.Product> products = new ArrayList<>();
         products.add(orderProduct);
 
-        OrderDTO order = OrderDTO.builder()
+        OrderSaveResponseDTO.Response responseDTO = OrderSaveResponseDTO.Response.builder()
                 .id(2)
                 .products(products)
                 .totalPrice(209000)
                 .build();
 
-        List<OrderDTO> orders = new ArrayList<>();
-        orders.add(order);
-        OrderDTO order1 = OrderDTO.builder()
+        List<OrderSaveResponseDTO.Response> responseDTOs = new ArrayList<>();
+        responseDTOs.add(responseDTO);
+        OrderSaveResponseDTO.Response order1 = OrderSaveResponseDTO.Response.builder()
                 .id(2)
                 .products(products)
                 .totalPrice(209000)
                 .build();
-        orders.add(order);
+        responseDTOs.add(responseDTO);
 
-        return ResponseEntity.ok().body(ApiUtils.success(orders.get(idx-1)));
+        return ResponseEntity.ok().body(ApiUtils.success(responseDTOs.get(idx-1)));
     }
 }
