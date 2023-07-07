1e1850984a43cbadba4567d998bb02aa923038ae
zxc88kr <zxc88kr@naver.com>
2023-07-07 12:22:32 +0900

    fix: Update Code for Function 5

--- 2week/src/main/java/com/example/kakaoshop/product/response/ProductRespFindByIdDTO.java
+++ 2week/src/main/java/com/example/kakaoshop/product/response/ProductRespFindByIdDTO.java
@@ -13,17 +13,15 @@ public class ProductRespFindByIdDTO {
     private String description;
     private String image;
     private int price;
-    private int starCount; // 0~5
     private List<ProductOptionDTO> options;
 
     @Builder
-    public ProductRespFindByIdDTO(int id, String productName, String description, String image, int price, int starCount, List<ProductOptionDTO> options) {
+    public ProductRespFindByIdDTO(int id, String productName, String description, String image, int price, List<ProductOptionDTO> options) {
         this.id = id;
         this.productName = productName;
         this.description = description;
         this.image = image;
         this.price = price;
-        this.starCount = starCount;
         this.options = options;
     }
 }
