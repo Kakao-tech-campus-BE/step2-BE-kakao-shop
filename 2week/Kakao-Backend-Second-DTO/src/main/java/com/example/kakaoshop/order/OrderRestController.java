8c380f75bf3ea1efcbbfe8b07ed8d9c65d688f56
Cheoroo <195822@jnu.ac.kr>
2023-07-07 20:15:23 +0900

    update Order 주문과 확인 테스트를 위한 엔트포인트를 받아 컨트롤러 수정

--- 2week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/order/OrderRestController.java
+++ 2week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/order/OrderRestController.java
@@ -1,88 +1,28 @@
-// OrderRestController
 package com.example.kakaoshop.order;
 
-import com.example.kakaoshop._core.utils.ApiUtils;
-import com.example.kakaoshop.order.request.OrderItemDTO;
-import com.example.kakaoshop.order.request.OrderProductDTO;
-import com.example.kakaoshop.order.request.OrderRequestDTO;
 import com.example.kakaoshop.order.response.OrderResponseDTO;
 import com.example.kakaoshop.order.service.OrderService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 @RestController
-@RequestMapping("/api")
-public class MockOrderController {
+@RequestMapping("/orders")
+public class OrderRestController {
+    private final OrderService orderService;
 
-    @PostMapping("/orders/save")
-    public ResponseEntity<OrderRequestDTO> orderSave(@RequestHeader("Authorization") String token) {
-        // Mock OrderItems
-        OrderItemDTO orderItem1 = OrderItemDTO.builder()
-                .id(4)
-                .optionName("01. 슬라이딩 지퍼백 크리스마스에디션 4종")
-                .quantity(10)
-                .price(100000)
-                .build();
-
-        OrderItemDTO orderItem2 = OrderItemDTO.builder()
-                .id(5)
-                .optionName("02. 슬라이딩 지퍼백 플라워에디션 5종")
-                .quantity(10)
-                .price(109000)
-                .build();
-
-        // Mock Products
-        OrderProductDTO product = OrderProductDTO.builder()
-                .productName("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전")
-                .items(Arrays.asList(orderItem1, orderItem2))
-                .build();
-
-        // Mock Order
-        OrderRequestDTO order = OrderRequestDTO.builder()
-                .id(2)
-                .products(Collections.singletonList(product))
-                .totalPrice(209000)
-                .build();
-
-        return ResponseEntity.ok(order);
+    public OrderRestController(OrderService orderService) {
+        this.orderService = orderService;
     }
 
-    @GetMapping("/orders/{id}")
-    public ResponseEntity<OrderRequestDTO> findById(@PathVariable int id, @RequestHeader("Authorization") String token) {
-        // Returning the same mock data as /orders/save for simplicity
-        // In a real application, this should retrieve the order based on the id from the database
+    @PostMapping("/save")
+    public ResponseEntity<OrderResponseDTO> orderSave() {
+        OrderResponseDTO response = orderService.createOrder();
+        return ResponseEntity.ok(response);
+    }
 
-        // Mock OrderItems
-        OrderItemDTO orderItem1 = OrderItemDTO.builder()
-                .id(4)
-                .optionName("01. 슬라이딩 지퍼백 크리스마스에디션 4종")
-                .quantity(10)
-                .price(100000)
-                .build();
-
-        OrderItemDTO orderItem2 = OrderItemDTO.builder()
-                .id(5)
-                .optionName("02. 슬라이딩 지퍼백 플라워에디션 5종")
-                .quantity(10)
-                .price(109000)
-                .build();
-
-        // Mock Products
-        OrderProductDTO product = OrderProductDTO.builder()
-                .productName("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전")
-                .items(Arrays.asList(orderItem1, orderItem2))
-                .build();
-
-        // Mock Order
-        OrderRequestDTO order = OrderRequestDTO.builder()
-                .id(id)
-                .products(Collections.singletonList(product))
-                .totalPrice(209000)
-                .build();
-
-        return ResponseEntity.ok(order);
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderResponseDTO> findById(@PathVariable int id) {
+        OrderResponseDTO response = orderService.getOrder(id);
+        return ResponseEntity.ok(response);
     }
 }
