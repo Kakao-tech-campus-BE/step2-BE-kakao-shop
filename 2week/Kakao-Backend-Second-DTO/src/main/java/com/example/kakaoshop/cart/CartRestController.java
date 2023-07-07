f5e93726c634c3bf54710d361768e2b83cda305f
Cheoroo <195822@jnu.ac.kr>
2023-07-07 20:08:15 +0900

    update cart담기와 업데이트를 추가하여 컨트롤러 수정

--- 2week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/cart/CartRestController.java
+++ 2week/Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/cart/CartRestController.java
@@ -1,13 +1,16 @@
 package com.example.kakaoshop.cart;
 
+
 import com.example.kakaoshop._core.utils.ApiUtils;
-import com.example.kakaoshop.cart.response.CartItemDTO;
-import com.example.kakaoshop.cart.response.CartRespFindAllDTO;
-import com.example.kakaoshop.cart.response.ProductOptionDTO;
-import com.example.kakaoshop.cart.response.ProductDTO;
+import com.example.kakaoshop.cart.request.CartItemAddDTO;
+import com.example.kakaoshop.cart.request.CartItemUpdateDTO;
+import com.example.kakaoshop.cart.response.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,20 +20,19 @@ public class CartRestController {
 
     @GetMapping("/carts")
     public ResponseEntity<?> findAll() {
-        // 카트 아이템 리스트 만들기
+
         List<CartItemDTO> cartItemDTOList = new ArrayList<>();
 
-        // 카트 아이템 리스트에 담기
         CartItemDTO cartItemDTO1 = CartItemDTO.builder()
                 .id(4)
                 .quantity(5)
                 .price(50000)
                 .build();
         cartItemDTO1.setOption(ProductOptionDTO.builder()
-                                .id(1)
-                                .optionName("01. 슬라이딩 지퍼백 크리스마스에디션 4종")
-                                .price(10000)
-                                .build());
+                .id(1)
+                .optionName("01. 슬라이딩 지퍼백 크리스마스에디션 4종")
+                .price(10000)
+                .build());
         cartItemDTOList.add(cartItemDTO1);
 
         CartItemDTO cartItemDTO2 = CartItemDTO.builder()
@@ -39,16 +41,14 @@ public class CartRestController {
                 .price(54500)
                 .build();
         cartItemDTO2.setOption(ProductOptionDTO.builder()
-                                .id(1)
-                                .optionName("02. 슬라이딩 지퍼백 크리스마스에디션 5종")
-                                .price(10900)
-                                .build());
+                .id(1)
+                .optionName("02. 슬라이딩 지퍼백 크리스마스에디션 5종")
+                .price(10900)
+                .build());
         cartItemDTOList.add(cartItemDTO2);
 
-        // productDTO 리스트 만들기
         List<ProductDTO> productDTOList = new ArrayList<>();
 
-        // productDTO 리스트에 담기
         productDTOList.add(
                 ProductDTO.builder()
                         .id(1)
@@ -58,6 +58,49 @@ public class CartRestController {
         );
 
         CartRespFindAllDTO responseDTO = new CartRespFindAllDTO(productDTOList, 104500);
+
+        return ResponseEntity.ok(ApiUtils.success(responseDTO));
+    }
+
+    @PostMapping("/carts/add")
+    public ResponseEntity<?> addToCart(@RequestBody List<CartItemAddDTO> cartItems) {
+
+        return ResponseEntity.ok(ApiUtils.success("장바구니에 아이템이 성공적으로 추가되었습니다."));
+    }
+    @PostMapping("/carts/update")
+    public ResponseEntity<?> updateCart(@RequestBody List<CartItemUpdateDTO> cartUpdates) {
+        // 임시 응답 리스트 생성
+        List<CartItemResponseDTO> cartResponse = new ArrayList<>();
+        int totalPrice = 0;
+
+        for (CartItemUpdateDTO update : cartUpdates) {
+            CartItemResponseDTO updatedItem;
+            if (update.getCartId() == 4) {
+                updatedItem = CartItemResponseDTO.builder()
+                        .cartId(4)
+                        .optionId(1)
+                        .optionName("01. 슬라이딩 지퍼백 크리스마스에디션 4종")
+                        .quantity(update.getQuantity())
+                        .price(10000 * update.getQuantity())
+                        .build();
+            } else {
+                updatedItem = CartItemResponseDTO.builder()
+                        .cartId(5)
+                        .optionId(2)
+                        .optionName("02. 슬라이딩 지퍼백 플라워에디션 5종")
+                        .quantity(update.getQuantity())
+                        .price(10900 * update.getQuantity())
+                        .build();
+            }
+
+            cartResponse.add(updatedItem);
+            totalPrice += updatedItem.getPrice();
+        }
+
+        CartUpdateResponseDTO responseDTO = CartUpdateResponseDTO.builder()
+                .carts(cartResponse)
+                .totalPrice(totalPrice)
+                .build();
 
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
