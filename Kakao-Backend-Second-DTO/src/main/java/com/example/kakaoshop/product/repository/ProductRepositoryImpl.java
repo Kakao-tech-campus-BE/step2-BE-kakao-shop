2d2be52fc29aae8f6b3c24887a9fdc8417f5f8f9
kssumin <201566@jnu.ac.kr>
2023-07-11 12:29:46 +0900

    feat : product repository 생성
    business layer가 persistence layer에 영향을 받지 않도록 의존성 역전을 함

---
+++ Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/product/repository/ProductRepositoryImpl.java
@@ -0,0 +1,27 @@
+package com.example.kakaoshop.product.repository;
+
+import com.example.kakaoshop.product.domain.service.ProductRepository;
+import com.example.kakaoshop.product.entity.ProductEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class ProductRepositoryImpl implements ProductRepository {
+
+    private final MemoryProductRepository productRepository;
+
+    @Override
+    public Optional<ProductEntity> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    @Override
+    public List<ProductEntity> findAll(int page) {
+        return productRepository.findAll(page);
+    }
+
+}
