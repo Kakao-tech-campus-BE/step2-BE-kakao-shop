877144a65df4ccb8800c0d99a129f41bd9180c1f
xcelxlorx <xcelxlorx@gmail.com>
2023-07-13 08:23:10 +0900

    conf: Change week3 dir

--- week3/kakao-jpa-main/src/main/java/com/example/kakao/product/ProductJPARepository.java
+++ week3/kakao-jpa-main/src/main/java/com/example/kakao/product/ProductJPARepository.java
@@ -3,5 +3,5 @@ package com.example.kakao.product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductJPARepository extends JpaRepository<Product, Integer> {
-    
+
 }
