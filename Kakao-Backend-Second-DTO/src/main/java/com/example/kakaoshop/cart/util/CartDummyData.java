662f95dab124a4756be9682f92add938caa0c31a
kssumin <201566@jnu.ac.kr>
2023-07-13 00:39:34 +0900

    feat : cart mock data 생성
    cart mock 데이터를 생성하기 위해 cart mock 과 연관된 user mock 데이터를 생성

---
+++ Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/cart/util/CartDummyData.java
@@ -0,0 +1,43 @@
+package com.example.kakaoshop.cart.util;
+
+import com.example.kakaoshop.cart.entity.CartEntity;
+import com.example.kakaoshop.product.entity.ProductOptionEntity;
+import com.example.kakaoshop.product.util.ProductDummyData;
+import com.example.kakaoshop.user.util.UserDummyData;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class CartDummyData {
+    private final ProductDummyData productDummyData;
+    private final UserDummyData userDummyData;
+
+    public CartDummyData(ProductDummyData productDummyData, UserDummyData userDummyData) {
+        this.productDummyData = productDummyData;
+        this.userDummyData = userDummyData;
+    }
+
+
+    private List<CartEntity> cartDummyList() {
+        return Arrays.asList(
+                newCart(getProProductOptionEntities().get(0), 1, 5),
+                newCart(getProProductOptionEntities().get(1), 2, 5)
+        );
+    }
+
+    private CartEntity newCart(ProductOptionEntity option, Integer id, Integer quantity) {
+        return CartEntity.builder()
+                .id(id)
+                .user(userDummyData.newUser(1, "ssar"))
+                .productOptionEntity(option)
+                .quantity(quantity)
+                .price(option.getPrice() * quantity)
+                .build();
+    }
+
+    private List<ProductOptionEntity> getProProductOptionEntities() {
+        return productDummyData.optionDummyList();
+    }
+}
