2aaf2c636f0ac4ab11057b27f1b9d583a712d75e
kssumin <201566@jnu.ac.kr>
2023-07-13 12:54:07 +0900

    chore : 필드명에서 자료구조가 들어가지 않도록 필드명 변경

--- Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/cart/domain/service/CartService.java
+++ Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/cart/domain/service/CartService.java
@@ -4,30 +4,40 @@ import com.example.kakaoshop.cart.domain.converter.CartConverter;
 import com.example.kakaoshop.cart.domain.converter.CashierConverter;
 import com.example.kakaoshop.cart.domain.model.Cart;
 import com.example.kakaoshop.cart.domain.model.Cashier;
+import com.example.kakaoshop.cart.domain.validation.CartValidator;
 import com.example.kakaoshop.cart.web.converter.CartFindAllResponseConverter;
+import com.example.kakaoshop.cart.web.request.CartReqeust;
+import com.example.kakaoshop.cart.web.request.CartSaveRequest;
 import com.example.kakaoshop.cart.web.response.CartFindAllResponse;
+import com.example.kakaoshop.product.domain.converter.ProductOptionConverter;
+import com.example.kakaoshop.product.domain.model.ProductOption;
+import com.example.kakaoshop.product.domain.service.ProductOptionRepository;
+import com.example.kakaoshop.product.entity.ProductOptionEntity;
 import com.example.kakaoshop.user.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CartService {
     private final CartRepository cartRepository;
-
-    public CartService(CartRepository cartRepository) {
-        this.cartRepository = cartRepository;
-    }
+    private final ProductOptionRepository productOptionRepository;
+    private final CartValidator cartValidator;
 
     public CartFindAllResponse getCartsByUser(User user) {
-        List<Cart> cartList = cartRepository.findByUser(user)
+        List<Cart> carts = cartRepository.findByUser(user)
                 .stream().map(CartConverter::from)
                 .collect(Collectors.toList());
 
-        Cashier cashier = CashierConverter.from(cartList);
+        Cashier cashier = CashierConverter.from(carts);
         int totalPrice = cashier.calculateTotalPrice();
 
-        return CartFindAllResponseConverter.from(cashier,totalPrice);
+        return CartFindAllResponseConverter.from(cashier, totalPrice);
     }
 }
