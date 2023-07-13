ea2947011da376a8c5ff9b1c39dc2eb8459ddf83
fienestar <fienestar@gmail.com>
2023-07-13 20:43:41 +0900

    feat: order_findByUserId()

--- 3week/kakao-jpa/src/main/java/com/example/kakao/order/OrderJPARepository.java
+++ 3week/kakao-jpa/src/main/java/com/example/kakao/order/OrderJPARepository.java
@@ -1,5 +1,11 @@
 package com.example.kakao.order;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface OrderJPARepository extends JpaRepository<Order, Integer> {}
+public interface OrderJPARepository extends JpaRepository<Order, Integer> {
+  @Query("select o from Order o where o.user.id = :userId")
+  List<Order> findByUserId(@Param("userId") int userId);
+}
