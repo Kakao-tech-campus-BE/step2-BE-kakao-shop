a84a729bd81f636170453c826945d4692db34f1f
Jin_00 <anjdal64@gmail.com>
2023-07-13 16:53:57 +0900

    조회, 수정 테스트 코드 작성

--- 3week/kakao-jpa/src/test/java/com/example/kakao/cart/CartJPARepositoryTest.java
+++ 3week/kakao-jpa/src/test/java/com/example/kakao/cart/CartJPARepositoryTest.java
@@ -8,6 +8,7 @@ import com.example.kakao.product.option.OptionJPARepository;
 import com.example.kakao.user.User;
 import com.example.kakao.user.UserJPARepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -69,12 +70,51 @@ public class CartJPARepositoryTest extends DummyEntity {
         entityManager.clear();
     }
 
-    // test - 조회 ( carts ) , 수정 ( update ) , 담기 ( add )
+    // test - 조회 , 수정
+    // 조회
     @Test
-    public void Test() {
+    void cart_findAll_test() {
+        // given
+        int id = 1;
 
+        // when
+        List<Cart> cartListPS = cartJPARepository.findByUserId(id);
+        // cartListPS.get(0) -> id, user, option, quantity, price
+        // 기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전
+        System.out.println(cartListPS.get(0).getOption().getProduct().getProductName());
+        System.out.println(cartListPS.get(0).getPrice());
+        // then ( 상태 검사 )
+
+        // productId check
+        Assertions.assertThat(cartListPS.get(0).getOption().getProduct().getId()).isEqualTo(1);
+        // productName check
+        Assertions.assertThat(cartListPS.get(0).getOption().getProduct().getProductName()).isEqualTo("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전");
+
+        // carts check
+        Assertions.assertThat(cartListPS.get(0).getOption().getId()).isEqualTo(1);
+        Assertions.assertThat(cartListPS.get(0).getOption().getOptionName()).isEqualTo("01. 슬라이딩 지퍼백 크리스마스에디션 4종");
+        Assertions.assertThat(cartListPS.get(0).getOption().getPrice()).isEqualTo(10000);
+        Assertions.assertThat(cartListPS.get(0).getQuantity()).isEqualTo(5);
+        Assertions.assertThat(cartListPS.get(0).getPrice()).isEqualTo(50000);
     }
 
+    // 수정
+    @Test
+    void cart_update_test() {
+        // given
+        int quantity = 10;
+        int id = 1;
 
+        List<Cart> cartListPS = cartJPARepository.findByUserId(id);
+
+        // when
+        cartJPARepository.updateQuantityById(cartListPS.get(0).getId(), quantity);
+        // 영속성 컨텍스트 초기화
+        entityManager.flush();
+        entityManager.clear();
+        // then ( 상태 검사 )
+        List<Cart> updatedCartList = cartJPARepository.findByUserId(id);
+        Assertions.assertThat(updatedCartList.get(0).getQuantity()).isEqualTo(quantity);
+    }
 
 }
---
+++ 3week/kakao-jpa/src/test/java/com/example/kakao/order/OrderJPARepositoryTest.java
@@ -0,0 +1,4 @@
+package com.example.kakao.order;
+
+public class OrderJPARepositoryTest {
+}
