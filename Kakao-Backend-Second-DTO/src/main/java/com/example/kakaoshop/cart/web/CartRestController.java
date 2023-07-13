2a9fcc642c8cb278f82fc11b9d6aa79bab368b47
kssumin <201566@jnu.ac.kr>
2023-07-13 11:21:57 +0900

    feat : 한 사용자의 장바구니 목록을 가져오도록 controller 구현
    CartRestController에서 findAll()에 해당 책임을 부여

--- Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/cart/web/CartRestController.java
+++ Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/cart/web/CartRestController.java
@@ -1,11 +1,17 @@
 package com.example.kakaoshop.cart.web;
 
+import com.example.kakaoshop._core.security.CustomUserDetails;
 import com.example.kakaoshop._core.utils.ApiUtils;
+import com.example.kakaoshop.cart.domain.service.CartService;
 import com.example.kakaoshop.cart.web.request.CartSaveRequest;
 import com.example.kakaoshop.cart.web.request.CartUpdateRequest;
 import com.example.kakaoshop.cart.web.response.*;
+import com.example.kakaoshop.user.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,70 +21,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class CartRestController {
+    private final CartService cartService;
 
     @GetMapping("/carts")
-    public ResponseEntity<ApiUtils.ApiResult<CartFindAllResponse>> findAll() {
-
-        ArrayList<CartSingleOptionResponse> cartOptionsBySingleProduct = new ArrayList<>();
-
-        CartOptionInfoResponse optionInfo = CartOptionInfoResponse.builder()
-                .id(1)
-                .optionName("01. 슬라이딩 지퍼백 크리스마스에디션 4종")
-                .price(10000)
-                .build();
-
-        // 카트 아이템 리스트에 담기
-        CartSingleOptionResponse cartSingleOptionBySingleProduct = CartSingleOptionResponse.builder()
-                .id(4)
-                .option(optionInfo)
-                .quantity(5)
-                .price(50000)
-                .build();
-
-        cartOptionsBySingleProduct.add(cartSingleOptionBySingleProduct);
-
-        CartOptionInfoResponse optionInfo2 = CartOptionInfoResponse.builder()
-                .id(1)
-                .optionName("02. 슬라이딩 지퍼백 크리스마스에디션 5종")
-                .price(10900)
-                .build();
-
-        CartSingleOptionResponse cartItemDTO2 = CartSingleOptionResponse.builder()
-                .id(5)
-                .option(optionInfo2)
-                .quantity(5)
-                .price(54500)
-                .build();
-
-        cartOptionsBySingleProduct.add(cartItemDTO2);
-
-        // productDTO 리스트 만들기
-        List<CartSingleProductItemResponse> cartProducts = new ArrayList<>();
-
-        // productDTO 리스트에 담기
-        cartProducts.add(
-                CartSingleProductItemResponse.builder()
-                        .id(1)
-                        .productName("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전")
-                        .cartItems(cartOptionsBySingleProduct)
-                        .build()
-        );
-
-        CartFindAllResponse responseDTO = new CartFindAllResponse(cartProducts, 104500);
-
-        return ResponseEntity.ok(ApiUtils.success(responseDTO));
-
+    public ResponseEntity<ApiUtils.ApiResult<CartFindAllResponse>> findAll(@AuthenticationPrincipal CustomUserDetails user) {
+        return ResponseEntity.ok(ApiUtils.success(cartService.getCartsByUser(user.getUser())));
     }
+
     @PostMapping("/carts/add")
 //    @PostMapping("/carts")
-    public ResponseEntity<Void> saveCarts(@Valid CartSaveRequest cartSaveRequest){
+    public ResponseEntity<Void> saveCarts(@Valid CartSaveRequest cartSaveRequest) {
         return new ResponseEntity(ApiUtils.success(null), HttpStatus.OK);
     }
-    @PostMapping("/carts/update")
-    public ResponseEntity<ApiUtils.ApiResult<CartUpdateResponse>> updateCarts(@Valid CartUpdateRequest cartUpdateRequest){
 
-        List<CartChangedOptionResponse> changedOptions= new ArrayList<>();
+    @PostMapping("/carts/update")
+    public ResponseEntity<ApiUtils.ApiResult<CartUpdateResponse>> updateCarts(@Valid CartUpdateRequest cartUpdateRequest) {
+
+        List<CartChangedOptionResponse> changedOptions = new ArrayList<>();
 
         CartChangedOptionResponse singleChangedOption = CartChangedOptionResponse.builder()
                 .cartId(4L)
@@ -107,7 +68,7 @@ public class CartRestController {
                 .totalPrice(totalPrice)
                 .build();
 
-        return new ResponseEntity<>(ApiUtils.success(cartUpdateResponse),HttpStatus.OK);
+        return new ResponseEntity<>(ApiUtils.success(cartUpdateResponse), HttpStatus.OK);
 
     }
 }
