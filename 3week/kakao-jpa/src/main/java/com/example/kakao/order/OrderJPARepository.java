c8b032e69298466a6d642a35cdd13517de300710
yunzae <dsa05037@naver.com>
2023-07-14 23:45:07 +0900

    test: Order query test
    영속성 관련 테스트, API 요청,반환 최적 쿼리 테스트

--- 3week/kakao-jpa/src/main/java/com/example/kakao/order/OrderJPARepository.java
+++ 3week/kakao-jpa/src/main/java/com/example/kakao/order/OrderJPARepository.java
@@ -1,7 +1,14 @@
 package com.example.kakao.order;
 
+import com.example.kakao.cart.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface OrderJPARepository extends JpaRepository<Order, Integer> {
 
+    Optional<Order> findById(int orderId);
 }
