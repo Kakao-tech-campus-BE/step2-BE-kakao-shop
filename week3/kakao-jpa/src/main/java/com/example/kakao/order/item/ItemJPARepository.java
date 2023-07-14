424fb2c4fd12f3653c686ef29c1ae24795114c39
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-07-14 20:40:27 +0900

    feat : test for order

--- week3/kakao-jpa/src/main/java/com/example/kakao/order/item/ItemJPARepository.java
+++ week3/kakao-jpa/src/main/java/com/example/kakao/order/item/ItemJPARepository.java
@@ -1,10 +1,20 @@
 package com.example.kakao.order.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 
 public interface ItemJPARepository extends JpaRepository<Item, Integer> {
+    @Query("select count(*)" +
+            "from Item i " +
+            "where i.order.user.id = :user_id")
+    int countByUserId(@Param("user_id") int user_id);
 
+    @Query("select i " +
+            "from Item i " +
+            "where i.order.id = :order_id")
+    List<Item> findByOrderId(@Param("order_id") int order_id);
 }
