731bd9d00ee5bbf8328f04a1cc3810213a9fc92e
zxc88kr <zxc88kr@naver.com>
2023-07-14 23:32:28 +0900

    style: Update User Code for Spacing

--- 3week/src/main/java/com/example/kakao/user/User.java
+++ 3week/src/main/java/com/example/kakao/user/User.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name="user_tb")
-public class User{
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
