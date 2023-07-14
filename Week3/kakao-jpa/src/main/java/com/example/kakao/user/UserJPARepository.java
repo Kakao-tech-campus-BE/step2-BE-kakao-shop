f7b0a62b5c25b67fb535614671db1d5f02bd3a08
cosmos_1885 <dmsrkql104321@gmail.com>
2023-07-14 21:47:57 +0900

    feat: update UserJPARepositoryTest

---
+++ Week3/kakao-jpa/src/main/java/com/example/kakao/user/UserJPARepository.java
@@ -0,0 +1,14 @@
+package com.example.kakao.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UserJPARepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
+
+    Optional<User> findById(int id);
+}
