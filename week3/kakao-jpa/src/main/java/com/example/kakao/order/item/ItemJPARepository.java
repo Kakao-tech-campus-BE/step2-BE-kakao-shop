4ccc0957435d3f140ceced93e73f0b609833c0af
Jun2-Lee <ljh19980823@gmail.com>
2023-07-14 09:37:25 +0900

    test : order,item test 완료

--- week3/kakao-jpa/src/main/java/com/example/kakao/order/item/ItemJPARepository.java
+++ week3/kakao-jpa/src/main/java/com/example/kakao/order/item/ItemJPARepository.java
@@ -1,10 +1,15 @@
 package com.example.kakao.order.item;
 
+import com.example.kakao.cart.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 
 public interface ItemJPARepository extends JpaRepository<Item, Integer> {
 
+    @Query("select i from Item i join fetch i.order where i.order.id = :orderId")
+    List<Item> findItemByOrderId(@Param("orderId") int orderId);
 }
