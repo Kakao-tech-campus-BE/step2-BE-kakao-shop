cf1f4bb37a25b4bfd22f9c88557cf40f9f23d258
WoeunJung <leena09123@naver.com>
2023-07-14 13:04:35 +0900

    feat: add JPQL query methods in cart, item, order repository

--- week3/kakao-jpa-main/src/main/java/com/example/kakao/cart/CartJPARepository.java
+++ week3/kakao-jpa-main/src/main/java/com/example/kakao/cart/CartJPARepository.java
@@ -1,11 +1,20 @@
 package com.example.kakao.cart;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface CartJPARepository extends JpaRepository<Cart, Integer> {
+    @EntityGraph(attributePaths = {"option", "user", "option.product"})
+    @Query("SELECT c FROM Cart c WHERE c.user.id = :userId")
+    List<Cart> findByUserId(@Param("userId") int userId);
 
+    @EntityGraph(attributePaths = {"option", "user", "option.product"})
+    @Query("SELECT c FROM Cart c WHERE c.id = :id")
+    Optional<Cart> findByCartId(@Param("id") int id);
 }
