84c5f6365164e6d60cf8c86ce3815b4092a468a5
MinseokGo <rhalstjr1999@naver.com>
2023-07-14 11:51:41 +0900

    test: 주문 리포지토리 테스트 리팩토링
    중복 로직을 메서드로 대체 하였습니다.

--- week3/kakao-jpa/src/test/java/com/example/kakao/order/OrderJPARepositoryTest.java
+++ week3/kakao-jpa/src/test/java/com/example/kakao/order/OrderJPARepositoryTest.java
@@ -13,6 +13,7 @@ import com.example.kakao.user.User;
 import com.example.kakao.user.UserJPARepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,56 +30,39 @@ import static org.junit.jupiter.api.Assertions.*;
 @Import(ObjectMapper.class)
 @DataJpaTest
 class OrderJPARepositoryTest extends DummyEntity {
+    private final CartJPARepository cartJPARepository;
+    private final OptionJPARepository optionJPARepository;
+    private final ProductJPARepository productJPARepository;
+    private final UserJPARepository userJPARepository;
+    private final OrderJPARepository orderJPARepository;
+    private final ItemJPARepository itemJPARepository;
+    private final EntityManager em;
+    private final ObjectMapper om;
+
     @Autowired
-    private CartJPARepository cartJPARepository;
-    @Autowired
-    private OptionJPARepository optionJPARepository;
-    @Autowired
-    private ProductJPARepository productJPARepository;
-    @Autowired
-    private UserJPARepository userJPARepository;
-    @Autowired
-    private OrderJPARepository orderJPARepository;
-    @Autowired
-    private ItemJPARepository itemJPARepository;
-    @Autowired
-    private EntityManager em;
-    @Autowired
-    private ObjectMapper om;
+    public OrderJPARepositoryTest(CartJPARepository cartJPARepository, OptionJPARepository optionJPARepository, ProductJPARepository productJPARepository, UserJPARepository userJPARepository, OrderJPARepository orderJPARepository, ItemJPARepository itemJPARepository, EntityManager em, ObjectMapper om) {
+        this.cartJPARepository = cartJPARepository;
+        this.optionJPARepository = optionJPARepository;
+        this.productJPARepository = productJPARepository;
+        this.userJPARepository = userJPARepository;
+        this.orderJPARepository = orderJPARepository;
+        this.itemJPARepository = itemJPARepository;
+        this.em = em;
+        this.om = om;
+    }
 
     @BeforeEach
     public void setUp() {
-        //상품 저장 및 장바구니 담기
         User user = userJPARepository.save(newUser("rhalstjr1999"));
         List<Product> products = productJPARepository.saveAll(productDummyList());
         List<Option> options = optionJPARepository.saveAll(optionDummyList(products));
-        List<Cart> carts = Arrays.asList(
-                newCart(user, options.get(0), 5),
-                newCart(user, options.get(1), 5)
-        );
+        List<Cart> carts = cartDummys(user, options);
 
         cartJPARepository.saveAll(carts);
-        //장바구니 상품 주문
-        int id = 1;
 
-        List<Cart> searchCarts = cartJPARepository.findByUserId(id).orElseThrow(
-                () -> new RuntimeException("해당 유저의 장바구니는 비어있습니다.")
-        );
+        Order order = orderJPARepository.save(newOrder(user));
 
-        User searchUser = userJPARepository.findById(id).orElseThrow(
-                () -> new RuntimeException("해당 유저를 찾을 수 없습니다.")
-        );
-
-        Order saveOrder = newOrder(user);
-        orderJPARepository.save(saveOrder);
-
-        Order order = orderJPARepository.findByUserId(id);
-
-        List<Item> items = Arrays.asList(
-                newItem(searchCarts.get(0), order),
-                newItem(searchCarts.get(1), order)
-        );
-
+        List<Item> items = itemDummys(carts, order);
         itemJPARepository.saveAll(items);
 
         em.clear();
@@ -114,31 +98,21 @@ class OrderJPARepositoryTest extends DummyEntity {
         int id = 1;
 
         List<Cart> carts = cartJPARepository.findByUserId(id).orElseThrow(
-                () -> new RuntimeException("해당 유저의 장바구니는 비어있습니다.")
-        );
+                () -> new RuntimeException("해당 유저의 장바구니는 비어있습니다."));
 
         User user = userJPARepository.findById(id).orElseThrow(
-                () -> new RuntimeException("해당 유저를 찾을 수 없습니다.")
-        );
+                () -> new RuntimeException("해당 유저를 찾을 수 없습니다."));
 
         //when
-        Order saveOrder = newOrder(user);
-        orderJPARepository.save(saveOrder);
+        Order order = orderJPARepository.save(newOrder(user));
 
-        Order order = orderJPARepository.findByUserId(id);
-
-        List<Item> items = Arrays.asList(
-                newItem(carts.get(0), order),
-                newItem(carts.get(1), order)
-        );
-
+        List<Item> items = itemDummys(carts, order);
         itemJPARepository.saveAll(items);
 
-        List<Item> search = itemJPARepository.findByOrderId(1);
+        //then
+        List<Item> searchItems = itemJPARepository.findByOrderId(order.getId());
 
-        System.out.println("============JSON 직렬화============");
-        String responseBody = om.writeValueAsString(search);
-        System.out.println("테스트 : " + responseBody);
+        Assertions.assertThat(items).isEqualTo(searchItems);
     }
 
     @Test
@@ -152,7 +126,5 @@ class OrderJPARepositoryTest extends DummyEntity {
         System.out.println("============JSON 직렬화============");
         String responseBody = om.writeValueAsString(items);
         System.out.println("테스트 : " + responseBody);
-
-        //then
     }
 }
