80c071859fb4eabc716d374c2ccbae054dce7722
zxc88kr <zxc88kr@naver.com>
2023-07-14 23:05:22 +0900

    fix: Delete Unused OptionJPARepository Method

--- 3week/src/main/java/com/example/kakao/product/option/OptionJPARepository.java
+++ 3week/src/main/java/com/example/kakao/product/option/OptionJPARepository.java
@@ -1,14 +1,10 @@
 package com.example.kakao.product.option;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface OptionJPARepository extends JpaRepository<Option, Integer> {
     List<Option> findByProductId(@Param("productId") int productId);
-
-    @Query("select o from Option o join fetch o.product where o.product.id = :productId")
-    List<Option> mFindByProductId(@Param("productId") int productId);
 }
