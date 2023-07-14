cbc12ba2dadf425c99921874d3bc5998562c966d
fienestar <fienestar@gmail.com>
2023-07-14 12:09:49 +0900

    feat: cart_findByUserId()

--- 3week/kakao-jpa/src/main/java/com/example/kakao/cart/CartJPARepository.java
+++ 3week/kakao-jpa/src/main/java/com/example/kakao/cart/CartJPARepository.java
@@ -1,5 +1,12 @@
 package com.example.kakao.cart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface CartJPARepository extends JpaRepository<Cart, Integer> {}
+import java.util.List;
+
+public interface CartJPARepository extends JpaRepository<Cart, Integer> {
+  @Query("select c from Cart c join fetch c.option where c.user.id = :userId")
+  List<Cart> findByUserId(@Param("userId") int userId);
+}
