13610d0921763ee776f36525ace1307c34328c90
Starlight258 <audwl03071@gmail.com>
2023-07-14 13:23:25 +0900

    feat : 3주차 과제 수행

---
+++ week3/src/main/java/com/example/kakao/order/OrderJPARepository.java
@@ -0,0 +1,14 @@
+package com.example.kakao.order;
+
+import com.example.kakao.cart.Cart;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface OrderJPARepository extends JpaRepository<Order, Integer> {
+    @Query("select o from Order o join fetch o.user")
+    List<Order> mFindFetchAll();
+
+}
