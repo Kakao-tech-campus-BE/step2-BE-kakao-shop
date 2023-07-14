42fbae1e15c904b030052d5accf09f78ca9fd3f8
zxc88kr <zxc88kr@naver.com>
2023-07-15 03:14:29 +0900

    feat: Update Code for Function Test 12, 13

--- 3week/src/main/java/com/example/kakao/order/OrderJPARepository.java
+++ 3week/src/main/java/com/example/kakao/order/OrderJPARepository.java
@@ -1,6 +1,15 @@
 package com.example.kakao.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderJPARepository extends JpaRepository<Order, Integer> {
+    @Query("select o from Order o join fetch o.user where o.user.id = :userId")
+    List<Order> mFindByUserId(@Param("userId") int userId);
+
+    @Query("select o from Order o join fetch o.user")
+    List<Order> mFindAll();
 }
