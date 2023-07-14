e40ef3a3a5e79cc7356e720fd078205b9e137898
yunzae <dsa05037@naver.com>
2023-07-14 23:46:36 +0900

    test: item query test
    영속성 관련 테스트, API 요청,반환 최적 쿼리 테스트

--- 3week/kakao-jpa/src/main/java/com/example/kakao/order/item/ItemJPARepository.java
+++ 3week/kakao-jpa/src/main/java/com/example/kakao/order/item/ItemJPARepository.java
@@ -1,10 +1,18 @@
 package com.example.kakao.order.item;
 
+import com.example.kakao.cart.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface ItemJPARepository extends JpaRepository<Item, Integer> {
 
+    @Query("select i from Item i join fetch i.option op join fetch op.product p join fetch i.order o join fetch  o.user u where i.order.id = :orderId")
+    List<Item> mFindAllByOrderId(@Param("orderId") int orderId);
+    Optional<Item> findByOrderId(int orderId);
+    List<Item> findAllByOrderId(int orderId);
 }
