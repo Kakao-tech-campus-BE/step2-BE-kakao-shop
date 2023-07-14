eef7dd072009f1492e2beb557abb23ad787aeddb
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-07-14 19:52:24 +0900

    Feature findByProductAndOptionName

--- 3week/kakao-jpa/src/main/java/com/example/kakao/product/option/OptionJPARepository.java
+++ 3week/kakao-jpa/src/main/java/com/example/kakao/product/option/OptionJPARepository.java
@@ -1,5 +1,6 @@
 package com.example.kakao.product.option;
 
+import com.example.kakao.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,4 +17,9 @@ public interface OptionJPARepository extends JpaRepository<Option, Integer> {
     // findById_select_product_lazy_error_fix_test
     @Query("select o from Option o join fetch o.product where o.product.id = :productId")
     List<Option> mFindByProductId(@Param("productId") int productId);
+
+    //단일 엔티티에만 접근하기 위해 fetch 조인 사용하지 않음
+    @Query("SELECT o FROM Option o WHERE o.product = :product AND o.optionName = :optionName")
+    Option findByProductAndOptionName(@Param("product") Product product, @Param("optionName") String optionName);
+
 }
