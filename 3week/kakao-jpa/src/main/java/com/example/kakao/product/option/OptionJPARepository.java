fe71d6b97925f4f893daaff41d459f5262b63b30
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-07-14 23:15:46 +0900

    Feature findWithProductById

--- 3week/kakao-jpa/src/main/java/com/example/kakao/product/option/OptionJPARepository.java
+++ 3week/kakao-jpa/src/main/java/com/example/kakao/product/option/OptionJPARepository.java
@@ -22,4 +22,8 @@ public interface OptionJPARepository extends JpaRepository<Option, Integer> {
     @Query("SELECT o FROM Option o WHERE o.product = :product AND o.optionName = :optionName")
     Option findByProductAndOptionName(@Param("product") Product product, @Param("optionName") String optionName);
 
+    //Option을 조회하되 Product 내의 타옵션은 조회되지 않음
+    @Query("SELECT o FROM Option o JOIN FETCH o.product WHERE o.id = :id")
+    Option findWithProductById(@Param("id") Integer id);
+
 }
