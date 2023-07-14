eee463dea4239abc50436452cd98e6d318c045ed
zxc88kr <zxc88kr@naver.com>
2023-07-14 20:26:35 +0900

    style: Update Test Code for Indent and Spacing

--- 3week/src/main/java/com/example/kakao/product/option/OptionJPARepository.java
+++ 3week/src/main/java/com/example/kakao/product/option/OptionJPARepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface OptionJPARepository extends JpaRepository<Option, Integer> {
     List<Option> findByProductId(@Param("productId") int productId);
--- 3week/src/test/java/com/example/kakao/user/UserJPARepositoryTest.java
+++ 3week/src/test/java/com/example/kakao/user/UserJPARepositoryTest.java
@@ -44,6 +44,6 @@ public class UserJPARepositoryTest extends DummyEntity {
     }
 
     @Test
-    public void save(){
+    public void save() {
     }
 }
