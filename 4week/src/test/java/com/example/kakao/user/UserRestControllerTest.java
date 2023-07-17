525eeb8b5b8bb4ddd987cad59df12820187a7a51
greennarae <smk5647@gmail.com>
2023-07-17 20:32:07 +0900

    4주차 예제 다운로드

---
+++ 4week/src/test/java/com/example/kakao/user/UserRestControllerTest.java
@@ -0,0 +1,115 @@
+package com.example.kakao.user;
+
+import com.example.kakao._core.errors.GlobalExceptionHandler;
+import com.example.kakao._core.security.JWTProvider;
+import com.example.kakao._core.security.SecurityConfig;
+import com.example.kakao.log.ErrorLogJPARepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.mockito.ArgumentMatchers.any;
+
+// GlobalExceptionHandler 와 UserRestController를 SpringContext에 등록합니다.
+
+@Import({
+        SecurityConfig.class,
+        GlobalExceptionHandler.class
+})
+@WebMvcTest(controllers = {UserRestController.class})
+public class UserRestControllerTest {
+
+    // 객체의 모든 메서드는 추상메서드로 구현됩니다. (가짜로 만들면)
+    // 해당 객체는 SpringContext에 등록됩니다.
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private ErrorLogJPARepository errorLogJPARepository;
+
+    // @WebMvcTest를 하면 MockMvc가 SpringContext에 등록되기 때문에 DI할 수 있습니다.
+    @Autowired
+    private MockMvc mvc;
+
+    // @WebMvcTest를 하면 ObjectMapper가 SpringContext에 등록되기 때문에 DI할 수 있습니다.
+    @Autowired
+    private ObjectMapper om;
+
+    @Test
+    public void t1(){}
+
+    @Test
+    public void join_test() throws Exception {
+        // given
+        UserRequest.JoinDTO requestDTO = new UserRequest.JoinDTO();
+        requestDTO.setEmail("ssarmango@nate.com");
+        requestDTO.setPassword("meta1234!");
+        requestDTO.setUsername("ssarmango");
+        String requestBody = om.writeValueAsString(requestDTO);
+
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/join")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : "+responseBody);
+
+        // then
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
+    }
+
+    @Test
+    public void login_test() throws Exception {
+        // given
+        UserRequest.LoginDTO loginDTO = new UserRequest.LoginDTO();
+        loginDTO.setEmail("ssar@nate.com");
+        loginDTO.setPassword("meta1234!");
+        User user = User.builder().id(1).roles("ROLE_USER").build();
+        String requestBody = om.writeValueAsString(loginDTO);
+
+        // stub
+        String jwt = JWTProvider.create(user);
+        Mockito.when(userService.login(any())).thenReturn(jwt);
+
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/login")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        String responseHeader = result.andReturn().getResponse().getHeader(JWTProvider.HEADER);
+        System.out.println("테스트 : "+responseBody);
+        System.out.println("테스트 : "+responseHeader);
+
+        // then
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
+        Assertions.assertTrue(jwt.startsWith(JWTProvider.TOKEN_PREFIX));
+    }
+
+    @Test
+    public void length_test(){
+        String value = "Bearer eyJ0eX";
+        System.out.println(value.substring(0,6));
+    }
+
+}
