0dd68cf966ae43f2cb8161792b0a3c9ce5a83c6f
SongGaEun16 <thdrkdms561@gmail.com>
2023-07-18 10:43:10 +0900

    3주차 과제 수정

---
+++ 3week/kakao-jpa/src/main/java/com/example/kakao/cart/CartJPARepository.java
@@ -0,0 +1,15 @@
+package com.example.kakao.cart;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+
+public interface CartJPARepository extends JpaRepository<Cart, Integer> {
+
+    @Query("select c from Cart c join fetch c.user where c.user.id = :userId")
+    List<Cart> findCartByUserId(@Param("userId") int userId);
+}
+
