b78f1a64fee66325ea551cec7a5e66376007ab55
xcelxlorx <xcelxlorx@gmail.com>
2023-07-19 01:05:35 +0900

    feat: Cart Controller & Service 구현

--- week4/kakao-controller-unit-test-main/src/main/java/com/example/kakao/cart/CartRestController.java
+++ week4/kakao-controller-unit-test-main/src/main/java/com/example/kakao/cart/CartRestController.java
@@ -1,82 +1,90 @@
 package com.example.kakao.cart;
 
+import com.example.kakao._core.errors.GlobalExceptionHandler;
+import com.example.kakao._core.errors.exception.Exception400;
 import com.example.kakao._core.security.CustomUserDetails;
 import com.example.kakao._core.utils.ApiUtils;
-import com.example.kakao._core.utils.FakeStore;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
 public class CartRestController {
 
-    private final FakeStore fakeStore;
+    private final GlobalExceptionHandler globalExceptionHandler;
+    private final CartService cartService;
 
-// [
-//     {
-//         "optionId":1,
-//         "quantity":5
-//     },
-//     {
-//         "optionId":2,
-//         "quantity":5
-//     }
-// ]
     // (기능8) 장바구니 담기
     @PostMapping("/carts/add")
-    public ResponseEntity<?> addCartList(@RequestBody List<CartRequest.SaveDTO> requestDTOs, @AuthenticationPrincipal CustomUserDetails userDetails) {
-        requestDTOs.forEach(
-                saveDTO -> System.out.println("요청 받은 장바구니 옵션 : "+saveDTO.toString())
-        );
-        return ResponseEntity.ok(ApiUtils.success(null));
+    public ResponseEntity<?> addCartList(@RequestBody @Valid List<CartRequest.SaveDTO> requestDTOs,
+                                         Errors errors,
+                                         @AuthenticationPrincipal CustomUserDetails userDetails,
+                                         HttpServletRequest request) {
+        if (errors.hasErrors()) {
+            List<FieldError> fieldErrors = errors.getFieldErrors();
+            Exception400 ex = new Exception400(fieldErrors.get(0).getDefaultMessage() + ":" + fieldErrors.get(0).getField());
+            return new ResponseEntity<>(
+                    ex.body(),
+                    ex.status()
+            );
+        }
+
+        try{
+            cartService.add(requestDTOs, userDetails);
+            requestDTOs.forEach(
+                    saveDTO -> System.out.println("요청 받은 장바구니 옵션 : "+saveDTO.toString())
+            );
+            return ResponseEntity.ok(ApiUtils.success(null));
+        }catch(RuntimeException e){
+            return globalExceptionHandler.handle(e, request);
+        }
     }
 
     // (기능9) 장바구니 보기 - (주문화면, 결재화면)
     @GetMapping("/carts")
-    public ResponseEntity<?> findAll(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        List<Cart> cartList = fakeStore.getCartList();
-        CartResponse.FindAllDTO responseDTO = new CartResponse.FindAllDTO(cartList);
-        return ResponseEntity.ok(ApiUtils.success(responseDTO));
+    public ResponseEntity<?> findAll(@AuthenticationPrincipal CustomUserDetails userDetails, HttpServletRequest request) {
+        try{
+            CartResponse.FindAllDTO responseDTO = cartService.findAll(userDetails.getUser().getId());
+            return ResponseEntity.ok(ApiUtils.success(responseDTO));
+        }catch(RuntimeException e){
+            return globalExceptionHandler.handle(e, request);
+        }
     }
 
-
-// [
-//     {
-//         "cartId":1,
-//         "quantity":10
-//     },
-//     {
-//         "cartId":2,
-//         "quantity":10
-//     }
-// ]
     // (기능11) 주문하기 - (장바구니 업데이트)
     @PostMapping("/carts/update")
-    public ResponseEntity<?> update(@RequestBody @Valid List<CartRequest.UpdateDTO> requestDTOs, @AuthenticationPrincipal CustomUserDetails userDetails) {
-        requestDTOs.forEach(
-                updateDTO -> System.out.println("요청 받은 장바구니 수정 내역 : "+updateDTO.toString())
-        );
+    public ResponseEntity<?> update(@RequestBody @Valid List<CartRequest.UpdateDTO> requestDTOs,
+                                    Errors errors,
+                                    @AuthenticationPrincipal CustomUserDetails userDetails,
+                                    HttpServletRequest request) {
 
-        // 가짜 저장소의 값을 변경한다.
-        for (CartRequest.UpdateDTO updateDTO : requestDTOs) {
-            for (Cart cart : fakeStore.getCartList()) {
-                if(cart.getId() == updateDTO.getCartId()){
-                    cart.update(updateDTO.getQuantity(), cart.getPrice() * updateDTO.getQuantity());
-                }
-            }
+        if (errors.hasErrors()) {
+            List<FieldError> fieldErrors = errors.getFieldErrors();
+            Exception400 ex = new Exception400(fieldErrors.get(0).getDefaultMessage() + ":" + fieldErrors.get(0).getField());
+            return new ResponseEntity<>(
+                    ex.body(),
+                    ex.status()
+            );
         }
 
-        // DTO를 만들어서 응답한다.
-        CartResponse.UpdateDTO responseDTO = new CartResponse.UpdateDTO(fakeStore.getCartList());
-        return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
+        try{
+            CartResponse.UpdateDTO responseDTO = cartService.update(requestDTOs, userDetails);
+            requestDTOs.forEach(
+                    updateDTO -> System.out.println("요청 받은 장바구니 수정 내역 : " + updateDTO.toString())
+            );
+            return ResponseEntity.ok(ApiUtils.success(responseDTO));
+        }catch(RuntimeException e){
+            return globalExceptionHandler.handle(e, request);
+        }
     }
 
 
---
+++ week4/kakao-controller-unit-test-main/src/main/java/com/example/kakao/cart/CartService.java
@@ -0,0 +1,92 @@
+package com.example.kakao.cart;
+
+import com.example.kakao._core.errors.exception.Exception400;
+import com.example.kakao._core.errors.exception.Exception500;
+import com.example.kakao._core.security.CustomUserDetails;
+import com.example.kakao.log.ErrorLog;
+import com.example.kakao.log.ErrorLogJPARepository;
+import com.example.kakao.product.option.Option;
+import com.example.kakao.product.option.OptionJPARepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class CartService {
+    private final OptionJPARepository optionJPARepository;
+    private final CartJPARepository cartJPARepository;
+    private final ErrorLogJPARepository errorLogJPARepository;
+
+    @Transactional
+    public void add(List<CartRequest.SaveDTO> requestDTOs, @AuthenticationPrincipal CustomUserDetails userDetails){
+        try{
+            for (CartRequest.SaveDTO requestDTO : requestDTOs) {
+                Option option = optionJPARepository.findById(requestDTO.getOptionId()).orElseThrow(
+                        () -> new Exception400("옵션을 찾을 수 없습니다. : " + requestDTO.getOptionId())
+                );
+                Cart cart = Cart.builder()
+                        .user(userDetails.getUser())
+                        .option(option)
+                        .quantity(requestDTO.getQuantity())
+                        .price(option.getPrice() * requestDTO.getQuantity())
+                        .build();
+                cartJPARepository.save(cart);
+            }
+        }catch (Exception e){
+            ErrorLog errorLog = ErrorLog.builder()
+                    .message(e.getMessage())
+                    .build();
+            errorLogJPARepository.save(errorLog);
+            throw new Exception500("unknown server error");
+        }
+    }
+
+    public CartResponse.FindAllDTO findAll(Integer id){
+        try{
+            List<Cart> findAllList = cartJPARepository.findByUserId(id);
+            return new CartResponse.FindAllDTO(findAllList);
+        }catch (Exception e){
+            ErrorLog errorLog = ErrorLog.builder()
+                    .message(e.getMessage())
+                    .build();
+            errorLogJPARepository.save(errorLog);
+            throw new Exception500("unknown server error");
+        }
+    }
+
+    @Transactional
+    public CartResponse.UpdateDTO update(List<CartRequest.UpdateDTO> requestDTOs, @AuthenticationPrincipal CustomUserDetails userDetails){
+        try{
+            List<Cart> carts = new ArrayList<>();
+            for (CartRequest.UpdateDTO requestDTO : requestDTOs) {
+                Cart cart = cartJPARepository.mFindById(requestDTO.getCartId()).orElseThrow(
+                        () -> new Exception400("카트를 찾을 수 없습니다. : " + requestDTO.getCartId())
+                );
+
+                cart.update(requestDTO.getQuantity(), requestDTO.getQuantity() * cart.getOption().getPrice());
+                cartJPARepository.update(cart);
+
+                Cart findCart = cartJPARepository.mFindById(cart.getId()).orElseThrow(
+                        () -> new Exception400("카트를 찾을 수 없습니다. : " + cart.getId())
+                );
+
+                carts.add(findCart);
+            }
+
+            return new CartResponse.UpdateDTO(carts);
+
+        }catch (Exception e){
+            ErrorLog errorLog = ErrorLog.builder()
+                    .message(e.getMessage())
+                    .build();
+            errorLogJPARepository.save(errorLog);
+            throw new Exception500("unknown server error");
+        }
+    }
+}
