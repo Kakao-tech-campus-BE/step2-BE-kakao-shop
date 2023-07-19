e510b0c654201e7b0033f0c7ec475240e5a7e825
Kong Heechan <sk980919@naver.com>
2023-07-19 12:21:13 +0900

    feat: add constructors to CartRequestDtos

--- week4/kakao-controller-unit-test/src/main/java/com/example/kakao/cart/CartRequest.java
+++ week4/kakao-controller-unit-test/src/main/java/com/example/kakao/cart/CartRequest.java
@@ -1,18 +1,16 @@
 package com.example.kakao.cart;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 public class CartRequest {
 
-    @Getter @Setter @ToString
+    @Getter @Setter @ToString @AllArgsConstructor // AllArgsConstructor를 추가하여, test시에 쉽도록 했음
     public static class SaveDTO {
         private int optionId;
         private int quantity;
     }
 
-    @Getter @Setter @ToString
+    @Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
     public static class UpdateDTO {
         private int cartId;
         private int quantity;
