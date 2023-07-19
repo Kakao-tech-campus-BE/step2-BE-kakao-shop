f3b6b7fd450403f226d955a875d331bc896e19d8
Jin Hyuk <ekrrdj07@gmail.com>
2023-07-19 15:58:40 +0900

    feat: 개별 상품 상세 조회

--- 4week/kakao-controller-unit-test/src/main/java/com/example/kakao/product/ProductRestController.java
+++ 4week/kakao-controller-unit-test/src/main/java/com/example/kakao/product/ProductRestController.java
@@ -1,26 +1,22 @@
 package com.example.kakao.product;
 
-import com.example.kakao._core.errors.exception.Exception404;
-import com.example.kakao._core.errors.exception.Exception500;
+import com.example.kakao._core.errors.GlobalExceptionHandler;
 import com.example.kakao._core.utils.ApiUtils;
-import com.example.kakao._core.utils.FakeStore;
-import com.example.kakao.product.option.Option;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
 public class ProductRestController {
-
-    private final FakeStore fakeStore;
     private final ProductService productService;
+    private final GlobalExceptionHandler globalExceptionHandler;
+    private final ProductOptionService productOptionService;
 
     // (기능4) 전체 상품 목록 조회 (페이징 9개씩)
     @GetMapping("/products")
@@ -37,25 +33,12 @@ public class ProductRestController {
 
     // (기능5) 개별 상품 상세 조회
     @GetMapping("/products/{id}")
-    public ResponseEntity<?> findById(@PathVariable int id) {
-        // 1. 더미데이터 가져와서 상품 찾기
-        Product product = fakeStore.getProductList().stream().filter(p -> p.getId() == id).findFirst().orElse(null);
-
-        if(product == null){
-            Exception404 ex = new Exception404("해당 상품을 찾을 수 없습니다:"+id);
-            return new ResponseEntity<>(
-                    ex.body(),
-                    ex.status()
-            );
+    public ResponseEntity<?> findById(@PathVariable int id, HttpServletRequest request) {
+        try {
+            ProductResponse.FindByIdDTO responseDTO = productOptionService.findbyId(id);
+            return ResponseEntity.ok(ApiUtils.success(responseDTO));
+        } catch (RuntimeException e) {
+            return globalExceptionHandler.handle(e,request);
         }
-
-        // 2. 더미데이터 가져와서 해당 상품에 옵션 찾기
-        List<Option> optionList = fakeStore.getOptionList().stream().filter(option -> product.getId() == option.getProduct().getId()).collect(Collectors.toList());
-        
-        // 3. DTO 변환
-        ProductResponse.FindByIdDTO responseDTO = new ProductResponse.FindByIdDTO(product, optionList);
-
-        // 4. 공통 응답 DTO 만들기
-        return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
 }
