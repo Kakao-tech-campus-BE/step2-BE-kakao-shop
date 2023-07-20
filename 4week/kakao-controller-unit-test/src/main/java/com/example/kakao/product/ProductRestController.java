85c31414903a7b60cd7e4b5f0d453f8ae225a30f
MinjuKwak01 <mkwak1125@gmail.com>
2023-07-20 15:26:27 +0900

    fix: 상품 조회 Exception

--- 4week/kakao-controller-unit-test/src/main/java/com/example/kakao/product/ProductRestController.java
+++ 4week/kakao-controller-unit-test/src/main/java/com/example/kakao/product/ProductRestController.java
@@ -1,5 +1,6 @@
 package com.example.kakao.product;
 
+import com.example.kakao._core.errors.GlobalExceptionHandler;
 import com.example.kakao._core.errors.exception.Exception404;
 import com.example.kakao._core.errors.exception.Exception500;
 import com.example.kakao._core.utils.ApiUtils;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,39 +21,58 @@ import java.util.stream.Collectors;
 @RestController
 public class ProductRestController {
 
+
     private final FakeStore fakeStore;
+    //private final GlobalExceptionHandler globalExceptionHandler;
+    //private final ProductService productService;
 
     // (기능4) 전체 상품 목록 조회 (페이징 9개씩)
-    @GetMapping("/products")
-    public ResponseEntity<?> findAll(@RequestParam(defaultValue = "0") int page) {
+    @GetMapping("/products") //  /products?page=0
+    public ResponseEntity<?> findAll(@RequestParam(defaultValue = "0") int page, HttpServletRequest request) {
+    	/*  try {
+            System.out.println("실행");
+            List<ProductResponse.FindAllDTO> responseDTOs = productService.findAlls(page);
+            return ResponseEntity.ok().body(ApiUtils.success(responseDTOs));
+        } catch (RuntimeException e) {
+            return globalExceptionHandler.handle(e, request);
+        }*/
         // 1. 더미데이터 가져와서 페이징하기
-        List<Product> productList = fakeStore.getProductList().stream().skip(page*9).limit(9).collect(Collectors.toList());
+        List<Product> productList = fakeStore.getProductList().stream().skip(page * 9).limit(9).collect(Collectors.toList());
 
-        // 2. DTO 변환
-        List<ProductResponse.FindAllDTO> responseDTOs =
-                productList.stream().map(ProductResponse.FindAllDTO::new).collect(Collectors.toList());
-        
-        // 3. 공통 응답 DTO 만들기
-        return ResponseEntity.ok(ApiUtils.success(responseDTOs));
-    }
-
-    // (기능5) 개별 상품 상세 조회
-    @GetMapping("/products/{id}")
-    public ResponseEntity<?> findById(@PathVariable int id) {
-        // 1. 더미데이터 가져와서 상품 찾기
-        Product product = fakeStore.getProductList().stream().filter(p -> p.getId() == id).findFirst().orElse(null);
-
-        if(product == null){ //product가 null일때
-            Exception404 ex = new Exception404("해당 상품을 찾을 수 없습니다:"+id); //컨트롤러에서 404 에러 잡기
+        if (productList.isEmpty()) {
+            Exception404 ex = new Exception404("페이지가 존재하지 않습니다");
             return new ResponseEntity<>(
                     ex.body(),
                     ex.status()
             );
         }
+        // 2. DTO 변환
+        List<ProductResponse.FindAllDTO> responseDTOs =
+                productList.stream().map(ProductResponse.FindAllDTO::new).collect(Collectors.toList());
 
+
+        // 3. 공통 응답 DTO 만들기
+        return ResponseEntity.ok(ApiUtils.success(responseDTOs));
+
+    }
+
+    // (기능5) 개별 상품 상세 조회
+    @GetMapping("/products/{id}")
+    public ResponseEntity<?> findById(@PathVariable int id) {
+
+        // 1. 더미데이터 가져와서 상품 찾기
+        Product product = fakeStore.getProductList().stream().filter(p -> p.getId() == id).findFirst().orElse(null);
+
+        if(product == null){
+            Exception404 ex = new Exception404("해당 상품을 찾을 수 없습니다:"+id); //404 에러 잡기 (클라이언트가 잘못된 요청)
+            return new ResponseEntity<>(
+                    ex.body(),
+                    ex.status()
+            );
+        }
         // 2. 더미데이터 가져와서 해당 상품에 옵션 찾기
         List<Option> optionList = fakeStore.getOptionList().stream().filter(option -> product.getId() == option.getProduct().getId()).collect(Collectors.toList());
-        
+
         // 3. DTO 변환
         ProductResponse.FindByIdDTO responseDTO = new ProductResponse.FindByIdDTO(product, optionList);
 
