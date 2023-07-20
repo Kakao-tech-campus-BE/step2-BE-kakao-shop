94c61ccf9c7c4226b4b6d5ba6a7eefe9339d8fe1
Jin_00 <anjdal64@gmail.com>
2023-07-20 22:51:18 +0900

    /carts/update 테스트 코드 2
    quantity 에 음수가 들어갔을 때 테스트

--- 4week/kakao-controller-unit-test-main/src/test/java/com/example/kakao/cart/CartRestControllerTest.java
+++ 4week/kakao-controller-unit-test-main/src/test/java/com/example/kakao/cart/CartRestControllerTest.java
@@ -211,4 +211,38 @@ public class CartRestControllerTest {
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].quantity").value(10));
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].price").value(100000));
     }
+
+    @WithMockUser(username = "ssar@nate.com", roles = "USER")
+    @Test
+    // quantity 에 음수가 들어갔을 때
+    public void update_test2() throws Exception{
+        // given
+        List<CartRequest.UpdateDTO> requestDTOs = new ArrayList<>();
+        CartRequest.UpdateDTO d1 = new CartRequest.UpdateDTO();
+        d1.setCartId(1);
+        d1.setQuantity(-1);
+        CartRequest.UpdateDTO d2 = new CartRequest.UpdateDTO();
+        d2.setCartId(2);
+        d2.setQuantity(10);
+        requestDTOs.add(d1);
+        requestDTOs.add(d2);
+        String requestBody = om.writeValueAsString(requestDTOs);
+        System.out.println("테스트 : "+requestBody);
+
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/carts/update")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : "+responseBody);
+
+        // then
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("false"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response").value(nullValue()));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.error.message").value("수량은 0 이상이어야 합니다. quantity : -1"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.error.status").value(400));
+    }
 }
