a1101b5b2650cad4628fce7ea010e8a06c6ac9c3
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-07-20 11:33:24 +0900

    test : product repository

--- week4/kakao-controller-unit-test-main/src/test/java/com/example/kakao/product/ProductJPARepositoryTest.java
+++ week4/kakao-controller-unit-test-main/src/test/java/com/example/kakao/product/ProductJPARepositoryTest.java
@@ -77,6 +77,31 @@ public class ProductJPARepositoryTest extends DummyEntity {
         Assertions.assertThat(productPG.getContent().get(0).getPrice()).isEqualTo(1000);
     }
 
+    @Test
+    public void product_findAll_error_test() throws JsonProcessingException {
+        // given
+        int page = -1;
+        int size = 9;
+
+        // when
+        PageRequest pageRequest = PageRequest.of(page, size);
+        Page<Product> productPG = productJPARepository.findAll(pageRequest);
+        String responseBody = om.writeValueAsString(productPG);
+        System.out.println("테스트 : "+responseBody);
+
+        // then
+        Assertions.assertThat(productPG.getTotalPages()).isEqualTo(2);
+        Assertions.assertThat(productPG.getSize()).isEqualTo(9);
+        Assertions.assertThat(productPG.getNumber()).isEqualTo(0);
+        Assertions.assertThat(productPG.getTotalElements()).isEqualTo(15);
+        Assertions.assertThat(productPG.isFirst()).isEqualTo(true);
+        Assertions.assertThat(productPG.getContent().get(0).getId()).isEqualTo(1);
+        Assertions.assertThat(productPG.getContent().get(0).getProductName()).isEqualTo("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전");
+        Assertions.assertThat(productPG.getContent().get(0).getDescription()).isEqualTo("");
+        Assertions.assertThat(productPG.getContent().get(0).getImage()).isEqualTo("/images/1.jpg");
+        Assertions.assertThat(productPG.getContent().get(0).getPrice()).isEqualTo(1000);
+    }
+
     // ManyToOne 전략을 Eager로 간다면 추천
     @Test
     public void option_findByProductId_eager_test() throws JsonProcessingException {
@@ -123,7 +148,7 @@ public class ProductJPARepositoryTest extends DummyEntity {
         int id = 1;
 
         // when
-        List<Option> optionListPS = optionJPARepository.mFindByProductId(id); // Lazy
+        List<Option> optionListPS = optionJPARepository.findByProductId(id); // Lazy
 
         System.out.println("json 직렬화 직전========================");
         String responseBody = om.writeValueAsString(optionListPS);
@@ -153,5 +178,20 @@ public class ProductJPARepositoryTest extends DummyEntity {
         System.out.println("테스트 : "+responseBody2);
 
         // then
+    }
+
+    @Test
+    public void findById_test() throws JsonProcessingException {
+        // given
+        int id = 1;
+
+        // when
+        Product product = productJPARepository.findById(id).orElseThrow(
+                () -> new RuntimeException("제품을 찾을 수 없습니다.")
+        );
+
+        // then
+        String result = om.writeValueAsString(product);
+        System.out.println(result);
     }
 }
