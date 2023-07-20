c0d38a2d7a6b4c9765943d2ff6c03d2310bddedf
yunzae <dsa05037@naver.com>
2023-07-20 13:19:34 +0900

    test: Update @WithMockCustomeruser

--- 4week/kakao-controller-unit-test/src/test/java/com/example/kakao/_core/util/WithMockCustomUser.java
+++ 4week/kakao-controller-unit-test/src/test/java/com/example/kakao/_core/util/WithMockCustomUser.java
@@ -1,18 +1,16 @@
 package com.example.kakao._core.util;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-
-import com.example.kakao.user.User;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.test.context.support.WithSecurityContext;
 
 @Retention(RetentionPolicy.RUNTIME)
 @WithSecurityContext(factory = WithMockCustomUserSecurityContextFactory.class)
 public @interface WithMockCustomUser {
 
+    int userId() default 1;
     String username() default "yunzae";
 
-    String vlaue() default "yunzae2";
+    String roles() default "ROLE_USER";
 
 }
 
--- 4week/kakao-controller-unit-test/src/test/java/com/example/kakao/_core/util/WithMockCustomUserSecurityContextFactory.java
+++ 4week/kakao-controller-unit-test/src/test/java/com/example/kakao/_core/util/WithMockCustomUserSecurityContextFactory.java
@@ -1,25 +1,16 @@
 package com.example.kakao._core.util;
-
-
 import com.example.kakao.user.User;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class WithMockCustomUserSecurityContextFactory implements WithSecurityContextFactory<WithMockCustomUser> {
         @Override
         public SecurityContext createSecurityContext(WithMockCustomUser customUser) {
             SecurityContext context = SecurityContextHolder.createEmptyContext();
-            List<GrantedAuthority> grantedAuthorities = new ArrayList();
-            grantedAuthorities.add(new SimpleGrantedAuthority("USER"));
-
-            User principal = User.builder().id(1).roles("ROLE_USER").build();
+            User principal = User.builder().id(customUser.userId()).username(customUser.username()).roles(customUser.roles()).build();
             UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
                     principal, principal.getPassword(), principal.getAuthorities());
 
