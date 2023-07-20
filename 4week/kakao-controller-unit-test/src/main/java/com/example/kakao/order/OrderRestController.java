d0441570bb6833d83f3ca4ebfa0b0a3c6cec060e
MinjuKwak01 <mkwak1125@gmail.com>
2023-07-20 15:35:23 +0900

    style: 코드 포맷 변경

--- 4week/kakao-controller-unit-test/src/main/java/com/example/kakao/order/OrderRestController.java
+++ 4week/kakao-controller-unit-test/src/main/java/com/example/kakao/order/OrderRestController.java
@@ -1,5 +1,9 @@
 package com.example.kakao.order;
 
+import com.example.kakao._core.errors.GlobalExceptionHandler;
+import com.example.kakao._core.errors.exception.Exception400;
+import com.example.kakao._core.errors.exception.Exception403;
+import com.example.kakao._core.errors.exception.Exception404;
 import com.example.kakao._core.security.CustomUserDetails;
 import com.example.kakao._core.utils.ApiUtils;
 import com.example.kakao._core.utils.FakeStore;
@@ -11,8 +15,10 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,19 +28,23 @@ public class OrderRestController {
 
     // (기능12) 결재
     @PostMapping("/orders/save")
-    public ResponseEntity<?> save(@AuthenticationPrincipal CustomUserDetails userDetails) {
+    public ResponseEntity<?> save(@AuthenticationPrincipal CustomUserDetails userDetails, HttpServletRequest request) {
+
         Order order = fakeStore.getOrderList().get(0);
         List<Item> itemList = fakeStore.getItemList();
         OrderResponse.FindByIdDTO responseDTO = new OrderResponse.FindByIdDTO(order, itemList);
+
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
+
     }
 
     // (기능13) 주문 결과 확인
     @GetMapping("/orders/{id}")
     public ResponseEntity<?> findById(@PathVariable int id) {
-        Order order = fakeStore.getOrderList().get(id-1);
+        Order order = fakeStore.getOrderList().get(id - 1);
         List<Item> itemList = fakeStore.getItemList();
         OrderResponse.FindByIdDTO responseDTO = new OrderResponse.FindByIdDTO(order, itemList);
+
         return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
     }
 
