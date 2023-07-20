2afea81e918a78ab32fc8752526466bbd9d2a3fd
Jin_00 <anjdal64@gmail.com>
2023-07-20 22:43:13 +0900

    /carts/add 테스트2
    quantity 값에 음수가 들어갔을 경우 테스트

--- 4week/kakao-controller-unit-test-main/src/test/java/com/example/kakao/cart/CartRestControllerTest.java
+++ 4week/kakao-controller-unit-test-main/src/test/java/com/example/kakao/cart/CartRestControllerTest.java
@@ -1,12 +1,14 @@
 package com.example.kakao.cart;
 
 import com.example.kakao._core.errors.GlobalExceptionHandler;
+import com.example.kakao._core.errors.exception.Exception400;
 import com.example.kakao._core.security.JWTProvider;
 import com.example.kakao._core.security.SecurityConfig;
 import com.example.kakao._core.utils.FakeStore;
 import com.example.kakao.log.ErrorLogJPARepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -22,20 +24,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willThrow;
+import static org.mockito.Mockito.doNothing;
 
 @Import({
         FakeStore.class,
         SecurityConfig.class,
-        GlobalExceptionHandler.class
+        GlobalExceptionHandler.class,
+        CartService.class
 })
 @WebMvcTest(controllers = {CartRestController.class})
 public class CartRestControllerTest {
 
     @MockBean
     private ErrorLogJPARepository errorLogJPARepository;
-
-    @MockBean // CartService
-    private CartService cartService;
 
     @Autowired
     private MockMvc mvc;
@@ -74,6 +78,43 @@ public class CartRestControllerTest {
         result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response").value(nullValue()));
         result.andExpect(MockMvcResultMatchers.jsonPath("$.error").value(nullValue()));
+    }
+
+    @WithMockUser(username = "ssar@nate.com", roles = "USER")
+    @Test
+    // quantity 에 음수가 들어갔을 경우
+    public void add_test2() throws Exception {
+        // given
+        List<CartRequest.SaveDTO> requestDTOs = new ArrayList<>();
+        CartRequest.SaveDTO d1 = new CartRequest.SaveDTO();
+        d1.setOptionId(1);
+        d1.setQuantity(-1);
+        CartRequest.SaveDTO d2 = new CartRequest.SaveDTO();
+        d2.setOptionId(2);
+        d2.setQuantity(5);
+        requestDTOs.add(d1);
+        requestDTOs.add(d2);
+        String requestBody = om.writeValueAsString(requestDTOs);
+        System.out.println("테스트 : "+requestBody);
+
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/carts/add")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : "+responseBody);
+
+        // then
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("false"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response").value(nullValue()));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.error.message").value("수량은 0 이상이어야 합니다. quantity : -1"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.error.status").value(400));
+
+
+
     }
 
     @WithMockUser(username = "ssar@nate.com", roles = "USER")
