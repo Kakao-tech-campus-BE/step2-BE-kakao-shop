e392d84d0d011c122582395428b2b78d3f3410b0
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-07-20 23:54:38 +0900

    [4week] import Handouts

---
+++ 4week/kakao-controller-unit-test/src/test/java/com/example/kakao/cart/CartRestControllerTest.java
@@ -0,0 +1,68 @@
+package com.example.kakao.cart;
+
+import com.example.kakao._core.security.JWTProvider;
+import com.example.kakao._core.security.SecurityConfig;
+import com.example.kakao._core.utils.FakeStore;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Import({
+        FakeStore.class,
+        SecurityConfig.class
+})
+@WebMvcTest(controllers = {CartRestController.class})
+public class CartRestControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @WithMockUser(username = "ssar@nate.com", roles = "USER")
+    @Test
+    public void update_test() throws Exception {
+        // given
+        List<CartRequest.UpdateDTO> requestDTOs = new ArrayList<>();
+        CartRequest.UpdateDTO d1 = new CartRequest.UpdateDTO();
+        d1.setCartId(1);
+        d1.setQuantity(10);
+        CartRequest.UpdateDTO d2 = new CartRequest.UpdateDTO();
+        d2.setCartId(2);
+        d2.setQuantity(10);
+        requestDTOs.add(d1);
+        requestDTOs.add(d2);
+        String requestBody = om.writeValueAsString(requestDTOs);
+        System.out.println("테스트 : "+requestBody);
+
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/carts/update")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : "+responseBody);
+
+        // then
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].cartId").value(1));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].optionId").value(1));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].optionName").value("01. 슬라이딩 지퍼백 크리스마스에디션 4종"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].quantity").value(10));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].price").value(100000));
+    }
+}
