a6b234651144e83eefad465d5c3cfb10487a14d5
MinseokGo <rhalstjr1999@naver.com>
2023-07-20 20:44:46 +0900

    test: 장바구니 조회 테스트 추가
    기존의 베이스 파일에서 장바구니 조회 테스트가 누락되어 있어 추가하였습니다.

--- week4/kakao-controller-unit-test/src/test/java/com/example/kakao/cart/CartRestControllerTest.java
+++ week4/kakao-controller-unit-test/src/test/java/com/example/kakao/cart/CartRestControllerTest.java
@@ -1,9 +1,9 @@
 package com.example.kakao.cart;
 
-import com.example.kakao._core.security.JWTProvider;
 import com.example.kakao._core.security.SecurityConfig;
 import com.example.kakao._core.utils.FakeStore;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -17,6 +17,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Import({
         FakeStore.class,
@@ -33,7 +36,61 @@ public class CartRestControllerTest {
 
     @WithMockUser(username = "ssar@nate.com", roles = "USER")
     @Test
-    public void update_test() throws Exception {
+    @DisplayName("장바구니 조회 테스트")
+    void findAll_test() throws Exception {
+        //given
+
+        //when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .get("/carts")
+        );
+
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : "+responseBody);
+
+        //then
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
+    }
+
+    @WithMockUser(username = "ssar@nate.com", roles = "USER")
+    @Test
+    @DisplayName("장바구니 담기 테스트")
+    void add_test() throws Exception {
+        //given
+        List<CartRequest.SaveDTO> requestDTOs = new ArrayList<>();
+
+        CartRequest.SaveDTO saveDTO1 = new CartRequest.SaveDTO();
+        saveDTO1.setOptionId(2);
+        saveDTO1.setQuantity(5);
+        requestDTOs.add(saveDTO1);
+
+        CartRequest.SaveDTO saveDTO2 = new CartRequest.SaveDTO();
+        saveDTO2.setOptionId(2);
+        saveDTO2.setQuantity(5);
+        requestDTOs.add(saveDTO2);
+
+        String requestBody = om.writeValueAsString(requestDTOs);
+        System.out.println("테스트 : "+requestBody);
+
+        //then
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/carts/add")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : "+responseBody);
+
+        // then
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
+    }
+
+    @WithMockUser(username = "ssar@nate.com", roles = "USER")
+    @Test
+    @DisplayName("장바구니 수정 테스트")
+    void update_test() throws Exception {
         // given
         List<CartRequest.UpdateDTO> requestDTOs = new ArrayList<>();
         CartRequest.UpdateDTO d1 = new CartRequest.UpdateDTO();
@@ -64,5 +121,41 @@ public class CartRestControllerTest {
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].optionName").value("01. 슬라이딩 지퍼백 크리스마스에디션 4종"));
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].quantity").value(10));
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].price").value(100000));
+    }
+
+    @WithMockUser(username = "ssar@nate.com", roles = "USER")
+    @Test
+    @DisplayName("잘못된 HTTP 메서드로 서버에 요청 테스트")
+    void notAllowed_httpMethod_test() throws Exception {
+        //given
+        int id = 1;
+
+        //when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/carts")
+                        .param("page", String.valueOf(id))
+        );
+
+        //then
+        // 4xx 에러 검증 - 405
+        result.andExpect(status().is4xxClientError()).andDo(print());
+    }
+
+    @Test
+    @DisplayName("인증되지 않은 사용자가 서버 자원에 접근 테스트")
+    void notAuth_access_test() throws Exception {
+        //given
+
+        //when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .get("/carts")
+        );
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : "+responseBody);
+
+        //then
+        result.andExpect(status().is4xxClientError()).andDo(print());
     }
 }
