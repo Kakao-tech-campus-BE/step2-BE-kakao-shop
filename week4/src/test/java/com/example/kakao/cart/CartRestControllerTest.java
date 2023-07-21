c4c98bc00dc5106e19b701326b75d5ec3e0a395c
goberomsu <gobeumsu@gmail.com>
2023-07-21 12:10:42 +0900

    Feat : Car 테스트 마무리

--- week4/src/test/java/com/example/kakao/cart/CartRestControllerTest.java
+++ week4/src/test/java/com/example/kakao/cart/CartRestControllerTest.java
@@ -22,10 +22,11 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.Mockito.when;
+
 @Import({
         FakeStore.class,
         SecurityConfig.class,
-        FakeStore.class
 })
 @WebMvcTest(controllers = {CartRestController.class})
 public class CartRestControllerTest {
@@ -36,6 +37,8 @@ public class CartRestControllerTest {
     @Autowired
     private ObjectMapper om;
 
+    @MockBean
+    private FakeStore fakeStore;
 
     @DisplayName("장바구니 추가")
     @WithMockUser(username = "ssar@nate.com", roles = "USER")
@@ -43,16 +46,13 @@ public class CartRestControllerTest {
     public void add_test() throws Exception{
         // given
         List<CartRequest.SaveDTO> requestDTOs = new ArrayList<>();
-        CartRequest.SaveDTO d1 = new CartRequest.SaveDTO();
-        d1.setOptionId(1);
-        d1.setQuantity(10);
-        CartRequest.SaveDTO d2 = new CartRequest.SaveDTO();
-        d2.setOptionId(2);
-        d2.setQuantity(10);
+        CartRequest.SaveDTO d1 = createSaveDTO(1, 10);
+        CartRequest.SaveDTO d2 = createSaveDTO(2, 10);
+
         requestDTOs.add(d1);
         requestDTOs.add(d2);
         String requestBody = om.writeValueAsString(requestDTOs);
-        System.out.println("테스트 : "+requestBody);
+
 
         // when
         ResultActions result = mvc.perform(
@@ -65,6 +65,7 @@ public class CartRestControllerTest {
         System.out.println("테스트 : "+responseBody);
 
         // then
+        result.andExpect(MockMvcResultMatchers.status().isOk());
         result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
     }
 
@@ -82,10 +83,10 @@ public class CartRestControllerTest {
         String responseBody = result.andReturn().getResponse().getContentAsString();
         System.out.println("테스트 : "+responseBody);
         // then
+        result.andExpect(MockMvcResultMatchers.status().isOk());
         result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response.products[0].id").value(1));
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response.products[0].productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"));
-
     }
 
     @WithMockUser(username = "ssar@nate.com", roles = "USER")
@@ -93,16 +94,12 @@ public class CartRestControllerTest {
     public void update_test() throws Exception {
         // given
         List<CartRequest.UpdateDTO> requestDTOs = new ArrayList<>();
-        CartRequest.UpdateDTO d1 = new CartRequest.UpdateDTO();
-        d1.setCartId(1);
-        d1.setQuantity(10);
-        CartRequest.UpdateDTO d2 = new CartRequest.UpdateDTO();
-        d2.setCartId(2);
-        d2.setQuantity(10);
+        CartRequest.UpdateDTO d1 = createUpdateDTO(1, 10);
+        CartRequest.UpdateDTO d2 = createUpdateDTO(2, 10);
+
         requestDTOs.add(d1);
         requestDTOs.add(d2);
         String requestBody = om.writeValueAsString(requestDTOs);
-        System.out.println("테스트 : "+requestBody);
 
         // when
         ResultActions result = mvc.perform(
@@ -115,12 +112,86 @@ public class CartRestControllerTest {
         System.out.println("테스트 : "+responseBody);
 
         // then
+        result.andExpect(MockMvcResultMatchers.status().isOk());
         result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].cartId").value(1));
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].optionId").value(1));
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].optionName").value("01. 슬라이딩 지퍼백 크리스마스에디션 4종"));
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].quantity").value(10));
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].price").value(100000));
+    }
+
+    // 테스트용 DTO 생성
+    private CartRequest.SaveDTO createSaveDTO(int optionId, int quantity) {
+        CartRequest.SaveDTO dto = new CartRequest.SaveDTO();
+        dto.setOptionId(optionId);
+        dto.setQuantity(quantity);
+        return dto;
+    }
+    // 테스트용 DTO 생성
+    private CartRequest.UpdateDTO createUpdateDTO(int cartId, int quantity) {
+        CartRequest.UpdateDTO dto = new CartRequest.UpdateDTO();
+        dto.setCartId(cartId);
+        dto.setQuantity(quantity);
+        return dto;
+    }
+
+
+    // 추후 네거티브 테스트를 위해서 남겨둡니다
+    @Test @WithMockUser(username = "ssar@nate.com", roles = "USER")
+    public void add_test_with_empty_list() throws Exception {
+        // given
+        List<CartRequest.SaveDTO> requestDTOs = new ArrayList<>();
+        String requestBody = om.writeValueAsString(requestDTOs);
+
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/carts/add")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        result.andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test @WithMockUser(username = "ssar@nate.com", roles = "USER")
+    public void update_test_with_empty_list() throws Exception {
+        // given
+        List<CartRequest.UpdateDTO> requestDTOs = new ArrayList<>();
+        String requestBody = om.writeValueAsString(requestDTOs);
+
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/carts/update")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        result.andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+
+    @Test @WithMockUser(username = "ssar@nate.com", roles = "USER")
+    public void findAll_test_no_carts() throws Exception {
+        // given
+        when(fakeStore.getCartList()).thenReturn(new ArrayList<>());
+
+
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .get("/carts")
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        //401 에러
+        result.andExpect(MockMvcResultMatchers.status().isOk());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response").isEmpty());
     }
 
 }
