02fc15af8ead635114f272609f64025ae38397ff
xcelxlorx <xcelxlorx@gmail.com>
2023-07-21 14:19:14 +0900

    test: UserRestControllerTest 구현

--- week4/kakao-controller-unit-test-main/src/test/java/com/example/kakao/user/UserRestControllerTest.java
+++ week4/kakao-controller-unit-test-main/src/test/java/com/example/kakao/user/UserRestControllerTest.java
@@ -4,11 +4,9 @@ import com.example.kakao._core.errors.GlobalExceptionHandler;
 import com.example.kakao._core.security.JWTProvider;
 import com.example.kakao._core.security.SecurityConfig;
 import com.example.kakao.log.ErrorLogJPARepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,42 +16,33 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.mockito.ArgumentMatchers.any;
-
-// GlobalExceptionHandler 와 UserRestController를 SpringContext에 등록합니다.
 
 @Import({
         SecurityConfig.class,
         GlobalExceptionHandler.class
 })
 @WebMvcTest(controllers = {UserRestController.class})
-public class UserRestControllerTest {
+class UserRestControllerTest {
 
-    // 객체의 모든 메서드는 추상메서드로 구현됩니다. (가짜로 만들면)
-    // 해당 객체는 SpringContext에 등록됩니다.
     @MockBean
     private UserService userService;
 
     @MockBean
     private ErrorLogJPARepository errorLogJPARepository;
 
-    // @WebMvcTest를 하면 MockMvc가 SpringContext에 등록되기 때문에 DI할 수 있습니다.
     @Autowired
     private MockMvc mvc;
 
-    // @WebMvcTest를 하면 ObjectMapper가 SpringContext에 등록되기 때문에 DI할 수 있습니다.
     @Autowired
     private ObjectMapper om;
 
     @Test
-    public void t1(){}
-
-    @Test
-    public void join_test() throws Exception {
+    @DisplayName("회원가입 성공")
+    void join_success_test() throws Exception {
         // given
         UserRequest.JoinDTO requestDTO = new UserRequest.JoinDTO();
         requestDTO.setEmail("ssarmango@nate.com");
@@ -70,14 +59,102 @@ public class UserRestControllerTest {
         );
 
         String responseBody = result.andReturn().getResponse().getContentAsString();
-        System.out.println("테스트 : "+responseBody);
+        System.out.println("responseBody=" + responseBody);
 
         // then
         result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
     }
 
     @Test
-    public void login_test() throws Exception {
+    @DisplayName("회원가입 실패 - 이메일 형식 오류")
+    void join_fail_test1() throws Exception {
+        // given
+        UserRequest.JoinDTO requestDTO = new UserRequest.JoinDTO();
+        requestDTO.setEmail("ssarmangonate.com");
+        requestDTO.setPassword("meta1234!");
+        requestDTO.setUsername("ssarmango");
+        String requestBody = om.writeValueAsString(requestDTO);
+
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/join")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("responseBody=" + responseBody);
+
+        // then
+        result.andExpect(MockMvcResultMatchers.status().isBadRequest());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value(false));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response").isEmpty());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.error.message").value("이메일 형식으로 작성해주세요:email"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.error.status").value(400));
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 - 비밀번호 형식 오류")
+    void join_fail_test2() throws Exception {
+        // given
+        UserRequest.JoinDTO requestDTO = new UserRequest.JoinDTO();
+        requestDTO.setEmail("ssarmango@nate.com");
+        requestDTO.setPassword("meta1234");
+        requestDTO.setUsername("ssarmango");
+        String requestBody = om.writeValueAsString(requestDTO);
+
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/join")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("responseBody=" + responseBody);
+
+        // then
+        result.andExpect(MockMvcResultMatchers.status().isBadRequest());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value(false));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response").isEmpty());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.error.message").value("영문, 숫자, 특수문자가 포함되어야하고 공백이 포함될 수 없습니다.:password"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.error.status").value(400));
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 - 이름 길이 오류")
+    void join_fail_test3() throws Exception {
+        // given
+        UserRequest.JoinDTO requestDTO = new UserRequest.JoinDTO();
+        requestDTO.setEmail("ssarmango@nate.com");
+        requestDTO.setPassword("meta1234!");
+        requestDTO.setUsername("ssar");
+        String requestBody = om.writeValueAsString(requestDTO);
+
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/join")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("responseBody=" + responseBody);
+
+        // then
+        result.andExpect(MockMvcResultMatchers.status().isBadRequest());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value(false));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response").isEmpty());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.error.message").value("8에서 45자 이내여야 합니다.:username"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.error.status").value(400));
+    }
+
+    @Test
+    @DisplayName("로그인 성공")
+    void login_success_test() throws Exception {
         // given
         UserRequest.LoginDTO loginDTO = new UserRequest.LoginDTO();
         loginDTO.setEmail("ssar@nate.com");
@@ -98,8 +175,8 @@ public class UserRestControllerTest {
         );
         String responseBody = result.andReturn().getResponse().getContentAsString();
         String responseHeader = result.andReturn().getResponse().getHeader(JWTProvider.HEADER);
-        System.out.println("테스트 : "+responseBody);
-        System.out.println("테스트 : "+responseHeader);
+        System.out.println("responseBody=" + responseBody);
+        System.out.println("responseHeader=" + responseHeader);
 
         // then
         result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
@@ -107,9 +184,70 @@ public class UserRestControllerTest {
     }
 
     @Test
-    public void length_test(){
-        String value = "Bearer eyJ0eX";
-        System.out.println(value.substring(0,6));
+    @DisplayName("로그인 실패 - 이메일 형식 오류")
+    void login_fail_test1() throws Exception {
+        // given
+        UserRequest.LoginDTO loginDTO = new UserRequest.LoginDTO();
+        loginDTO.setEmail("ssarnate.com");
+        loginDTO.setPassword("meta1234!");
+        User user = User.builder().id(1).roles("ROLE_USER").build();
+        String requestBody = om.writeValueAsString(loginDTO);
+
+        // stub
+        String jwt = JWTProvider.create(user);
+        Mockito.when(userService.login(any())).thenReturn(jwt);
+
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/login")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        String responseHeader = result.andReturn().getResponse().getHeader(JWTProvider.HEADER);
+        System.out.println("responseBody=" + responseBody);
+        System.out.println("responseHeader=" + responseHeader);
+
+        // then
+        result.andExpect(MockMvcResultMatchers.status().isBadRequest());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value(false));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response").isEmpty());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.error.message").value("이메일 형식으로 작성해주세요:email"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.error.status").value(400));
     }
 
+    @Test
+    @DisplayName("로그인 실패 - 비밀번호 형식 오류")
+    void login_fail_test2() throws Exception {
+        // given
+        UserRequest.LoginDTO loginDTO = new UserRequest.LoginDTO();
+        loginDTO.setEmail("ssar@nate.com");
+        loginDTO.setPassword("meta1234");
+        User user = User.builder().id(1).roles("ROLE_USER").build();
+        String requestBody = om.writeValueAsString(loginDTO);
+
+        // stub
+        String jwt = JWTProvider.create(user);
+        Mockito.when(userService.login(any())).thenReturn(jwt);
+
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/login")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        String responseHeader = result.andReturn().getResponse().getHeader(JWTProvider.HEADER);
+        System.out.println("responseBody=" + responseBody);
+        System.out.println("responseHeader=" + responseHeader);
+
+        // then
+        result.andExpect(MockMvcResultMatchers.status().isBadRequest());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value(false));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response").isEmpty());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.error.message").value("영문, 숫자, 특수문자가 포함되어야하고 공백이 포함될 수 없습니다.:password"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.error.status").value(400));
+    }
 }
