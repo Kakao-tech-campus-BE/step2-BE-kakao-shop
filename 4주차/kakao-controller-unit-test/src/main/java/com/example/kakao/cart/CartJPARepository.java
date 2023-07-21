05afc6c4aadc68b606a6b6cd8ca2ffbd55d932e9
yuseonkim <yusun715@icloud.com>
2023-07-21 10:21:54 +0900

    4주차 과제 수정

--- 4주차/kakao-controller-unit-test/src/main/java/com/example/kakao/cart/CartJPARepository.java
+++ 4주차/kakao-controller-unit-test/src/main/java/com/example/kakao/cart/CartJPARepository.java
@@ -2,6 +2,7 @@ package com.example.kakao.cart;
 
 import com.example.kakao.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -9,11 +10,11 @@ import java.util.Optional;
 
 
 public interface CartJPARepository extends JpaRepository<Cart, Integer> {
-
+    @Query("SELECT c FROM Cart c JOIN fetch c.option o JOIN fetch c.user u JOIN fetch o.product p WHERE c.user.id = :userId")
     List<Cart> findByUserId(int userId);
 
-    Cart findByUserIdAndOptionId(@Param("userId")int user_id,@Param("optionId") int option_id);
+    Cart findByUserIdAndOptionId(int userId,int optionId);
 
-    Optional<Cart> deleteByUserId(@Param("userId")int user_id);
+    Optional<Cart> deleteByUserId(int userId);
 
 }
