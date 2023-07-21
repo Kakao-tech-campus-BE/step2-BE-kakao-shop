1f6f509253fdb1e0eb6c5d946764930011bb3b73
cosmos_1885 <dmsrkql104321@gmail.com>
2023-07-21 20:40:17 +0900

    feat: create ProductRestControllerTest

---
+++ Week4/kakao-controller-unit-test/src/test/java/com/example/kakao/product/ProductRestControllerTest.java
@@ -0,0 +1,96 @@
+package com.example.kakao.product;
+
+import com.example.kakao._core.security.SecurityConfig;
+import com.example.kakao._core.utils.FakeStore;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@Import({
+        FakeStore.class,
+        SecurityConfig.class
+})
+@WebMvcTest(controllers = {ProductRestController.class})
+public class ProductRestControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Test
+    public void findAll_test() throws Exception {
+        // given
+        String page = "1";
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .get("/products")
+                        .param("page", page)
+        );
+
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트: " + responseBody);
+
+        // then
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response[0].id").value(10));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response[0].productName").value("통영 홍 가리비 2kg, 2세트 구매시 1kg 추가증정"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response[0].description").value(""));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response[0].image").value("/images/10.jpg"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response[0].price").value(8900));
+    }
+
+    @Test
+    public void finaById_test() throws Exception {
+        // given
+        int id = 1;
+
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .get("/products/" + id)
+        );
+
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트: " + responseBody);
+
+        // then
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.id").value(1));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.description").value(""));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.image").value("/images/1.jpg"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.price").value(1000));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.starCount").value(5));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.options[0].id").value(1));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.options[0].optionName").value("01. 슬라이딩 지퍼백 크리스마스에디션 4종"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.options[0].price").value(10000));
+    }
+
+    @Test
+    public void finaById_fail_test() throws Exception {
+        // given
+        int id = 100;
+
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .get("/products/" + id)
+        );
+
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트: " + responseBody);
+
+        // then
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("false"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.error.message").value("해당 상품을 찾을 수 없습니다:100"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.error.status").value(404));
+    }
+}
