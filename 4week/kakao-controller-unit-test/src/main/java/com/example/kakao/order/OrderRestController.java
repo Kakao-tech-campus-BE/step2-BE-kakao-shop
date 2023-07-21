96f664da495f11f46690f21d4ce9451ffff14704
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-07-21 10:45:50 +0900

    Update OrderRestController.java
    제공해주셨던 order에서 order가 없는 경우와 해당 order내에 선택된 item이 없는 경우를 에러 핸들링 해보았습니다.

--- 4week/kakao-controller-unit-test/src/main/java/com/example/kakao/order/OrderRestController.java
+++ 4week/kakao-controller-unit-test/src/main/java/com/example/kakao/order/OrderRestController.java
@@ -1,5 +1,7 @@
 package com.example.kakao.order;
 
+import com.example.kakao._core.errors.GlobalExceptionHandler;
+import com.example.kakao._core.errors.exception.Exception404;
 import com.example.kakao._core.security.CustomUserDetails;
 import com.example.kakao._core.utils.ApiUtils;
 import com.example.kakao._core.utils.FakeStore;
@@ -11,6 +13,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -18,22 +21,46 @@ import java.util.List;
 @RestController
 public class OrderRestController {
 
+    private final GlobalExceptionHandler globalExceptionHandler;
     private final FakeStore fakeStore;
 
     // (기능12) 결재
     @PostMapping("/orders/save")
-    public ResponseEntity<?> save(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        Order order = fakeStore.getOrderList().get(0);
-        List<Item> itemList = fakeStore.getItemList();
-        OrderResponse.FindByIdDTO responseDTO = new OrderResponse.FindByIdDTO(order, itemList);
-        return ResponseEntity.ok(ApiUtils.success(responseDTO));
+    public ResponseEntity<?> save(@AuthenticationPrincipal CustomUserDetails userDetails, HttpServletRequest request) {
+        try {
+            List<Order> orders = fakeStore.getOrderList();
+            //order이 비었거나 null이거나
+            if(orders==null || orders.isEmpty()){
+                throw new Exception404("결제를 진행할 주문이 존재하지 않습니다.");
+            }
+            List<Item> itemList = fakeStore.getItemList();
+            if (itemList==null){
+                throw new Exception404("주문 내에 주문상품이 존재하지 않습니다.");
+            }
+            OrderResponse.FindByIdDTO responseDTO = new OrderResponse.FindByIdDTO(orders.get(0), itemList);
+            System.out.println("테스트: " + responseDTO);
+            return ResponseEntity.ok(ApiUtils.success(responseDTO));
+        } catch (RuntimeException e) {
+            return globalExceptionHandler.handle(e,request);
+        }
     }
 
     // (기능13) 주문 결과 확인
     @GetMapping("/orders/{id}")
     public ResponseEntity<?> findById(@PathVariable int id) {
-        Order order = fakeStore.getOrderList().get(id-1);
+        // 1. 더미데이터 가져오기
+        Order order = fakeStore.getOrderList().stream().filter(o -> o.getId() == id).findFirst().orElse(null);
+        // 2. null 체크
+        if(order==null){
+            Exception404 ex = new Exception404("해당 주문을 찾을 수 없습니다:"+id);
+            return new ResponseEntity<>(
+                    ex.body(),
+                    ex.status()
+            );
+        }
+
         List<Item> itemList = fakeStore.getItemList();
+
         OrderResponse.FindByIdDTO responseDTO = new OrderResponse.FindByIdDTO(order, itemList);
         return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
     }
