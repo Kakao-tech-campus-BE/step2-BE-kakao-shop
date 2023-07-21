2402fe8668ebb6b1e1cb0e744dcde209f0054d7d
jinc20 <wh5971@pusan.ac.kr>
2023-07-21 11:14:23 +0900

    test: Cart, Product 컨트롤러 테스트 stub 추가

--- 4week/kakao-controller-unit-test/src/test/java/com/example/kakao/cart/CartRestControllerTest.java
+++ 4week/kakao-controller-unit-test/src/test/java/com/example/kakao/cart/CartRestControllerTest.java
@@ -3,11 +3,16 @@ package com.example.kakao.cart;
 import com.example.kakao._core.security.JWTProvider;
 import com.example.kakao._core.security.SecurityConfig;
 import com.example.kakao._core.utils.FakeStore;
+import com.example.kakao.product.Product;
+import com.example.kakao.product.option.Option;
+import com.example.kakao.user.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,9 +25,11 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import javax.persistence.EntityManager;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.core.IsNull.nullValue;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @Import({
         FakeStore.class,
@@ -30,12 +37,41 @@ import static org.hamcrest.core.IsNull.nullValue;
 })
 @WebMvcTest(controllers = {CartRestController.class})
 public class CartRestControllerTest {
+    @MockBean
+    private FakeStore fakeStore;
 
     @Autowired
     private MockMvc mvc;
 
     @Autowired
     private ObjectMapper om;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        // stub
+        User user = new User(1, "ssar@nate.com", "1234", "ssar", "USER");
+
+        List<Product> products = Arrays.asList(
+                new Product(1, "기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전", "", "/images/1.jpg", 1000),
+                new Product(2, "[황금약단밤 골드]2022년산 햇밤 칼집밤700g외/군밤용/생율", "", "/images/2.jpg", 2000)
+        );
+        List<Option> options = Arrays.asList(
+                new Option(1, products.get(0), "01. 슬라이딩 지퍼백 크리스마스에디션 4종", 10000),
+                new Option(2, products.get(0), "02. 슬라이딩 지퍼백 플라워에디션 5종", 10900),
+                new Option(3, products.get(0), "고무장갑 베이지 S(소형) 6팩", 9900),
+                new Option(4, products.get(0), "뽑아쓰는 키친타올 130매 12팩", 16900),
+                new Option(5, products.get(0), "2겹 식빵수세미 6매", 8900),
+                new Option(6, products.get(1), "22년산 햇단밤 700g(한정판매)", 9900),
+                new Option(7, products.get(1), "22년산 햇단밤 1kg(한정판매)", 14500),
+                new Option(8, products.get(1), "밤깎기+다회용 구이판 세트", 5500)
+        );
+        Mockito.when(fakeStore.getCartList()).thenReturn(
+                Arrays.asList(
+                        new Cart(1, user, options.get(2), 3, options.get(2).getPrice()*3),
+                        new Cart(2, user, options.get(6), 1, options.get(5).getPrice()*1)
+                )
+        );
+    }
 
     @WithMockUser(username = "ssar@nate.com", roles = "USER")
     @Test
@@ -60,21 +96,21 @@ public class CartRestControllerTest {
                         .content(requestBody)
                         .contentType(MediaType.APPLICATION_JSON)
         );
-
-        HttpStatus status = HttpStatus.valueOf(result.andReturn().getResponse().getStatus());
-        String errorMessage = result.andReturn().getResponse().getErrorMessage();
-        String responseBody = result.andReturn().getResponse().getContentAsString();
-        System.out.println("테스트 Status: " + status);
-        System.out.println("테스트 Error Message: " + errorMessage);
-        System.out.println("테스트 Response Body: " + responseBody);
+        result.andDo(print());
 
         // then
         result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
+
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].cartId").value(1));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].optionId").value(1));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].optionName").value("01. 슬라이딩 지퍼백 크리스마스에디션 4종"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].optionId").value(3));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].optionName").value("고무장갑 베이지 S(소형) 6팩"));
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].quantity").value(10));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].price").value(100000));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[0].price").value(9900*10));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[1].cartId").value(2));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[1].optionId").value(7));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[1].optionName").value("22년산 햇단밤 1kg(한정판매)"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[1].quantity").value(10));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.carts[1].price").value(14500*10));
 
         result.andExpect(MockMvcResultMatchers.jsonPath("$.error").value(nullValue()));
     }
@@ -102,12 +138,7 @@ public class CartRestControllerTest {
                         .content(requestBody)
                         .contentType(MediaType.APPLICATION_JSON)
         );
-        HttpStatus status = HttpStatus.valueOf(result.andReturn().getResponse().getStatus());
-        String errorMessage = result.andReturn().getResponse().getErrorMessage();
-        String responseBody = result.andReturn().getResponse().getContentAsString();
-        System.out.println("테스트 Status: " + status);
-        System.out.println("테스트 Error Message: " + errorMessage);
-        System.out.println("테스트 Response Body: " + responseBody);
+        result.andDo(print());
 
         // then
         result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
@@ -124,27 +155,22 @@ public class CartRestControllerTest {
         ResultActions result = mvc.perform(
                 MockMvcRequestBuilders.get("/carts")
         );
-
-        HttpStatus status = HttpStatus.valueOf(result.andReturn().getResponse().getStatus());
-        String errorMessage = result.andReturn().getResponse().getErrorMessage();
-        String responseBody = result.andReturn().getResponse().getContentAsString();
-        System.out.println("테스트 Status: " + status);
-        System.out.println("테스트 Error Message: " + errorMessage);
-        System.out.println("테스트 Response Body: " + responseBody);
+        result.andDo(print());
 
         // then
         result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
 
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response.products[0].id").value(1));
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response.products[0].carts[0].id").value(1));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.products[0].carts[0].option.id").value(1));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.products[0].carts[0].quantity").value(5));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.products[0].carts[0].price").value(10000*5));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.products[0].carts[0].option.id").value(3));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.products[0].carts[0].quantity").value(3));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.products[0].carts[0].price").value(9900*3));
 
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.products[0].carts[1].id").value(2));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.products[0].carts[1].option.id").value(2));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.products[0].carts[1].quantity").value(5));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.products[0].carts[1].price").value(10900*5));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.products[1].id").value(2));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.products[1].carts[0].id").value(2));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.products[1].carts[0].option.id").value(7));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.products[1].carts[0].quantity").value(1));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.products[1].carts[0].price").value(14500*1));
 
         result.andExpect(MockMvcResultMatchers.jsonPath("$.error").value(nullValue()));
     }
@@ -160,13 +186,7 @@ public class CartRestControllerTest {
                 MockMvcRequestBuilders
                         .post("/carts/clear")
         );
-
-        HttpStatus status = HttpStatus.valueOf(result.andReturn().getResponse().getStatus());
-        String errorMessage = result.andReturn().getResponse().getErrorMessage();
-        String responseBody = result.andReturn().getResponse().getContentAsString();
-        System.out.println("테스트 Status: " + status);
-        System.out.println("테스트 Error Message: " + errorMessage);
-        System.out.println("테스트 Response Body: " + responseBody);
+        result.andDo(print());
 
         // then
         result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
--- 4week/kakao-controller-unit-test/src/test/java/com/example/kakao/product/ProductRestControllerTest.java
+++ 4week/kakao-controller-unit-test/src/test/java/com/example/kakao/product/ProductRestControllerTest.java
@@ -5,8 +5,11 @@ import com.example.kakao._core.errors.GlobalExceptionHandler;
 import com.example.kakao._core.security.SecurityConfig;
 import com.example.kakao._core.utils.FakeStore;
 import com.example.kakao.log.ErrorLogJPARepository;
+import com.example.kakao.product.option.Option;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,12 +21,15 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.core.IsNull.nullValue;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @Import({
         GlobalExceptionHandler.class,
         SecurityConfig.class,
-        FakeStore.class
 })
 @WebMvcTest(controllers = {ProductRestController.class})
 public class ProductRestControllerTest {
@@ -31,28 +37,49 @@ public class ProductRestControllerTest {
     @MockBean
     private ErrorLogJPARepository errorLogJPARepository;
 
+    @MockBean
+    private FakeStore fakeStore;
+
     @Autowired
     private MockMvc mvc;
 
     @Autowired
     ObjectMapper om;
 
+    @BeforeEach
+    public void setUp() throws Exception {
+        // stub
+        List<Product> products = Arrays.asList(
+                new Product(1, "기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전", "", "/images/1.jpg", 1000),
+                new Product(2, "[황금약단밤 골드]2022년산 햇밤 칼집밤700g외/군밤용/생율", "", "/images/2.jpg", 2000)
+        );
+        Mockito.when(fakeStore.getProductList()).thenReturn(products);
+
+        Mockito.when(fakeStore.getOptionList()).thenReturn(
+                Arrays.asList(
+                        new Option(1, products.get(0), "01. 슬라이딩 지퍼백 크리스마스에디션 4종", 10000),
+                        new Option(2, products.get(0), "02. 슬라이딩 지퍼백 플라워에디션 5종", 10900),
+                        new Option(3, products.get(0), "고무장갑 베이지 S(소형) 6팩", 9900),
+                        new Option(4, products.get(0), "뽑아쓰는 키친타올 130매 12팩", 16900),
+                        new Option(5, products.get(0), "2겹 식빵수세미 6매", 8900),
+                        new Option(6, products.get(1), "22년산 햇단밤 700g(한정판매)", 9900),
+                        new Option(7, products.get(1), "22년산 햇단밤 1kg(한정판매)", 14500),
+                        new Option(8, products.get(1), "밤깎기+다회용 구이판 세트", 5500)
+                        )
+        );
+    }
+
     @Test
     public void findByAll_test() throws Exception {
         // given
+
 
         // when
         ResultActions result = mvc.perform(
                 MockMvcRequestBuilders
                         .get("/products")
         );
-
-        HttpStatus status = HttpStatus.valueOf(result.andReturn().getResponse().getStatus());
-        String errorMessage = result.andReturn().getResponse().getErrorMessage();
-        String responseBody = result.andReturn().getResponse().getContentAsString();
-        System.out.println("테스트 : " + status);
-        System.out.println("테스트: " + errorMessage);
-        System.out.println("테스트: " + responseBody);
+        result.andDo(print());
 
         // then
         result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
@@ -63,16 +90,6 @@ public class ProductRestControllerTest {
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response[1].id").value(2));
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response[1].productName").value("[황금약단밤 골드]2022년산 햇밤 칼집밤700g외/군밤용/생율"));
         result.andExpect(MockMvcResultMatchers.jsonPath("$.response[1].price").value(2000));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.response[2].id").value(3));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.response[2].productName").value("삼성전자 JBL JR310 외 어린이용/성인용 헤드셋 3종!"));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.response[2].price").value(30000));
-        // 4~7번 product는 생략, 9번 product까지 페이지에 응답
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.response[7].id").value(8));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.response[7].productName").value("제나벨 PDRN 크림 2개. 피부보습/진정 케어"));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.response[7].price").value(25900));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.response[8].id").value(9));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.response[8].productName").value("플레이스테이션 VR2 호라이즌 번들. 생생한 몰입감"));
-        result.andExpect(MockMvcResultMatchers.jsonPath("$.response[8].price").value(797000));
 
         result.andExpect(MockMvcResultMatchers.jsonPath("$.error").value(nullValue()));
     }
@@ -80,19 +97,12 @@ public class ProductRestControllerTest {
     @Test
     public void findById_test() throws Exception {
         // given
-
-
         // when
         ResultActions result = mvc.perform(
                 MockMvcRequestBuilders
                         .get("/products/{id}", 1)
         );
-        HttpStatus status = HttpStatus.valueOf(result.andReturn().getResponse().getStatus());
-        String errorMessage = result.andReturn().getResponse().getErrorMessage();
-        String responseBody = result.andReturn().getResponse().getContentAsString();
-        System.out.println("테스트 : " + status);
-        System.out.println("테스트: " + errorMessage);
-        System.out.println("테스트: " + responseBody);
+        result.andDo(print());
 
         // then
         result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
