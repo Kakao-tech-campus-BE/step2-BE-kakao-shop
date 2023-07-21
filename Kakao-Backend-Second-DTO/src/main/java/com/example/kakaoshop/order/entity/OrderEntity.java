354598c1d186fca8ba93f7f25c16b35b2c60c575
kssumin <201566@jnu.ac.kr>
2023-07-21 18:20:55 +0900

    feat : order, orderItem entity 생성

---
+++ Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/order/entity/OrderEntity.java
@@ -0,0 +1,33 @@
+package com.example.kakaoshop.order.entity;
+
+import com.example.kakaoshop.user.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "order_entity")
+public class OrderEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    private Long orderDate;
+    private boolean isCancel;
+    private int totalPrice;
+
+    @Builder
+    public OrderEntity(Long orderId, User user, Long orderDate, boolean isCancel) {
+        this.orderId = orderId;
+        this.user = user;
+        this.orderDate = orderDate;
+        this.isCancel = isCancel;
+    }
+}
---
+++ Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/order/entity/OrderItemEntity.java
@@ -0,0 +1,37 @@
+package com.example.kakaoshop.order.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "order_item_entity")
+public class OrderItemEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderItemId;
+    private Long orderId;
+    private Long productId;
+    @Column(length = 30, nullable = false)
+    private String optionName;
+    @Column(length = 20, nullable = false)
+    private String productName;
+    private int price;
+    private int quantity;
+
+    @Builder
+    public OrderItemEntity(Long orderItemId, Long orderId, Long productId, String optionName, String productName, int price, int quantity) {
+        this.orderItemId = orderItemId;
+        this.orderId = orderId;
+        this.productId = productId;
+        this.optionName = optionName;
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
