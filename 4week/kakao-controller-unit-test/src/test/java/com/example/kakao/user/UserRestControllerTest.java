84a30a596284333e0e5e267ae20c0aab7a9783f8
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-07-21 10:52:47 +0900

    Update UserRestControllerTest.java
    제공해주셨던 코드에 join_test 메서드에 stub을 추가해보았고(결과는 다르지 않지만 의도를 생각하면 추가해야된다고 생각하였습니다.)
    로그인 성능 테스트에 when 을 사용해보았고
    로그인 실패 테스트 잘못된 패스워드의 경우를 넣어보았고 추가적으로 이메일 중복 테스트를 통과한 경우와 그렇지 않은 경우에 대해서도 추가적으로 테스트를 해보았습니다.

--- 4week/kakao-controller-unit-test/src/test/java/com/example/kakao/user/UserRestControllerTest.java
+++ 4week/kakao-controller-unit-test/src/test/java/com/example/kakao/user/UserRestControllerTest.java
@@ -1,15 +1,19 @@
 package com.example.kakao.user;
 
 import com.example.kakao._core.errors.GlobalExceptionHandler;
+import com.example.kakao._core.errors.exception.Exception400;
 import com.example.kakao._core.security.JWTProvider;
 import com.example.kakao._core.security.SecurityConfig;
+import com.example.kakao._core.utils.FakeStore;
 import com.example.kakao.log.ErrorLogJPARepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -20,15 +24,21 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.Optional;
+
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 // GlobalExceptionHandler 와 UserRestController를 SpringContext에 등록합니다.
 
 @Import({
         SecurityConfig.class,
-        GlobalExceptionHandler.class
+        GlobalExceptionHandler.class,
+        FakeStore.class
 })
 @WebMvcTest(controllers = {UserRestController.class})
 public class UserRestControllerTest {
@@ -37,7 +47,8 @@ public class UserRestControllerTest {
     // 해당 객체는 SpringContext에 등록됩니다.
     @MockBean
     private UserService userService;
-
+    @MockBean
+    private UserJPARepository userJPARepository;
     @MockBean
     private ErrorLogJPARepository errorLogJPARepository;
 
@@ -49,8 +60,10 @@ public class UserRestControllerTest {
     @Autowired
     private ObjectMapper om;
 
-    @Test
-    public void t1(){}
+    @BeforeEach
+    public void setUp(){
+
+    }
 
     @Test
     public void join_test() throws Exception {
@@ -60,6 +73,10 @@ public class UserRestControllerTest {
         requestDTO.setPassword("meta1234!");
         requestDTO.setUsername("ssarmango");
         String requestBody = om.writeValueAsString(requestDTO);
+
+        //login과 마찬가지로 stub이 추가되어야한다고 생각이 듭니다
+        //stub
+        Mockito.doNothing().when(userService).join(any());
 
         // when
         ResultActions result = mvc.perform(
@@ -76,6 +93,7 @@ public class UserRestControllerTest {
         result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
     }
 
+    @DisplayName("로그인 성공 테스트")
     @Test
     public void login_test() throws Exception {
         // given
@@ -87,7 +105,7 @@ public class UserRestControllerTest {
 
         // stub
         String jwt = JWTProvider.create(user);
-        Mockito.when(userService.login(any())).thenReturn(jwt);
+        when(userService.login(any())).thenReturn(jwt);
 
         // when
         ResultActions result = mvc.perform(
@@ -105,6 +123,91 @@ public class UserRestControllerTest {
         result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
         Assertions.assertTrue(jwt.startsWith(JWTProvider.TOKEN_PREFIX));
     }
+
+
+    @DisplayName("로그인 실패 테스트: 잘못된 패스워드")
+    @Test
+    public void login_wrongPassword_fail_test() throws Exception {
+        // given
+        UserRequest.LoginDTO loginDTO = new UserRequest.LoginDTO();
+        loginDTO.setEmail("ssar@nate.com");
+        loginDTO.setPassword("mete12!");
+        String requestBody = om.writeValueAsString(loginDTO);
+
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/login")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // then
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("false"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.error.message").value("8에서 20자 이내여야 합니다.:password"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.error.status").value(400));
+    }
+
+// ... [Other test methods will follow the same structure]
+
+    @DisplayName("이메일 중복확인 테스트")
+    @Test
+    public void check_sameEmail_test() throws Exception {
+        // given
+        UserRequest.EmailCheckDTO emailCheckDTO = new UserRequest.EmailCheckDTO();
+        emailCheckDTO.setEmail("dummy1@nate.com");
+        String requestBody = om.writeValueAsString(emailCheckDTO);
+
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/check")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // then
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response").value("이메일 사용 가능합니다."));
+    }
+
+
+    @DisplayName("이메일 중복확인에러 테스트")
+    @Test
+    public void check_sameEmail_fail_test() throws Exception {
+        // given
+        String duplicateEmail = "dummy1";
+        UserRequest.EmailCheckDTO emailCheckDTO = new UserRequest.EmailCheckDTO();
+        emailCheckDTO.setEmail(duplicateEmail);
+        String requestBody = om.writeValueAsString(emailCheckDTO);
+
+
+        doThrow(new Exception400("동일한 이메일이 존재합니다 : " + duplicateEmail))
+                .when(userService).sameCheckEmail(duplicateEmail);
+
+        // check 에 post 보내기
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/check")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // then
+        result.andExpect(MockMvcResultMatchers.status().isBadRequest()) // Expecting 400 error status
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value("false"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.error.message").value("동일한 이메일이 존재합니다 : " + duplicateEmail));
+    }
+
+// ... [Continue rewriting the remaining test methods in a similar style]
+
 
     @Test
     public void length_test(){
