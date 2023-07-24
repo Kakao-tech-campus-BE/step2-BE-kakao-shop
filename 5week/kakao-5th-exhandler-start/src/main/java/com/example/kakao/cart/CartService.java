bce6684faf6bc6fe9e3db8919a27cbdbfaf6f6ba
jminkkk <minl741@naver.com>
2023-07-24 16:44:11 +0900

    장바구니 담기 구현

--- 5week/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/CartService.java
+++ 5week/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/CartService.java
@@ -1,2 +1,39 @@
-package com.example.kakao.cart;public class CartService {
+package com.example.kakao.cart;
+
+import com.example.kakao._core.errors.exception.Exception404;
+import com.example.kakao.product.option.Option;
+import com.example.kakao.product.option.OptionJPARepository;
+import com.example.kakao.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+    private final OptionJPARepository optionJPARepository;
+    private final CartJPARepository cartJPARepository;
+    @Transactional
+    public void addCartList(List<CartRequest.SaveDTO> requestDTOs, User sessionUser) {
+        // TODO: 1. 동일한 옵션이 들어오면 예외처리
+        // [ { optionId:1, quantity:5 }, { optionId:1, quantity:10 } ] 와 같이 요청이 들어오면?
+        // optionId:1, quantity:15로 처리되는 것이 맞
+
+        // TODO: 2. cartJPARepository.findByOptionIdAndUserId() 조회 -> 존재하면 장바구니에 수량을 추가하는 업데이트를 해야함. (더티체킹하기)
+
+        // TODO: 3. [2번이 아니라면] 유저의 장바구니에 담기
+        for (CartRequest.SaveDTO requestDTO : requestDTOs) {
+            int optionId = requestDTO.getOptionId();
+            int quantity = requestDTO.getQuantity();
+            Option optionPS = optionJPARepository.findById(optionId)
+                    .orElseThrow(() -> new Exception404("해당 옵션을 찾을 수 없습니다 : " + optionId));
+            int price = optionPS.getPrice() * quantity;
+            Cart cart = Cart.builder().user(sessionUser).option(optionPS).quantity(quantity).price(price).build();
+            cartJPARepository.save(cart);
+        }
+
+    } // 변경 감지, 더티체킹, flush, 트랜잭션 종료
 }
+
--- 5week/kakao-5th-exhandler-start/src/main/java/com/example/kakao/product/option/OptionJPARepository.java
+++ 5week/kakao-5th-exhandler-start/src/main/java/com/example/kakao/product/option/OptionJPARepository.java
@@ -10,8 +10,11 @@ import java.util.Optional;
 
 public interface OptionJPARepository extends JpaRepository<Option, Integer> {
 
+    // JPQL을 사용하여 select문 에서 필요없는 join이 되는 현상 해결 -> 2번 설렉
+    @Query("select o from Option o where o.product.id = :productId")
     List<Option> findByProductId(@Param("productId") int productId);
 
+    // join을 통해 설렉트문 자체를 1번만 실행
     @Query("select o from Option o join fetch o.product where o.product.id = :productId")
     List<Option> findByProductIdJoinProduct(@Param("productId") int productId);
 }
