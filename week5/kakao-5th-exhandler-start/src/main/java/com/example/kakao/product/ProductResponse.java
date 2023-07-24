1a11333bc44e9954b40bc8d2d596fcb233a4cd3e
jagaldol <yoy06056@pusan.ac.kr>
2023-07-24 15:37:23 +0900

    feat: (기능1) 상품 목록 보기 구현

--- week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/product/ProductResponse.java
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/product/ProductResponse.java
@@ -1,6 +1,25 @@
 package com.example.kakao.product;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class ProductResponse {
+    @Getter
+    @Setter
+    public static class FindAllDTO {
 
+        private int id;
+        private String productName;
+        private String description;
+        private String image;
+        private int price;
 
+        public FindAllDTO(Product product) {
+            this.id = product.getId();
+            this.productName = product.getProductName();
+            this.description = product.getDescription();
+            this.image = product.getImage();
+            this.price = product.getPrice();
+        }
+    }
 }
