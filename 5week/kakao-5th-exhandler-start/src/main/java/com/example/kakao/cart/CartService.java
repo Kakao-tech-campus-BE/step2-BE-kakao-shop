54c70396e24f1eb3ae346bfdd68e3d1675968adf
rnjsgo <rnjsgo2008@naver.com>
2023-07-24 16:19:22 +0900

    feat : 5주차 기능구현
    글로벌 익셉션 핸들러, AOP 적용, 기능 1,2 구현

---
+++ 5week/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/CartService.java
@@ -0,0 +1,49 @@
+package com.example.kakao.cart;
+
+import com.example.kakao._core.errors.exception.Exception400;
+import com.example.kakao._core.errors.exception.Exception404;
+import com.example.kakao._core.errors.exception.Exception500;
+import com.example.kakao.product.option.Option;
+import com.example.kakao.product.option.OptionJPARepository;
+import com.example.kakao.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class CartService {
+
+    private final CartJPARepository cartJPARepository;
+    private final OptionJPARepository optionJPARepository;
+
+    @Transactional
+    public void addCartList(List<CartRequest.SaveDTO> requestDTOs, User sessionUser) {
+        // 1. 동일한 옵션이 들어오면 예외처리
+        // [ { optionId:1, quantity:5 }, { optionId:1, quantity:10 } ]
+
+        // 2. cartJPARepository.findByOptionIdAndUserId() 조회 -> 존재하면 장바구니에 수량을 추가하는 업데이트를 해야함. (더티체킹하기)
+
+        // 3. [2번이 아니라면] 유저의 장바구니에 담기
+        for (CartRequest.SaveDTO requestDTO : requestDTOs) {
+            int optionId = requestDTO.getOptionId();
+            int quantity = requestDTO.getQuantity();
+            Option optionPS = optionJPARepository.findById(optionId)
+                    .orElseThrow(() -> new Exception404("해당 옵션을 찾을 수 없습니다 : " + optionId));
+            int price = optionPS.getPrice() * quantity;
+            Cart cart = Cart.builder().user(sessionUser).option(optionPS).quantity(quantity).price(price).build();
+            cartJPARepository.save(cart);
+        }
+
+
+    }
+
+
+}
