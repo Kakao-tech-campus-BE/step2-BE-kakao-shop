b5c18daa5ffd5d98b54f987857785884248eb3c3
INUK-ai <arheon2469@gmail.com>
2023-07-24 15:58:31 +0900

    feat: 기능 2
    - OptionJPARepository
        - JPQL 수정

--- week5/kakao-5th-exhandler-start-main/src/main/java/com/example/kakao/product/option/OptionJPARepository.java
+++ week5/kakao-5th-exhandler-start-main/src/main/java/com/example/kakao/product/option/OptionJPARepository.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 
 public interface OptionJPARepository extends JpaRepository<Option, Integer> {
 
+    @Query("select o from Option o where o.product.id = :productId")
     List<Option> findByProductId(@Param("productId") int productId);
 
     // findById_select_product_lazy_error_fix_test
