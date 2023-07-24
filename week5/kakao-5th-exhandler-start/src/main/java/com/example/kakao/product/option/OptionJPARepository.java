35c630856e85ad39c389f490cf800276c6aedf0a
jagaldol <yoy06056@pusan.ac.kr>
2023-07-24 16:04:31 +0900

    feat: 상품 상세 조회(옵션 포함) 구현
    v2의 경우 쿼리하나로 해결됨

--- week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/product/option/OptionJPARepository.java
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/product/option/OptionJPARepository.java
@@ -10,10 +10,10 @@ import java.util.Optional;
 
 public interface OptionJPARepository extends JpaRepository<Option, Integer> {
 
+    @Query("SELECT o FROM Option o WHERE o.product.id = :productId")
     List<Option> findByProductId(@Param("productId") int productId);
-    Optional<Option> findById(int id);
 
-    // findById_select_product_lazy_error_fix_test
+
     @Query("select o from Option o join fetch o.product where o.product.id = :productId")
-    List<Option> mFindByProductId(@Param("productId") int productId);
+    List<Option> findByProductIdJoinProduct(@Param("productId") int productId);
 }
