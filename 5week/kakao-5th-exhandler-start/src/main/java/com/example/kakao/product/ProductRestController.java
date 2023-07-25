ba9de831126223fcf5fc7da83b6ed4bd61b5cf35
jinc20 <wh5971@pusan.ac.kr>
2023-07-25 16:05:44 +0900

    init: Init 5week assignment

---
+++ 5week/kakao-5th-exhandler-start/src/main/java/com/example/kakao/product/ProductRestController.java
@@ -0,0 +1,21 @@
+package com.example.kakao.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class ProductRestController {
+
+    // (기능1) 전체 상품 목록 조회 (페이징 9개씩)
+    // /products
+    public void findAll() {
+
+    }
+
+    // (기능2) 개별 상품 상세 조회
+    // /products/{id}
+    public void findById() {
+
+    }
+}
