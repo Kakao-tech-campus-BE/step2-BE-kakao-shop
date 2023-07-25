c2972dcca37fb751376c5def7952afcefd26cfe0
rizingblare <rizingblare@gmail.com>
2023-07-25 16:53:58 +0900

    Global exception complete

--- week5/src/main/java/com/example/kakao/user/UserRestController.java
+++ week5/src/main/java/com/example/kakao/user/UserRestController.java
@@ -26,6 +26,7 @@ public class UserRestController {
     // (기능3) 이메일 중복체크
     @PostMapping("/check")
     public ResponseEntity<?> check(@RequestBody @Valid UserRequest.EmailCheckDTO emailCheckDTO, Errors errors) {
+        // 부가기능
         if (errors.hasErrors()) {
             List<FieldError> fieldErrors = errors.getFieldErrors();
             Exception400 ex = new Exception400(fieldErrors.get(0).getDefaultMessage() + ":" + fieldErrors.get(0).getField());
@@ -34,6 +35,8 @@ public class UserRestController {
                     ex.status()
             );
         }
+        
+        // 핵심로직
         userService.sameCheckEmail(emailCheckDTO.getEmail());
         return ResponseEntity.ok(ApiUtils.success(null));
     }
@@ -41,6 +44,7 @@ public class UserRestController {
     // (기능4) 회원가입
     @PostMapping("/join")
     public ResponseEntity<?> join(@RequestBody @Valid UserRequest.JoinDTO requestDTO, Errors errors, HttpServletRequest request) {
+        // 부가기능
         if (errors.hasErrors()) {
             List<FieldError> fieldErrors = errors.getFieldErrors();
             Exception400 ex = new Exception400(fieldErrors.get(0).getDefaultMessage() + ":" + fieldErrors.get(0).getField());
@@ -49,18 +53,22 @@ public class UserRestController {
                     ex.status()
             );
         }
-        try {
-            userService.join(requestDTO);
-            return ResponseEntity.ok().body(ApiUtils.success(null));
-        } catch (RuntimeException e) {
-            return globalExceptionHandler.handle(e, request);
-        }
+        
+//        try {
+//            userService.join(requestDTO);
+//            return ResponseEntity.ok().body(ApiUtils.success(null));
+//        } catch (RuntimeException e) {
+//            return globalExceptionHandler.handle(e, request);
+//        }
+        // 핵심 로직
+        userService.join(requestDTO);
+        return ResponseEntity.ok().body(ApiUtils.success(null));
     }
 
     // (기능5) 로그인
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody @Valid UserRequest.LoginDTO requestDTO, Errors errors, HttpServletRequest request) {
-
+        // 부가기능
         if (errors.hasErrors()) {
             List<FieldError> fieldErrors = errors.getFieldErrors();
             Exception400 ex = new Exception400(fieldErrors.get(0).getDefaultMessage() + ":" + fieldErrors.get(0).getField());
@@ -70,12 +78,15 @@ public class UserRestController {
             );
         }
 
-        try {
-            String jwt = userService.login(requestDTO);
-            return ResponseEntity.ok().header(JWTProvider.HEADER, jwt).body(ApiUtils.success(null));
-        }catch (RuntimeException e){
-            return globalExceptionHandler.handle(e, request);
-        }
+//        try {
+//            String jwt = userService.login(requestDTO);
+//            return ResponseEntity.ok().header(JWTProvider.HEADER, jwt).body(ApiUtils.success(null));
+//        }catch (RuntimeException e){
+//            return globalExceptionHandler.handle(e, request);
+//        }
+        // 핵심 로직
+        String jwt = userService.login(requestDTO);
+        return ResponseEntity.ok().header(JWTProvider.HEADER, jwt).body(ApiUtils.success(null));
     }
 
     // 로그아웃 사용안함 - 프론트에서 JWT 토큰을 브라우저의 localstorage에서 삭제하면 됨.
