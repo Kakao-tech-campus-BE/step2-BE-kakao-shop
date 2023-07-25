32fc70db558aeab195c9ee0fff7722e869652a9f
kimdonwoo <ehstjstod@gmail.com>
2023-07-25 14:59:34 +0900

    feat : 5주차 과제 장바구니 수정(주문하기)
    - 장바구니 수정(주문하기) -> 예외처리하기 (완료)

--- 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/cart/CartService.java
+++ 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/cart/CartService.java
@@ -13,6 +13,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -67,26 +68,40 @@ public class CartService {
         }
     }
 
-    public CartResponse.FindAllDTO findAll(User user) {
-        List<Cart> cartList = cartJPARepository.findByUserIdOrderByOptionIdAsc(user.getId());
-        // Cart에 담긴 옵션이 3개이면, 2개는 바나나 상품, 1개는 딸기 상품이면 Product는 2개인 것이다.
-        return new CartResponse.FindAllDTO(cartList);
-    }
-
-    public CartResponse.FindAllDTOv2 findAllv2(User user) {
-        List<Cart> cartList = cartJPARepository.findByUserIdOrderByOptionIdAsc(user.getId());
-        return new CartResponse.FindAllDTOv2(cartList);
-    }
-
+    // 5주차 과제 : 장바구니 수정(주문하기) -> 예외처리하기
     @Transactional
     public CartResponse.UpdateDTO update(List<CartRequest.UpdateDTO> requestDTOs, User user) {
         List<Cart> cartList = cartJPARepository.findAllByUserId(user.getId());
 
+        // List<Cart> => List<Integer>
+        List<Integer> cartListIds = cartList.stream().map((o1)->{
+            return o1.getId();
+        }).collect(Collectors.toList());
+
         // 1. 유저 장바구니에 아무것도 없으면 예외처리
+        if(cartListIds.size() == 0 ){
+            throw new Exception404("주문할 장바구니 상품이 없습니다");
+        }
+
 
         // 2. cartId:1, cartId:1 이렇게 requestDTOs에 동일한 장바구니 아이디가 두번 들어오면 예외처리
-
         // 3. 유저 장바구니에 없는 cartId가 들어오면 예외처리
+        Set<Integer> cartIds = new HashSet();
+
+        for (CartRequest.UpdateDTO requestDTO : requestDTOs){
+            int cartId = requestDTO.getCartId(); // 이게 cartList에 존재하는지 체크?
+
+            if (!cartIds.contains(cartId)){
+                cartIds.add(cartId);
+            }else{
+                throw new Exception400("동일한 장바구니 아이디를 주문할 수 없습니다" + cartId);
+            }
+
+            if(!cartListIds.contains(cartId)){
+                throw new Exception400("장바구니에 없는 상품은 주문할 수 없습니다 : " + cartId);
+            }
+
+        }
 
         // 위에 3개를 처리하지 않아도 프로그램은 잘돌아간다. 예를 들어 1번을 처리하지 않으면 for문을 돌지 않고, cartList가 빈배열 []로 정상응답이 나감.
         for (Cart cart : cartList) {
@@ -99,4 +114,17 @@ public class CartService {
 
         return new CartResponse.UpdateDTO(cartList);
     } // 더티체킹
+
+
+    public CartResponse.FindAllDTO findAll(User user) {
+        List<Cart> cartList = cartJPARepository.findByUserIdOrderByOptionIdAsc(user.getId());
+        // Cart에 담긴 옵션이 3개이면, 2개는 바나나 상품, 1개는 딸기 상품이면 Product는 2개인 것이다.
+        return new CartResponse.FindAllDTO(cartList);
+    }
+
+    public CartResponse.FindAllDTOv2 findAllv2(User user) {
+        List<Cart> cartList = cartJPARepository.findByUserIdOrderByOptionIdAsc(user.getId());
+        return new CartResponse.FindAllDTOv2(cartList);
+    }
+
 }
