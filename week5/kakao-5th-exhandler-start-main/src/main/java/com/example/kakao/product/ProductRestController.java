e69a3d4ee359d1768bf705a3e0bdbc577f65f378
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-07-25 11:31:57 +0900

    lift : up service to controller

--- week5/kakao-5th-exhandler-start-main/src/main/java/com/example/kakao/product/ProductRestController.java
+++ week5/kakao-5th-exhandler-start-main/src/main/java/com/example/kakao/product/ProductRestController.java
@@ -1,16 +1,27 @@
 package com.example.kakao.product;
 
+import com.example.kakao._core.utils.ApiUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class ProductRestController {
 
+    private final ProductService productService;
+
     // (기능1) 전체 상품 목록 조회 (페이징 9개씩)
     // /products
-    public void findAll() {
-
+    @GetMapping("/products")
+    public ResponseEntity<?> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page) {
+        List<ProductResponse.FindAllDTO> responseDTOs = productService.findAll(page);
+        ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTOs);
+        return ResponseEntity.ok(apiResult);
     }
 
     // (기능2) 개별 상품 상세 조회
