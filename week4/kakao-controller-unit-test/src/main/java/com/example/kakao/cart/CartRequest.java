957feac72b12f174936d487bc1d8ab9e6f1d6e12
kssumin <201566@jnu.ac.kr>
2023-07-26 04:42:18 +0900

    feat : 장바구니 update controller 테스트

--- week4/kakao-controller-unit-test/src/main/java/com/example/kakao/cart/CartRequest.java
+++ week4/kakao-controller-unit-test/src/main/java/com/example/kakao/cart/CartRequest.java
@@ -1,12 +1,13 @@
 package com.example.kakao.cart;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 public class CartRequest {
 
-    @Getter @Setter @ToString
+    @Getter @Setter @ToString @Builder
     public static class SaveDTO {
         private int optionId;
         private int quantity;
