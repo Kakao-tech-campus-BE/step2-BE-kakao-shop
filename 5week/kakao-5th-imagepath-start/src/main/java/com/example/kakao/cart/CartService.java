034800940d20b3bcf527bfe5e836abdd1430a4d1
kimdonwoo <ehstjstod@gmail.com>
2023-07-25 14:04:39 +0900

    feat : 5주차 과제 장바구니 담기
    - 장바구니 담기 -> 예외처리하기 (완료)

---
+++ 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/cart/CartService.java
@@ -0,0 +1,102 @@
+package com.example.kakao.cart;
+
+import com.example.kakao._core.errors.exception.Exception400;
+import com.example.kakao._core.errors.exception.Exception404;
+import com.example.kakao.product.option.Option;
+import com.example.kakao.product.option.OptionJPARepository;
+import com.example.kakao.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class CartService {
+
+    private final CartJPARepository cartJPARepository;
+    private final OptionJPARepository optionJPARepository;
+
+    // 5주차 과제 : 장바구니 담기 -> 예외 처리하기
+    /*
+        흠 Cart에서 Price를 칼럼으로 들고 있으면 만약 해당 Option의 가격이 할인행사 같은거로
+        가격 변동이 생긴다면??? 모든 Cart에 Price 값을 다시 계산해줘야하는 문제가 생길거 같긴함
+        => 일단 고려 x
+
+     */
+    @Transactional
+    public void addCartList(List<CartRequest.SaveDTO> requestDTOs, User sessionUser) {
+        // 1. 동일한 옵션이 들어오면 예외처리
+        // [ { optionId:1, quantity:5 }, { optionId:1, quantity:10 } ]
+
+        Set<Integer> optionIds = new HashSet();
+
+        for (CartRequest.SaveDTO requestDTO : requestDTOs){
+            int optionId = requestDTO.getOptionId();
+            if (!optionIds.contains(optionId)){
+                optionIds.add(optionId);
+            }else{
+                throw new Exception400("동일한 옵션이 중복되어 들어왔습니다: " + optionId);
+            }
+        }
+
+        // 2. cartJPARepository.findByOptionIdAndUserId() 조회 -> 존재하면 장바구니에 수량을 추가하는 업데이트를 해야함. (더티체킹하기)
+        // 3. [2번이 아니라면] 유저의 장바구니에 담기
+        for (CartRequest.SaveDTO requestDTO : requestDTOs) {
+            int optionId = requestDTO.getOptionId();
+            int quantity = requestDTO.getQuantity();
+            Optional<Cart> byOptionIdAndUserId = cartJPARepository.findByOptionIdAndUserId(optionId, sessionUser.getId());
+
+            if(byOptionIdAndUserId.isPresent()){ // 존재하면 장바구니에 수량만 업데이트
+//                Option optionPS = optionJPARepository.findById(optionId).orElseThrow(() -> new Exception404("해당 옵션을 찾을 수 없습니다 : " + optionId));
+//                int price = optionPS.getPrice() * quantity;
+                byOptionIdAndUserId.get().plueQuantity(quantity);
+
+            }else{
+                Option optionPS = optionJPARepository.findById(optionId).orElseThrow(() -> new Exception404("해당 옵션을 찾을 수 없습니다 : " + optionId));
+                int price = optionPS.getPrice() * quantity; // 가격 받아오기위해 option findById
+                Cart cart = Cart.builder().user(sessionUser).option(optionPS).quantity(quantity).price(price).build(); // 이건 새로 만드는거 !
+                cartJPARepository.save(cart);
+            }
+
+        }
+    }
+
+    public CartResponse.FindAllDTO findAll(User user) {
+        List<Cart> cartList = cartJPARepository.findByUserIdOrderByOptionIdAsc(user.getId());
+        // Cart에 담긴 옵션이 3개이면, 2개는 바나나 상품, 1개는 딸기 상품이면 Product는 2개인 것이다.
+        return new CartResponse.FindAllDTO(cartList);
+    }
+
+    public CartResponse.FindAllDTOv2 findAllv2(User user) {
+        List<Cart> cartList = cartJPARepository.findByUserIdOrderByOptionIdAsc(user.getId());
+        return new CartResponse.FindAllDTOv2(cartList);
+    }
+
+    @Transactional
+    public CartResponse.UpdateDTO update(List<CartRequest.UpdateDTO> requestDTOs, User user) {
+        List<Cart> cartList = cartJPARepository.findAllByUserId(user.getId());
+
+        // 1. 유저 장바구니에 아무것도 없으면 예외처리
+
+        // 2. cartId:1, cartId:1 이렇게 requestDTOs에 동일한 장바구니 아이디가 두번 들어오면 예외처리
+
+        // 3. 유저 장바구니에 없는 cartId가 들어오면 예외처리
+
+        // 위에 3개를 처리하지 않아도 프로그램은 잘돌아간다. 예를 들어 1번을 처리하지 않으면 for문을 돌지 않고, cartList가 빈배열 []로 정상응답이 나감.
+        for (Cart cart : cartList) {
+            for (CartRequest.UpdateDTO updateDTO : requestDTOs) {
+                if (cart.getId() == updateDTO.getCartId()) {
+                    cart.update(updateDTO.getQuantity(), cart.getOption().getPrice() * updateDTO.getQuantity());
+                }
+            }
+        }
+
+        return new CartResponse.UpdateDTO(cartList);
+    } // 더티체킹
+}
