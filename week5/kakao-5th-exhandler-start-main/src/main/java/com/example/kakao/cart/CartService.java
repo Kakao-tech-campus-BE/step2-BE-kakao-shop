af076d36b3d69c3cc437165b2bd531afa40d3a66
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-07-25 20:00:05 +0900

    service : add Cart List

---
+++ week5/kakao-5th-exhandler-start-main/src/main/java/com/example/kakao/cart/CartService.java
@@ -0,0 +1,36 @@
+package com.example.kakao.cart;
+
+import com.example.kakao._core.errors.exception.Exception404;
+import com.example.kakao.product.option.Option;
+import com.example.kakao.product.option.OptionJPARepository;
+import com.example.kakao.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class CartService {
+    private final OptionJPARepository optionJPARepository;
+    private final CartJPARepository cartJPARepository;
+
+    public void addCartList(List<CartRequest.SaveDTO> requestDTOs, User sessionUser) {
+        for (CartRequest.SaveDTO requestDTO:requestDTOs) {
+            int optionId = requestDTO.getOptionId();
+            int quantity = requestDTO.getQuantity();
+            Option option = optionJPARepository.findById(optionId)
+                    .orElseThrow(() -> new Exception404("해당 옵션을 찾을 수 없습니다. : "+optionId));
+            int price = option.getPrice() * quantity;
+            Cart cart = Cart.builder()
+                    .user(sessionUser)
+                    .option(option)
+                    .quantity(quantity)
+                    .price(price)
+                    .build();
+            cartJPARepository.save(cart);
+        }
+    }
+}
