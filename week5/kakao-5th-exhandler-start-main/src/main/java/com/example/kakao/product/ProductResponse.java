6c21f251fb26f1bd9a7ad830793fa27629933429
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-07-25 11:46:53 +0900

    feat : Response FindByIdDTO

--- week5/kakao-5th-exhandler-start-main/src/main/java/com/example/kakao/product/ProductResponse.java
+++ week5/kakao-5th-exhandler-start-main/src/main/java/com/example/kakao/product/ProductResponse.java
@@ -1,8 +1,11 @@
 package com.example.kakao.product;
 
+import com.example.kakao.product.option.Option;
 import lombok.Builder;
 import lombok.Getter;
-import org.hibernate.StaleStateException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ProductResponse {
     @Getter
@@ -23,5 +26,39 @@ public class ProductResponse {
         }
     }
 
+    @Getter
+    @Builder
+    public static class FindByIdDTO {
+        int id;
+        String productName;
+        String description;
+        String image;
+        int price;
+        int starCount; // 0~5
+        List<OptionDTO> options;
 
+        public FindByIdDTO(Product product, List<Option> optionsList) {
+            this.id = product.getId();
+            this.productName = product.getProductName();
+            this.description = product.getDescription();
+            this.image = product.getImage();
+            this.price = product.getPrice();
+            this.starCount = 5;
+            this.options = optionsList.stream().map(OptionDTO::new).collect(Collectors.toList());
+        }
+
+        @Getter
+        @Builder
+        public class OptionDTO {
+            int id;
+            String optionName;
+            int price;
+
+            public OptionDTO(Option option) {
+                this.id = option.getId();
+                this.optionName = option.getOptionName();
+                this.price = option.getPrice();
+            }
+        }
+    }
 }
