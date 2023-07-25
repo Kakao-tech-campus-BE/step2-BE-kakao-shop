f0a0a6d2ecaa4da5e66ddaafe1c49c04bf1d9191
kssumin <201566@jnu.ac.kr>
2023-07-25 12:56:11 +0900

    feat : Order domain 생성

---
+++ Kakao-Backend-Second-DTO/src/main/java/com/example/kakaoshop/order/domain/model/OrderItem.java
@@ -0,0 +1,20 @@
+package com.example.kakaoshop.order.domain.model;
+
+import lombok.*;
+
+@Getter
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class OrderItem {
+    private Long orderItemId;
+    private Order order;
+    private Long optionId;
+    private Long productId;
+    private String optionName;
+    private String productName;
+    private int price;
+    private int quantity;
+}
