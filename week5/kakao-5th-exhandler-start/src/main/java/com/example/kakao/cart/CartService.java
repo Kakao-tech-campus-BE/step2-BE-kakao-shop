28db1b1b32ee4f2939deedf17073bf4bb1b4db6a
Jun2-Lee <ljh19980823@gmail.com>
2023-07-25 12:08:45 +0900

    feat : 상품 상세조회 작성

---
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/CartService.java
@@ -0,0 +1,35 @@
+package com.example.kakao.cart;
+
+import com.example.kakao._core.errors.exception.Exception404;
+import com.example.kakao.product.option.Option;
+import com.example.kakao.product.option.OptionJPARepository;
+import com.example.kakao.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class CartService {
+    private final OptionJPARepository optionJPARepository;
+    @Transactional
+    public void addCartList(List<CartRequest.SaveDTO> requestDTOs, User sessionUser) {
+        // 1. 동일한 옵션이 들어오면 예외처리
+        // [ { optionId:1, quantity:5 }, { optionId:1, quantity:10 } ]
+
+        // 2. cartJPARepository.findByOptionIdAndUserId() 조회 -> 존재하면 장바구니에 수량을 추가하는 업데이트를 해야함. (더티체킹하기)
+
+        // 3. [2번이 아니라면] 유저의 장바구니에 담기
+        for (CartRequest.SaveDTO requestDTO : requestDTOs) {
+            int optionId = requestDTO.getOptionId();
+            int quantity = requestDTO.getQuantity();
+            Option optionPS = optionJPARepository.findById(optionId)
+                    .orElseThrow(() -> new Exception404("해당 옵션을 찾을 수 없습니다 : " + optionId));
+            int price = optionPS.getPrice() * quantity;
+            Cart cart = Cart.builder().user(sessionUser).option(optionPS).quantity(quantity).price(price).build();
+            cartJPARepository.save(cart);
+        }
+    }
+}
