c6b1e228d160581224e09ae873a2bb9ef380b3c1
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-07-26 00:12:30 +0900

    fix : add @Modifying because of using DML

--- week5/kakao-5th-exhandler-start-main/src/main/java/com/example/kakao/cart/CartJPARepository.java
+++ week5/kakao-5th-exhandler-start-main/src/main/java/com/example/kakao/cart/CartJPARepository.java
@@ -1,6 +1,7 @@
 package com.example.kakao.cart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -10,6 +11,7 @@ import java.util.Optional;
 
 public interface CartJPARepository extends JpaRepository<Cart, Integer> {
 
+    @Modifying
     @Query("delete from Cart c where c.user.id = :userId")
     List<Cart> findAllByUserId(int userId);
 
