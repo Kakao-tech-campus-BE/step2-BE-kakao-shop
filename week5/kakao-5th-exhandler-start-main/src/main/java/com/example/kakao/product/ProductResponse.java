6276c8a635865cadabfca9ae83f8010ef80659f1
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-07-25 11:18:36 +0900

    feat : Response FindAllDTO

--- week5/kakao-5th-exhandler-start-main/src/main/java/com/example/kakao/product/ProductResponse.java
+++ week5/kakao-5th-exhandler-start-main/src/main/java/com/example/kakao/product/ProductResponse.java
@@ -1,6 +1,27 @@
 package com.example.kakao.product;
 
+import lombok.Builder;
+import lombok.Getter;
+import org.hibernate.StaleStateException;
+
 public class ProductResponse {
+    @Getter
+    @Builder
+    public static class FindAllDTO {
+        private int id;
+        private String productName;
+        private String description;
+        private String image;
+        private int price;
+
+        public FindAllDTO(int id, String productName, String description, String image, int price) {
+            this.id = id;
+            this.productName = productName;
+            this.description = description;
+            this.image = image;
+            this.price = price;
+        }
+    }
 
 
 }
