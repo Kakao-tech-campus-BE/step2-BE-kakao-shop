aa88fd22aa919bc5d36f4a82165636cb344594a6
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-07-25 12:39:57 +0900

    fix : option repository query

--- week5/kakao-5th-exhandler-start-main/src/main/java/com/example/kakao/product/option/OptionJPARepository.java
+++ week5/kakao-5th-exhandler-start-main/src/main/java/com/example/kakao/product/option/OptionJPARepository.java
@@ -10,10 +10,9 @@ import java.util.Optional;
 
 public interface OptionJPARepository extends JpaRepository<Option, Integer> {
 
+    @Query("select o from Option o where o.product.id = :productId")
     List<Option> findByProductId(@Param("productId") int productId);
-    Optional<Option> findById(int id);
 
-    // findById_select_product_lazy_error_fix_test
     @Query("select o from Option o join fetch o.product where o.product.id = :productId")
-    List<Option> mFindByProductId(@Param("productId") int productId);
+    List<Option> findByProductIdJoinProduct(@Param("productId") int productId);
 }
