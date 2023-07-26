2e9358953c8672a8d8416ffa2d57bf3dd14c21ac
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-07-26 14:40:27 +0900

    feat : apply valid to list

--- week5/kakao-5th-exhandler-start-main/src/main/java/com/example/kakao/cart/CartRequest.java
+++ week5/kakao-5th-exhandler-start-main/src/main/java/com/example/kakao/cart/CartRequest.java
@@ -4,19 +4,22 @@ import lombok.Getter;
 import lombok.ToString;
 
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
 
 public class CartRequest {
 
     @Getter @ToString
     public static class SaveDTO {
+        @Min(value = 1, message = "잘못된 옵션입니다.")
         private int optionId;
+        @Min(value = 1, message = "잘못된 수량입니다.")
         private int quantity;
     }
 
     @Getter @ToString
     public static class UpdateDTO {
+        @Min(value = 1, message = "잘못된 옵션입니다.")
         private int cartId;
+        @Min(value = 1, message = "잘못된 수량입니다.")
         private int quantity;
     }
 }
--- week5/kakao-5th-exhandler-start-main/src/main/java/com/example/kakao/cart/CartRestController.java
+++ week5/kakao-5th-exhandler-start-main/src/main/java/com/example/kakao/cart/CartRestController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@Validated
 public class CartRestController {
     private final CartService cartService;
     /**
@@ -36,6 +38,7 @@ public class CartRestController {
     public ResponseEntity<?> addCartList(@RequestBody @Valid List<CartRequest.SaveDTO> requestDTOs,
                                          Errors errors,
                                          @AuthenticationPrincipal CustomUserDetails userDetails) {
+
         cartService.addCartList(requestDTOs, userDetails.getUser());
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(null);
         return ResponseEntity.ok(apiResult);
