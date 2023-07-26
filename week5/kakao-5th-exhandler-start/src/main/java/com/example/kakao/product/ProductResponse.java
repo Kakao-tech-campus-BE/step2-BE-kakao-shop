bdec382f5796025730066ae131887cda587d7765
sjmjys954646 <sjmjys954646@gmail.com>
2023-07-26 17:27:07 +0900

    product find all

--- week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/product/ProductResponse.java
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/product/ProductResponse.java
@@ -1,6 +1,26 @@
 package com.example.kakao.product;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class ProductResponse {
+    @Getter
+    @Setter
+    public static class FindAllDTO {
 
+        private int id;
+        private String productName;
+        private String description;
+        private String image;
+        private int price;
 
+        //깊은 복사
+        public FindAllDTO(Product product) {
+            this.id = product.getId();
+            this.productName = product.getProductName();
+            this.description = product.getDescription();
+            this.image = product.getImage();
+            this.price = product.getPrice();
+        }
+    }
 }
