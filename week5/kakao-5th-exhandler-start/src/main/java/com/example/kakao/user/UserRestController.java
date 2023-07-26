17e497ffe1981d7c5c3d03a1d731b114c3f325b6
eunjin917 <cgoya01@likelion.org>
2023-07-27 00:44:02 +0900

    5주차과제 - AOP 적용 완료

--- week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/user/UserRestController.java
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/user/UserRestController.java
@@ -20,7 +20,7 @@ import java.util.List;
 @RestController
 public class UserRestController {
 
-    private final GlobalExceptionHandler globalExceptionHandler;
+//    private final GlobalExceptionHandler globalExceptionHandler;
     private final UserService userService;
 
     // (기능3) 이메일 중복체크
@@ -41,18 +41,18 @@ public class UserRestController {
     // (기능4) 회원가입
     @PostMapping("/join")
     public ResponseEntity<?> join(@RequestBody @Valid UserRequest.JoinDTO requestDTO, Errors errors) {
-        // 부가기능 => 코드에서 제외시키기 => AOP
-        if (errors.hasErrors()) {
-            List<FieldError> fieldErrors = errors.getFieldErrors();
-            Exception400 ex = new Exception400(fieldErrors.get(0).getDefaultMessage() + ":" + fieldErrors.get(0).getField());
-            return new ResponseEntity<>(
-                    ex.body(),
-                    ex.status()
-            );
-        }
-        // 디스패쳐서블릿이 글로벌익셉션핸들러에서 캐치해줄 것이므로, 여기서 감쌀 필요가 없어짐
+        // 부가기능 => 코드에서 제외시키려면 => AOP를 사용해서 없애면 됨 + errors 매개변수는 놔둬야 함!!
+//        if (errors.hasErrors()) {
+//            List<FieldError> fieldErrors = errors.getFieldErrors();
+//            Exception400 ex = new Exception400(fieldErrors.get(0).getDefaultMessage() + ":" + fieldErrors.get(0).getField());
+//            return new ResponseEntity<>(
+//                    ex.body(),
+//                    ex.status()
+//            );
+//        }
+
+        // 디스패쳐서블릿이 글로벌익셉션핸들러에서 캐치해줄 것이므로, 여기서 감쌀 필요가 없어짐 => request 매개변수도 없애기
 //        try {
-        // 핵심로직
         userService.join(requestDTO);
         return ResponseEntity.ok().body(ApiUtils.success(null));
 //        } catch (RuntimeException e) {
@@ -64,21 +64,8 @@ public class UserRestController {
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody @Valid UserRequest.LoginDTO requestDTO, Errors errors) {
 
-        if (errors.hasErrors()) {
-            List<FieldError> fieldErrors = errors.getFieldErrors();
-            Exception400 ex = new Exception400(fieldErrors.get(0).getDefaultMessage() + ":" + fieldErrors.get(0).getField());
-            return new ResponseEntity<>(
-                    ex.body(),
-                    ex.status()
-            );
-        }
-
-//        try {
-            String jwt = userService.login(requestDTO);
-            return ResponseEntity.ok().header(JWTProvider.HEADER, jwt).body(ApiUtils.success(null));
-//        }catch (RuntimeException e){
-//            return globalExceptionHandler.handle(e, request);
-//        }
+        String jwt = userService.login(requestDTO);
+        return ResponseEntity.ok().header(JWTProvider.HEADER, jwt).body(ApiUtils.success(null));
     }
 
     // 로그아웃 사용안함 - 프론트에서 JWT 토큰을 브라우저의 localstorage에서 삭제하면 됨.
