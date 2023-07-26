6432fd1e8818428c8e8d98577f6aa3b35655a402
CMS1108 <asv7420@naver.com>
2023-07-27 02:20:02 +0900

    feat: 전남대_BE_최민서_5주차 과제

---
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/order/OrderJPARepository.java
@@ -0,0 +1,14 @@
+package com.example.kakao.order;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderJPARepository extends JpaRepository<Order, Integer> {
+    @Query("select o from Order o join fetch o.user where o.user.id = :userId")
+    public List<Order> findByUserId(@Param("userId") int userId);
+
+}
