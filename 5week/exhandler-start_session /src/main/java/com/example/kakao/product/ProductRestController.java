47629a390e5771c50fe0744b02acb9e6c708c7c5
won0104 <ksdyys3@naver.com>
2023-07-27 01:07:29 +0900

    [session] 기능2. 상품 상세  보기

--- 5week/exhandler-start_session /src/main/java/com/example/kakao/product/ProductRestController.java
+++ 5week/exhandler-start_session /src/main/java/com/example/kakao/product/ProductRestController.java
@@ -4,6 +4,7 @@ import com.example.kakao._core.utils.ApiUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,21 +13,33 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 public class ProductRestController {
+
     private final ProductService productService;
 
     // (기능1) 전체 상품 목록 조회 (페이징 9개씩)
     // /products
     @GetMapping("/products")
-    //defaultValue가 없으면 500이 터지게 됨
     public ResponseEntity<?> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page) {
         List<ProductResponse.FindAllDTO> responseDTOs = productService.findAll(page);
-        return ResponseEntity.ok().body(ApiUtils.success(responseDTOs));
-        //인증이나, 검증? 필요 없다.
+        ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTOs);
+        return ResponseEntity.ok(apiResult);
     }
 
     // (기능2) 개별 상품 상세 조회
     // /products/{id}
-    public void findById() {
+    @GetMapping("/products/{id}")
+    public ResponseEntity<?> findById(@PathVariable int id) {
+        ProductResponse.FindByIdDTO responseDTO = productService.findById(id);
+        ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
+        return ResponseEntity.ok(apiResult);
+    }
 
+    // (기능2) 개별 상품 상세 조회 v2
+    // /products/{id}/v2
+    @GetMapping("/products/{id}/v2")
+    public ResponseEntity<?> findByIdv2(@PathVariable int id) {
+        ProductResponse.FindByIdDTOv2 responseDTO = productService.findByIdv2(id);
+        ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
+        return ResponseEntity.ok(apiResult);
     }
 }
