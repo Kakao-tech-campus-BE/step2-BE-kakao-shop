4592c2ce580e596e945fd45e55d8c2f4e675f543
SeokjunMoon <seokjun0915@icloud.com>
2023-07-26 11:59:38 +0900

    feat: 상품 상세 조회 버전 통일
    - 상품 상세 조회 시 2가지 버전을 하나로 통일

--- week5/kakao-5th-imagepath-start/src/main/java/com/example/kakao/product/ProductService.java
+++ week5/kakao-5th-imagepath-start/src/main/java/com/example/kakao/product/ProductService.java
@@ -20,22 +20,13 @@ public class ProductService {
     private final ProductJPARepository productRepository;
     private final OptionJPARepository optionRepository;
 
-    public ProductResponse.FindByIdDTOv2 findByIdv2(int id) {
+    public ProductResponse.FindByIdDTO findById(int id) {
         List<Option> optionListPS = optionRepository.findByProductIdJoinProduct(id);
         if(optionListPS.size() == 0){
             throw new Exception404("해당 상품을 찾을 수 없습니다 : "+id);
         }
-        return new ProductResponse.FindByIdDTOv2(optionListPS);
+        return new ProductResponse.FindByIdDTO(optionListPS);
     }
-
-    public ProductResponse.FindByIdDTO findById(int id) {
-        Product productPS = productRepository.findById(id).orElseThrow(
-                () -> new Exception404("해당 상품을 찾을 수 없습니다 : "+id)
-        );
-        List<Option> optionListPS = optionRepository.findByProductId(productPS.getId());
-        return new ProductResponse.FindByIdDTO(productPS, optionListPS);
-    }
-
 
 
     public List<ProductResponse.FindAllDTO> findAll(int page) {
@@ -50,9 +41,8 @@ public class ProductService {
         // List를 stream()으로 변환 -> 자바 오브젝트의 타입이 없어진다. (강물에 흩뿌린다)
         // map으로 순회하면서 값을 변한한다. (가공)
         // 가공된 데이터를 다시 자바 오브젝트로 변환한다.
-        List<ProductResponse.FindAllDTO> responseDTOs = pageContent.getContent().stream()
-                .map(product -> new ProductResponse.FindAllDTO(product))
+        return pageContent.getContent().stream()
+                .map(ProductResponse.FindAllDTO::new)
                 .collect(Collectors.toList());
-        return responseDTOs;
     }
 }
