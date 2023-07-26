a57733580ad88b6e48eda480b7c1da1572fbd21e
jinc20 <wh5971@pusan.ac.kr>
2023-07-27 01:44:22 +0900

    feat: CartService.addCartList 요구사항 1, 2번 구현
    과제 1. 동일한 옵션을 담을 때 예외처리한다.
    
    과제 2. 이미 담겨 있는 옵션을 또 담으면 기존 수량에 누적시킨다.

--- 5week/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/CartService.java
+++ 5week/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/CartService.java
@@ -1,5 +1,6 @@
 package com.example.kakao.cart;
 
+import com.example.kakao._core.errors.exception.Exception400;
 import com.example.kakao._core.errors.exception.Exception404;
 import com.example.kakao.product.option.Option;
 import com.example.kakao.product.option.OptionJPARepository;
@@ -9,10 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -26,13 +24,31 @@ public class CartService {
     public void addCartList(List<CartRequest.SaveDTO> requestDTOs, User sessionUser) {
         // 1. 동일한 옵션이 들어오면 예외처리
         // [ { optionId:1, quantity:5 }, { optionId:1, quantity:10 } ]
+        Set<Integer> optionIds = new HashSet<>();
+        for (CartRequest.SaveDTO requestDTO : requestDTOs) {
+            int optionId = requestDTO.getOptionId();
+            if (optionIds.contains(optionId)) throw new Exception400("동일한 옵션 상품을 담을 수 없습니다.");
+            optionIds.add(requestDTO.getOptionId());
+        }
+
 
         // 2. cartJPARepository.findByOptionIdAndUserId() 조회 -> 존재하면 장바구니에 수량을 추가하는 업데이트를 해야함. (더티체킹하기)
-
         // 3. [2번이 아니라면] 유저의 장바구니에 담기
         for (CartRequest.SaveDTO requestDTO : requestDTOs) {
             int optionId = requestDTO.getOptionId();
             int quantity = requestDTO.getQuantity();
+
+            // 2번
+            Optional<Cart> savedCart = cartJPARepository.findByOptionIdAndUserId(optionId, sessionUser.getId());
+            if (!savedCart.isEmpty()) {
+                int originQuantity = savedCart.get().getQuantity();
+                int price = savedCart.get().getPrice() / originQuantity;
+                int totalQuantity = originQuantity + quantity;
+                savedCart.get().update(totalQuantity, price * totalQuantity);
+                continue;
+            }
+
+            // 3번
             Option optionPS = optionJPARepository.findById(optionId)
                     .orElseThrow(() -> new Exception404("해당 옵션을 찾을 수 없습니다 : " + optionId));
             int price = optionPS.getPrice() * quantity;
