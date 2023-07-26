2fc0b0fd89fd6b24746ac325e03bf958b618138f
KimSehoon <shggm2000@gmail.com>
2023-07-26 19:58:50 +0900

    feat: addCartList 예외 추가
    장바구니 담기 기능에 대해
    1. 중복 옵션 추가 시 예외 발생
    2. 기존이 존재하는 옵션일 시 update 하는 기능 구현

--- 5Week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/cart/CartJPARepository.java
+++ 5Week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/cart/CartJPARepository.java
@@ -1,5 +1,6 @@
 package com.example.kakao.cart;
 
+import com.example.kakao.product.option.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,6 +14,9 @@ public interface CartJPARepository extends JpaRepository<Cart, Integer> {
 
     @Query("select c from Cart c join fetch c.option o join fetch o.product where c.user.id = :userId order by c.option.id asc")
     List<Cart> findByUserIdOrderByOptionIdAsc(int userId);
+
+    @Query("select c from Cart c where c.option.id = :optionId and c.user.id = :userId")
+    Cart findByOptionIdAndUserId(int optionId, int userId);
 
     void deleteByUserId(int userId);
 }
--- 5Week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/cart/CartService.java
+++ 5Week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/cart/CartService.java
@@ -1,5 +1,6 @@
 package com.example.kakao.cart;
 
+import com.example.kakao._core.errors.exception.Exception400;
 import com.example.kakao._core.errors.exception.Exception404;
 import com.example.kakao.product.option.Option;
 import com.example.kakao.product.option.OptionJPARepository;
@@ -8,7 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -20,20 +23,21 @@ public class CartService {
 
     @Transactional
     public void addCartList(List<CartRequest.SaveDTO> requestDTOs, User sessionUser) {
-        // 1. 동일한 옵션이 들어오면 예외처리
-        // [ { optionId:1, quantity:5 }, { optionId:1, quantity:10 } ]
+        checkSameOptionId(requestDTOs);
 
-        // 2. cartJPARepository.findByOptionIdAndUserId() 조회 -> 존재하면 장바구니에 수량을 추가하는 업데이트를 해야함. (더티체킹하기)
-
-        // 3. [2번이 아니라면] 유저의 장바구니에 담기
-        for (CartRequest.SaveDTO requestDTO : requestDTOs) {
-            int optionId = requestDTO.getOptionId();
-            int quantity = requestDTO.getQuantity();
-            Option optionPS = optionJPARepository.findById(optionId)
-                    .orElseThrow(() -> new Exception404("해당 옵션을 찾을 수 없습니다 : " + optionId));
-            int price = optionPS.getPrice() * quantity;
-            Cart cart = Cart.builder().user(sessionUser).option(optionPS).quantity(quantity).price(price).build();
-            cartJPARepository.save(cart);
+        for(CartRequest.SaveDTO requestDTO : requestDTOs){
+            Cart cart = cartJPARepository.findByOptionIdAndUserId(requestDTO.getOptionId(), sessionUser.getId());
+            if(cart != null){
+                cart.update(requestDTO.getOptionId(), requestDTO.getQuantity()+cart.getQuantity());
+            }else {
+                int optionId = requestDTO.getOptionId();
+                int quantity = requestDTO.getQuantity();
+                Option optionPS = optionJPARepository.findById(optionId)
+                        .orElseThrow(() -> new Exception404("해당 옵션을 찾을 수 없습니다 : " + optionId));
+                int price = optionPS.getPrice() * quantity;
+                cart = Cart.builder().user(sessionUser).option(optionPS).quantity(quantity).price(price).build();
+                cartJPARepository.save(cart);
+            }
         }
     }
 
@@ -64,4 +68,15 @@ public class CartService {
 
         return new CartResponse.UpdateDTO(cartList);
     } // 더티체킹
+
+    private void checkSameOptionId(List<CartRequest.SaveDTO> requestDTOs){
+        // 1. 동일한 옵션이 들어오면 예외처리
+        // [ { optionId:1, quantity:5 }, { optionId:1, quantity:10 } ]
+        Set<Integer> uniqueRequestDTOS = new HashSet<>();
+        for(CartRequest.SaveDTO carts : requestDTOs){
+            if(!uniqueRequestDTOS.add(carts.getOptionId())){
+                throw new Exception400("동일한 옵션이 존재합니다.");
+            }
+        }
+    }
 }
