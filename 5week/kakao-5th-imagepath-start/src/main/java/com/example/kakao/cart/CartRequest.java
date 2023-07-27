43bbf1dbe68e77f3d9f781713d802f98f8a96a0e
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-07-27 11:46:58 +0900

    Update CartRequest.java
    Valid 옵션 추가 Postive로 0개 이하의 수량을 업데이트 할 수 없게 하였음

--- 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/cart/CartRequest.java
+++ 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/cart/CartRequest.java
@@ -3,16 +3,21 @@ package com.example.kakao.cart;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 public class CartRequest {
 
-    @Getter @Setter @ToString
+    @Getter
+    @Setter
+    @ToString
     public static class SaveDTO {
         @NotNull
         private int optionId;
         @NotNull
+        @Positive //0개를 입력할수는 없다!
         private int quantity;
     }
 
@@ -21,6 +26,7 @@ public class CartRequest {
         @NotNull
         private int cartId;
         @NotNull
+        @Positive
         private int quantity;
     }
 }
