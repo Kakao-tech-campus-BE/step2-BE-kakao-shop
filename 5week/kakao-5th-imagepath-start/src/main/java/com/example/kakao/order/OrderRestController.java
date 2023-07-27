fab9208319616c564fa2dbdcd989fce16d43c179
MilkTea24 <kimmanjae15@pusan.ac.kr>
2023-07-27 11:04:48 +0900

    feat: create OrderRestController
    OrderRestController와 두가지 메서드를 생성하였습니다.
    
    - save : 장바구니에 담긴 상품을 주문하는 메서드
    
    - findById : 주문 내역을 확인하는 메서드

--- 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/OrderRestController.java
+++ 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/OrderRestController.java
@@ -1,30 +1,40 @@
 package com.example.kakao.order;
 
+import com.example.kakao._core.security.CustomUserDetails;
 import com.example.kakao._core.utils.ApiUtils;
 import com.example.kakao.order.item.Item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/orders")
 public class OrderRestController {
+
+    private final OrderService orderService;
 
 
     // (기능9) 결재하기 - (주문 인서트) POST
     // /orders/save
-    public void save() {
-
+    @PostMapping("/save")
+    public void save(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        OrderResponse.saveDTO responseDTO = orderService.save(userDetails.getUser());
+        ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
+        return ResponseEntity.ok(apiResult);
     }
 
     // (기능10) 주문 결과 확인 GET
     // /orders/{id}
-    public void findById() {
-
+    @GetMapping("/{id}")
+    public void findById(@PathVariable int id, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        OrderResponse.findByIdDTO responseDTO = orderService.findByOrderId(id, userDetails.getUser());
+        ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
+        return ResponseEntity.ok(apiResult);
     }
 
 }
