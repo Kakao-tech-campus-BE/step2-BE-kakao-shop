d31c0961301607c4ae592b29dd94a223241a8988
이지민 <min102602@naver.com>
2023-07-28 01:11:56 +0900

    fix : `@Param` 수정

--- 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/item/ItemJPARepository.java
+++ 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/item/ItemJPARepository.java
@@ -4,10 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import org.springframework.data.repository.query.Param;
 
 
 public interface ItemJPARepository extends JpaRepository<Item, Integer> {
 
   @Query("select i from Item i where i.order.id = :orderId order By i.order.id asc")
-  List<Item> findByOrderId(int orderId);
+  List<Item> findByOrderId(@Param("orderId") int orderId);
 }
