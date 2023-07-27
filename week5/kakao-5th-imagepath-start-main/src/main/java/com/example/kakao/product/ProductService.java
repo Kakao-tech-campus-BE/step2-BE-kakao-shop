3378dde1f9b7b6137a65aa02d27f4fe69d4b9d9a
xcelxlorx <xcelxlorx@gmail.com>
2023-07-27 21:09:20 +0900

    feat: Implement product controller, service

---
+++ week5/kakao-5th-imagepath-start-main/src/main/java/com/example/kakao/product/ProductService.java
@@ -0,0 +1,47 @@
+package com.example.kakao.product;
+
+import com.example.kakao._core.errors.exception.Exception404;
+import com.example.kakao.product.option.Option;
+import com.example.kakao.product.option.OptionJPARepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class ProductService {
+
+    private final ProductJPARepository productRepository;
+    private final OptionJPARepository optionRepository;
+
+    public List<ProductResponse.FindAllDTO> findAll(int page) {
+        Pageable pageable = PageRequest.of(page,9);
+        Page<Product> pageContent = productRepository.findAll(pageable);
+        return pageContent.getContent().stream()
+                .map(ProductResponse.FindAllDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    public ProductResponse.FindByIdDTO findById(int id) {
+        Product product = productRepository.findById(id).orElseThrow(
+                () -> new Exception404("해당 상품을 찾을 수 없습니다 : "+id)
+        );
+        List<Option> options = optionRepository.findByProductId(product.getId());
+        return new ProductResponse.FindByIdDTO(product, options);
+    }
+
+    public ProductResponse.FindByIdDTOv2 findByIdv2(int id) {
+        List<Option> options = optionRepository.findByProductIdJoinFetch(id);
+        if(options.isEmpty()){
+            throw new Exception404("해당 상품을 찾을 수 없습니다 : " + id);
+        }
+        return new ProductResponse.FindByIdDTOv2(options);
+    }
+}
