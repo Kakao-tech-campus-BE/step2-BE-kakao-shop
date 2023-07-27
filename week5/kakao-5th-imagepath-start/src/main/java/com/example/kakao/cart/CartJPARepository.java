7f2bb21234e35bf0a4b53ef0a3840dfb478fff77
MinseokGo <rhalstjr1999@naver.com>
2023-07-27 22:03:09 +0900

    feat: add 'delete cart method'
    주문 완료 시, 장바구니를 비우기 위해 delete 메서드를 구현하였습니다.

--- week5/kakao-5th-imagepath-start/src/main/java/com/example/kakao/cart/CartJPARepository.java
+++ week5/kakao-5th-imagepath-start/src/main/java/com/example/kakao/cart/CartJPARepository.java
@@ -1,6 +1,7 @@
 package com.example.kakao.cart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,7 +15,9 @@ public interface CartJPARepository extends JpaRepository<Cart, Integer> {
     @Query("select c from Cart c where c.user.id = :userId order by c.option.id asc")
     List<Cart> findByUserIdOrderByOptionIdAsc(int userId);
 
-    void deleteByUserId(int userId);
+    @Modifying
+    @Query("delete from Cart c where c.user.id = :userId")
+    void deleteByUserId(@Param("userId")int userId);
 
     @Query("select c from Cart c where c.option.id = :optionId and c.user.id = :userId")
     Optional<Cart> findByOptionIdAndUserId(@Param("optionId") int optionId, @Param("userId") int userId);
