edb12ff58e3a4fc5f66b301f164a265936b3a0a8
sososo0 <sh0000@pusan.ac.kr>
2023-07-27 17:21:42 +0900

    feat: added FindByIdDTO in ProductResponse

--- 5week/kakao-5th-assignment/src/main/java/com/example/kakao/product/ProductResponse.java
+++ 5week/kakao-5th-assignment/src/main/java/com/example/kakao/product/ProductResponse.java
@@ -3,6 +3,8 @@ package com.example.kakao.product;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 public class ProductResponse {
     @Getter
     @Setter
@@ -20,6 +22,26 @@ public class ProductResponse {
             this.description = product.getDescription();
             this.image = product.getImage();
             this.price = product.getPrice();
+        }
+    }
+
+    @Getter @Setter
+    public static class FindByIdDTO {
+
+        private int id;
+        private String productName;
+        private String description;
+        private String image;
+        private int price;
+        private int starCount; // 0~5
+        private List<OptionDTO> options;
+
+        @Getter @Setter
+        public class OptionDTO {
+            private int id;
+            private String optionName;
+            private int price;
+
         }
     }
 }
