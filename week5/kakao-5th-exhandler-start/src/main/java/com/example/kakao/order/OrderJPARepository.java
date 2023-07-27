9eedf45250e01968441c594d0a6db661f2e5712c
JS970 <son73097@pusan.ac.kr>
2023-07-27 17:02:20 +0900

    feat: Order
    - configure Order DTO
    - configure Order Service Layer
    - configure Order Control Layer
    - configure query at OrderJPARepository

--- week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/order/OrderJPARepository.java
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/order/OrderJPARepository.java
@@ -1,7 +1,13 @@
 package com.example.kakao.order;
 
+import com.example.kakao.order.item.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderJPARepository extends JpaRepository<Order, Integer> {
-
+    @Query("select i from Item i join fetch i.order where i.order.user.id = :orderNumber")
+    List<Item> findByOrderNumber(@Param("orderNumber") int orderNumber);
 }
--- week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/order/OrderRestController.java
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/order/OrderRestController.java
@@ -1,11 +1,15 @@
 package com.example.kakao.order;
 
+import com.example.kakao._core.security.CustomUserDetails;
 import com.example.kakao._core.utils.ApiUtils;
 import com.example.kakao.order.item.Item;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,17 +18,22 @@ import java.util.List;
 @RestController
 public class OrderRestController {
 
+    private final OrderService orderService;
 
     // (기능9) 결재하기 - (주문 인서트) POST
-    // /orders/save
-    public void save() {
-
+    @PostMapping("/orders/save")
+    public ResponseEntity<?> save(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        OrderResponse.OrderInsertDTO responseDTO = orderService.save(userDetails.getUser());
+        // DTO 생성 및 반환
+        ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
+        return ResponseEntity.ok(apiResult);
     }
 
     // (기능10) 주문 결과 확인 GET
-    // /orders/{id}
-    public void findById() {
-
+    @GetMapping("/orders/{id}")
+    public ResponseEntity<?> findById(@PathVariable int id) {
+        OrderResponse.OrderCheckDTO responseDTO = orderService.orderCheck(id);
+        ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
+        return ResponseEntity.ok(apiResult);
     }
-
 }
