a814290c153c1b61489acbe474f3c8fab4079db8
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-07-27 11:51:44 +0900

    Update OrderJPARepository.java

--- 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/OrderJPARepository.java
+++ 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/OrderJPARepository.java
@@ -1,7 +1,13 @@
 package com.example.kakao.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface OrderJPARepository extends JpaRepository<Order, Integer> {
+
+    boolean existsByIdAndUserId (Integer orderId, Integer userId);
 
 }
