6787fa8e3427df4c0e1307bafc4d4c76ee7c4606
eunjin917 <cgoya01@likelion.org>
2023-07-27 19:43:04 +0900

    장바5주차과제-장바구니수정 예외처리 완료

--- week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/CartJPARepository.java
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/CartJPARepository.java
@@ -13,13 +13,11 @@ public interface CartJPARepository extends JpaRepository<Cart, Integer> {
     @Query(value = "insert into cart_tb(user_id, option_id, quantity, price) values(:userId, :optionId, :quantity, :price)", nativeQuery = true)
     void mSave(@Param("userId") int userId, @Param("optionId") int optionId, @Param("quantity") int quantity, @Param("price") int price);
 
-    @Query("delete from Cart c where c.user.id = :userId")
+    @Query("select c from Cart c where c.user.id = :userId")
     List<Cart> findAllByUserId(int userId);
 
     @Query("select c from Cart c join fetch c.option o join fetch o.product p where c.user.id = :userId order by c.option.id asc")
     List<Cart> findByUserIdOrderByOptionIdAsc(int userId);
-
-//    void deleteByUserId(int userId);
 
     @Query("select c from Cart c where c.option.id = :optionId and c.user.id = :userId")
     Cart findByOptionIdAndUserId(@Param("optionId") int optionId, @Param("userId") int userId);
--- week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/CartService.java
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/CartService.java
@@ -73,10 +73,26 @@ public class CartService {
         List<Cart> cartList = cartJPARepository.findAllByUserId(user.getId());
 
         // 1. 유저 장바구니에 아무것도 없으면 예외처리
+        if (cartList.isEmpty()) {
+            throw new Exception404("장바구니가 비어있습니다");
+        }
 
         // 2. cartId:1, cartId:1 이렇게 requestDTOs에 동일한 장바구니 아이디가 두번 들어오면 예외처리
+        Set<Integer> cartIdSet = new HashSet<>(); // 해시셋 생성
+        Boolean hasSame = requestDTOs.stream()
+                .map(requestDTO -> requestDTO.getCartId())
+                .anyMatch(cartId -> !cartIdSet.add(cartId));   // 해시셋에 추가했는데 안 되면(false) -> true를 하나라도 리턴한다면 곧 true가 됨(중복이있다)
+        if (hasSame) {
+            throw new Exception400("동일한 장바구니아이템이 입력되었습니다");
+        }
 
         // 3. 유저 장바구니에 없는 cartId가 들어오면 예외처리
+        for (CartRequest.UpdateDTO requestDTO : requestDTOs) {
+            int cartId = requestDTO.getCartId();
+            cartJPARepository.findById(cartId).orElseThrow(
+                    () -> new Exception404("해당 장바구니아이템을 찾을 수 없습니다 : " + cartId)
+            );
+        }
 
         // 위에 3개를 처리하지 않아도 프로그램은 잘돌아간다. 예를 들어 1번을 처리하지 않으면 for문을 돌지 않고, cartList가 빈배열 []로 정상응답이 나감.
         for (Cart cart : cartList) {
