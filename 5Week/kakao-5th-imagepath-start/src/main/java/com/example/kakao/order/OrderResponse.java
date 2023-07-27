77202ef30d17b7e69b0f7ed93b19289b7dd672e3
KimSehoon <shggm2000@gmail.com>
2023-07-27 21:50:20 +0900

    feat: OrderResponse findByIdDTO 구현
    OrderResponse 내 findByIdDTO 구현

--- 5Week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/OrderResponse.java
+++ 5Week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/OrderResponse.java
@@ -1,6 +1,57 @@
 package com.example.kakao.order;
 
+import com.example.kakao.order.item.Item;
+import com.example.kakao.product.Product;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class OrderResponse {
 
+    @Getter @Setter
+    public static class FindByIdDTO {
+        private int id;
+        private List<ProductDTO> products;
+        private int totalPrice;
+
+        public FindByIdDTO(Order order, List<Item> items) {
+            this.id = order.getId();
+            this.products = items.stream().map(item -> item.getOption().getProduct()).distinct()
+                    .map(product -> new ProductDTO(items, product))
+                    .collect(Collectors.toList());
+            this.totalPrice = items.stream().mapToInt(item -> item.getPrice() * item.getQuantity()).sum();
+        }
+
+        @Getter @Setter
+        public class ProductDTO {
+            private String productName;
+            private List<ItemDTO> items;
+
+            public ProductDTO(List<Item> items, Product product) {
+                this.productName = product.getProductName();
+                this.items = items.stream()
+                        .filter(item -> item.getOption().getProduct().getId() == product.getId())
+                        .map(ItemDTO::new)
+                        .collect(Collectors.toList());
+            }
+
+            @Getter @Setter
+            public class ItemDTO {
+                private int id;
+                private String optionName;
+                private int quanity;
+                private int price;
+
+                public ItemDTO(Item item) {
+                    this.id = item.getId();
+                    this.optionName = item.getOption().getOptionName();
+                    this.quanity = item.getQuantity();
+                    this.price = item.getPrice();
+                }
+            }
+        }
+    }
 
 }
