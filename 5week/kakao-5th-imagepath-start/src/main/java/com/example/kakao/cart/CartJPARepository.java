09845ceee29c115b501bd3e138a962b100ba5856
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-07-27 11:46:28 +0900

    Refactor CartJPARepository
    boolean 타입의 반환을 하는 메서드 추가 외 추가구현

--- 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/cart/CartJPARepository.java
+++ 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/cart/CartJPARepository.java
@@ -1,21 +1,30 @@
 package com.example.kakao.cart;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 
 public interface CartJPARepository extends JpaRepository<Cart, Integer> {
+    @EntityGraph(attributePaths = "option")
     List<Cart> findAllByUserId(int userId);
 
     @Query("select c from Cart c where c.user.id = :userId order by c.option.id asc")
     List<Cart> findByUserIdOrderByOptionIdAsc(int userId);
 
-    void deleteByUserId(int userId);
+//    void deleteByUserId(int userId);
+
+    List<Cart> findByOptionIdInAndUserId(Set<Integer> optionIds, Integer userId);
 
     @Query("select c from Cart c where c.option.id = :optionId and c.user.id = :userId")
     Optional<Cart> findByOptionIdAndUserId(@Param("optionId") int optionId, @Param("userId") int userId);
+
+    //위와 같은 api 에서 사용할 예정이지만 boolean 타입으로 결과를 도출하는 쿼리를 작성해보고 싶었다.
+    @Query("select case when count(c)>0 then true else false end from Cart c where c.option.id = :optionId and c.user.id = :userId")
+    boolean existsByOptionIdAndUserId(@Param("optionId") int optionId, @Param("userId") int userId);
 }
