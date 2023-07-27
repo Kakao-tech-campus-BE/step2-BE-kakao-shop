152cc021baaf50b7523a93c061893779b1e2d6c0
이지민 <min102602@naver.com>
2023-07-27 22:14:12 +0900

    add : `findByOrderId` 구현

--- 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/item/ItemJPARepository.java
+++ 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/item/ItemJPARepository.java
@@ -3,8 +3,13 @@ package com.example.kakao.order.item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface ItemJPARepository extends JpaRepository<Item, Integer> {
+
+  @Query("select i from Item  i join fetch  i.order join fetch i.option o join fetch o.product p where i.order.id = :orderId order by i.order.id asc")
+  List<Item> findByOrderId(@Param("orderId") int orderId);
 
 }
