8a620f66fba01dac763e922d66eeb7d71c5b42f9
JS970 <son73097@pusan.ac.kr>
2023-07-27 16:55:12 +0900

    feat: Modified OptionJPARepository

--- week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/product/option/OptionJPARepository.java
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/product/option/OptionJPARepository.java
@@ -10,10 +10,11 @@ import java.util.Optional;
 
 public interface OptionJPARepository extends JpaRepository<Option, Integer> {
 
+    /* 개별상품조회 개별쿼리 */
+    @Query("select o from Option o where o.product.id = :productId")
     List<Option> findByProductId(@Param("productId") int productId);
-    Optional<Option> findById(int id);
 
-    // findById_select_product_lazy_error_fix_test
+    /* 개별상품조회 한방쿼리 */
     @Query("select o from Option o join fetch o.product where o.product.id = :productId")
-    List<Option> mFindByProductId(@Param("productId") int productId);
+    List<Option> findByProductIdJoinProduct(@Param("productId") int productId);
 }
