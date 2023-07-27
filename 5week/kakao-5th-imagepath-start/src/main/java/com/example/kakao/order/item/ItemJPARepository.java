1dbda3c63ae88b1b10aee323c495f2b066a6408b
ganeung <duf325@naver.com>
2023-07-27 21:17:44 +0900

    Feat : Add new features to the Order domain and modify ItemJPARepository
    - OrderJPARepository
    - OrderResponse
    - OrderRestController
    - OrderService
    - ItemJPARepository

--- 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/item/ItemJPARepository.java
+++ 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/item/ItemJPARepository.java
@@ -1,10 +1,16 @@
 package com.example.kakao.order.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 
 public interface ItemJPARepository extends JpaRepository<Item, Integer> {
+
+	// Item.getOption을 하면서 생기는 N+1문제를 해결하기 위해 한방 쿼리를 작성하였다.
+	@Query("SELECT i FROM Item i JOIN FETCH i.option o JOIN FETCH o.product WHERE i.id = :orderId")
+	List<Item> findAllByOrderId(@Param("orderId") int id);
 
 }
