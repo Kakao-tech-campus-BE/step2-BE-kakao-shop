e911ac08c5f5f5a77fcfd46845e2e7690cb68f5e
ganeung <duf325@naver.com>
2023-07-27 21:05:26 +0900

    Feat : Add GlobalExceptionHandler and GlobalValidationHandler
    - POST방식에 대한 validation 핸들링과 여러 예외 상황에 대한 핸들링 추가
    - 디스패처 서블릿에서 예외 핸들링하도록 함

--- 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/_core/errors/GlobalExceptionHandler.java
+++ 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/_core/errors/GlobalExceptionHandler.java
@@ -4,8 +4,10 @@ import com.example.kakao._core.errors.exception.*;
 import com.example.kakao._core.utils.ApiUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.dao.DataIntegrityViolationException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -35,9 +37,29 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(e.body(), e.status());
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+    	ApiUtils.ApiResult<?> apiResult = ApiUtils.error("잘못된 JSON 양식입니다", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(apiResult, HttpStatus.BAD_REQUEST);
+    }
+    
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public ResponseEntity<?> MethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+//    	ApiUtils.ApiResult<?> apiResult = ApiUtils.error("유효하지 않는 값입니다.", HttpStatus.BAD_REQUEST);
+//        return new ResponseEntity<>(apiResult, HttpStatus.BAD_REQUEST);
+//    }
+    
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<?> DataIntegrityViolationException(DataIntegrityViolationException ex) {
+    	ApiUtils.ApiResult<?> apiResult = ApiUtils.error("사용자를 찾을 수 없습니다", HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(apiResult, HttpStatus.NOT_FOUND);
+    }
+    
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> unknownServerError(Exception e){
         ApiUtils.ApiResult<?> apiResult = ApiUtils.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        System.out.println(e.getClass());
         return new ResponseEntity<>(apiResult, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
