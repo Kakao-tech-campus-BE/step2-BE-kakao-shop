1cbc6e90be2460eee3c50847b8fafb981df2acf3
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-07-27 11:51:41 +0900

    Feature ItemJPARepository
    Item에 같이 있는 Product와 Option 에 대한 옵션을 join fetch 로 가져왔다.

--- 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/item/ItemJPARepository.java
+++ 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/item/ItemJPARepository.java
@@ -1,10 +1,15 @@
 package com.example.kakao.order.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 
 public interface ItemJPARepository extends JpaRepository<Item, Integer> {
 
+    @Query("SELECT i FROM Item i JOIN FETCH i.option o JOIN FETCH o.product WHERE i.order.id = :orderId")
+    List<Item> findWithOptionAndProductByOrderId(@Param("orderId") Integer orderId);
+    List<Item> findAllByOrder_Id (int orderId);
 }
