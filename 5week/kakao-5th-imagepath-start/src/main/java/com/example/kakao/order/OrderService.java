a14333b31ad3bf6f535a8c27528410edf7b90176
Jin_00 <anjdal64@gmail.com>
2023-07-28 11:58:53 +0900

    OrderService 궁금한 부분

--- 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/OrderService.java
+++ 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/OrderService.java
@@ -27,27 +27,27 @@ public class OrderService {
     // 결재하기 기능 구현 ( 장바구니 초기화 )
     @Transactional
     public OrderResponse saveOrderList(int userId) {
+        // 유저의 카트 리스트 가져오기 -> 가장 위로 올린 이유 : 카트가 비어있는 경우를 먼저 체크해야, order 의 부정 생성을 막을 수 있음
+        // 이 카트를 비어있는 부분도 따로 함수로 빼는게 좋을지?
+        List<Cart> cartList = cartJPARepository.findAllByUserId(userId);
+        
+        // 카트가 비어있다면
+        if (cartList.isEmpty()) {
+            throw new Exception400("장바구니가 비어있습니다.");
+        }
+        
         // 해당하는 유저 가져오기
         User user = userJPARepository.findById(userId)
                 .orElseThrow(() -> new Exception400("해당하는 유저가 존재하지 않습니다. userId : " + userId));
 
-
-        // order 생성
+        // order 생성 -> build 후 save 하는 부분을 따로 함수로 빼는게 좋을지?
         Order order = Order.builder()
                 .user(user)
                 .build();
 
         orderJPARepository.save(order);
 
-        // 유저의 카트 리스트 가져오기
-        List<Cart> cartList = cartJPARepository.findAllByUserId(userId);
-
-        // 카트가 비어있다면
-        if (cartList.isEmpty()) {
-            throw new Exception400("장바구니가 비어있습니다.");
-        }
-
-        // 응답값 items 만들기
+        // 응답값 items 만들기 -> build 후 save 하는 부분을 따로 함수로 빼는게 좋을지?
         List<Item> itemList = cartList.stream()
                 .map(cart -> Item.builder()
                         .order(order)
@@ -71,12 +71,12 @@ public class OrderService {
         Order order = orderJPARepository.findById(orderId)
                 .orElseThrow(() -> new Exception400("주문 내역이 존재하지 않습니다. orderId : " + orderId));
 
-        // 인증
+        // 인증 -> 이런 인증 부분도 따로 함수로 빼는게 좋을지?
         if(orderId != order.getId()) {
             throw new Exception400("권한이 없습니다. orderId : " + orderId + " order.getId() : " + order.getId());
         }
 
-        // userId 인증
+        // userId 인증 -> 이런 인증 부분도 따로 함수로 빼는게 좋을지?
         if(userId != order.getUser().getId()) {
             throw new Exception400("다른 유저의 권한입니다. userId : " + userId + " order.getUser().getId() : " + order.getUser().getId());
         }
