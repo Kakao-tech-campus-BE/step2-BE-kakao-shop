7feb3b87c5dde0178215fc8b088efd35b5236d87
unknown <jhy0285@gmail.com>
2023-07-28 16:38:41 +0900

    order, item 부분 구현 완료

--- 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/OrderJPARepository.java
+++ 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/OrderJPARepository.java
@@ -1,7 +1,17 @@
 package com.example.kakao.order;
 
+import com.example.kakao.order.item.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface OrderJPARepository extends JpaRepository<Order, Integer> {
+    @Query("select o from Order o  where o.user.id = :userId and o.id = :orderId")
+    Optional<Order> findByUserIdAndOrderId(@Param("userId") int userId,@Param("orderId") int orderId);
+
 
 }
+
