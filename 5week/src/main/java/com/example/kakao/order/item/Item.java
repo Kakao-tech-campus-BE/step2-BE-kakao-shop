00b3316130d2c326d304ec3933c172e0df977fd7
전남대BE_이유진 <132261512+2Using@users.noreply.github.com>
2023-07-28 13:10:40 +0900

    Add files via upload

---
+++ 5week/src/main/java/com/example/kakao/order/item/Item.java
@@ -0,0 +1,41 @@
+package com.example.kakao.order.item;
+
+import com.example.kakao.product.option.Option;
+import com.example.kakao.order.Order;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name="item_tb", indexes = {
+        @Index(name = "item_option_id_idx", columnList = "option_id"),
+        @Index(name = "item_order_id_idx", columnList = "order_id")
+})
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Option option;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Order order;
+
+    @Column(nullable = false)
+    private int quantity;
+    @Column(nullable = false)
+    private int price;
+
+
+    @Builder
+    public Item(int id, Option option, Order order, int quantity, int price) {
+        this.id = id;
+        this.option = option;
+        this.order = order;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
---
+++ 5week/src/main/java/com/example/kakao/user/User.java
@@ -0,0 +1,42 @@
+package com.example.kakao.user;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name="user_tb")
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(length = 100, nullable = false, unique = true)
+    private String email; // 인증시 필요한 필드
+    @Column(length = 256, nullable = false)
+    private String password;
+    @Column(length = 45, nullable = false)
+    private String username;
+
+    @Column(length = 30)
+    private String roles;
+
+    @Builder
+    public User(int id, String email, String password, String username, String roles) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.roles = roles;
+    }
+
+    public void updatePassword(String password) {
+        this.password = password;
+    }
+
+}
