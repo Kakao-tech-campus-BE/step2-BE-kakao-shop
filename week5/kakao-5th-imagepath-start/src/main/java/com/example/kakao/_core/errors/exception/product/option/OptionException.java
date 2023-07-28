b17dfa9896cf7ef5af47dbaed8b8c07b8b52d7a3
Taeho <rjsdnxogh55@gmail.com>
2023-07-28 16:02:18 +0900

    feat: Implement ProductService & Test

---
+++ week5/kakao-5th-imagepath-start/src/main/java/com/example/kakao/_core/errors/exception/product/option/OptionException.java
@@ -0,0 +1,31 @@
+package com.example.kakao._core.errors.exception.product.option;
+
+import com.example.kakao._core.errors.exception.Exception401;
+import com.example.kakao._core.errors.exception.Exception404;
+
+import java.util.List;
+
+public class OptionException {
+
+    public static class DuplicatedOptionException extends Exception401 {
+        public DuplicatedOptionException() {
+            super("중복된 옵션이 입력되었습니다.");
+        }
+        public DuplicatedOptionException(List<Integer> optionIds) {
+            super("중복된 옵션이 입력되었습니다. : " + optionIds);
+        }
+    }
+
+    public static class OptionNotFoundException extends Exception404 {
+        public OptionNotFoundException() {
+            super("존재하지 않는 옵션입니다.");
+        }
+        public OptionNotFoundException(int id) {
+            super("존재하지 않는 옵션입니다. : " + id);
+        }
+        public OptionNotFoundException(List<Integer> ids) {
+            super("존재하지 않는 옵션입니다. : " + ids);
+        }
+    }
+
+}
