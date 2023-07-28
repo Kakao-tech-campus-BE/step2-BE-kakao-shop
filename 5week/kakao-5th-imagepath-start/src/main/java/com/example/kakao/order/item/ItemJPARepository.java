881d9214831189ae2c965cf6b2b29fe7c412748e
이지민 <min102602@naver.com>
2023-07-28 10:45:09 +0900

    fix : 쿼리 수정

--- 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/item/ItemJPARepository.java
+++ 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/item/ItemJPARepository.java
@@ -9,6 +9,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface ItemJPARepository extends JpaRepository<Item, Integer> {
 
-  @Query("select i from Item i where i.order.id = :orderId order By i.order.id asc")
+  @Query("select i from Item i join fetch i.order where i.order.id = :orderId order By i.order.id asc")
   List<Item> findByOrderId(@Param("orderId") int orderId);
 }
