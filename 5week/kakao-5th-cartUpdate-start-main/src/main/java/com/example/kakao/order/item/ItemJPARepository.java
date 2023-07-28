79c96b3c838adaf40e31b44fa582d51ab8e0adde
jb <wjdqls6704@naver.com>
2023-07-28 15:41:32 +0900

    fix: select query
    fixing with query statement join fetch

--- 5week/kakao-5th-cartUpdate-start-main/src/main/java/com/example/kakao/order/item/ItemJPARepository.java
+++ 5week/kakao-5th-cartUpdate-start-main/src/main/java/com/example/kakao/order/item/ItemJPARepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 public interface ItemJPARepository extends JpaRepository<Item, Integer> {
 
-    @Query("select i from Item i join fetch i.option o where i.order.id = :id")
+    @Query("select i from Item i join fetch i.option o join fetch o.product p where i.order.id = :id")
     List<Item> findAllByOrder(int id);
 }
