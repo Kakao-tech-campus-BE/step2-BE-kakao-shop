45af86443205853964df08909b47cc3891fa6d48
WoeunJung <leena09123@naver.com>
2023-07-28 11:08:42 +0900

    feat: delete carts in CartJPARepository

--- week5/kakao-5th-imagepath-start-main/src/main/java/com/example/kakao/cart/CartJPARepository.java
+++ week5/kakao-5th-imagepath-start-main/src/main/java/com/example/kakao/cart/CartJPARepository.java
@@ -1,6 +1,7 @@
 package com.example.kakao.cart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -13,6 +14,8 @@ public interface CartJPARepository extends JpaRepository<Cart, Integer> {
     @Query("select c from Cart c join fetch c.option o join fetch o.product p where c.user.id = :userId order by c.id asc, c.option.id asc")
     List<Cart> findByUserIdOrderByCartIdOptionIdAsc(int userId);
 
+    @Modifying(clearAutomatically = true)
+    @Query("delete from Cart c where c.user.id = :userId")
     void deleteByUserId(int userId);
 
     @Query("select c from Cart c where c.option.id = :optionId and c.user.id = :userId")
