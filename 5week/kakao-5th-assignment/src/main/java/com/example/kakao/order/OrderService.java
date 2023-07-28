824e5612d290db06748d749cf843e29d9cf0d23f
sososo0 <sh0000@pusan.ac.kr>
2023-07-28 12:41:19 +0900

    refactor: modified save in OrderService

--- 5week/kakao-5th-assignment/src/main/java/com/example/kakao/order/OrderService.java
+++ 5week/kakao-5th-assignment/src/main/java/com/example/kakao/order/OrderService.java
@@ -1,5 +1,6 @@
 package com.example.kakao.order;
 
+import com.example.kakao._core.errors.exception.Exception400;
 import com.example.kakao.cart.Cart;
 import com.example.kakao.cart.CartJPARepository;
 import com.example.kakao.cart.CartResponse;
@@ -23,10 +24,14 @@ public class OrderService {
 
     public OrderResponse.SaveDTO save(User user) {
         List<Cart> orderItemList = cartJPARepository.findAllByUserId(user.getId());
+
+        if (orderItemList.isEmpty()){
+            throw new Exception400("주문할 상품이 존재하지 않습니다. " + orderItemList);
+        }
+
         Order order = Order.builder().user(user).build();
         return new OrderResponse.SaveDTO(order, orderItemList);
     }
-
 
 
 }
