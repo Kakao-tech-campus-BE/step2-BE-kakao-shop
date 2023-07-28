7afa305af479192be3e574b203c80f0892eb7106
Jin_00 <anjdal64@gmail.com>
2023-07-28 12:03:46 +0900

    Controller RequestMapping 진행

--- 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/cart/CartRestController.java
+++ 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/cart/CartRestController.java
@@ -6,16 +6,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/carts")
 public class CartRestController {
 
     private final CartService cartListService;
@@ -34,7 +32,7 @@ public class CartRestController {
      */
     // (기능6) 장바구니 담기 POST
     // /carts/add
-    @PostMapping("/carts/add")
+    @PostMapping("/add")
     public ResponseEntity<?> addCartList(@RequestBody @Valid List<CartRequest.SaveDTO> requestDTOs, Errors errors, @AuthenticationPrincipal CustomUserDetails userDetails) {
         cartListService.addCartList(requestDTOs, userDetails.getUser());
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(null);
@@ -43,14 +41,14 @@ public class CartRestController {
 
     // (기능7) 장바구니 조회 - (주문화면) GET
     // /carts
-    @GetMapping("/carts")
+    @GetMapping("")
     public ResponseEntity<?> findAll(@AuthenticationPrincipal CustomUserDetails userDetails) {
         CartResponse.FindAllDTO responseDTO = cartListService.findAll(userDetails.getUser());
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
         return ResponseEntity.ok(apiResult);
     }
 
-    @GetMapping("/carts/v2")
+    @GetMapping("/v2")
     public ResponseEntity<?> findAllv2(@AuthenticationPrincipal CustomUserDetails userDetails) {
         CartResponse.FindAllDTOv2 responseDTO = cartListService.findAllv2(userDetails.getUser());
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
@@ -72,7 +70,7 @@ public class CartRestController {
      */
 // (기능8) 주문하기 - (주문화면에서 장바구니 수정하기)
 // /carts/update
-@PostMapping("/carts/update")
+@PostMapping("/update")
 public ResponseEntity<?> update(@RequestBody @Valid List<CartRequest.UpdateDTO> requestDTOs, Errors errors, @AuthenticationPrincipal CustomUserDetails userDetails) {
     CartResponse.UpdateDTO responseDTO = cartListService.update(requestDTOs,userDetails.getUser());
     ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
--- 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/OrderRestController.java
+++ 5week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/OrderRestController.java
@@ -7,22 +7,20 @@ import com.example.kakao.order.item.Item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/orders")
 public class OrderRestController {
 
     private final OrderService orderService;
 
     // (기능9) 결재하기 - (주문 인서트) POST
     // /orders/save
-    @PostMapping("/orders/save")
+    @PostMapping("/save")
     public ResponseEntity<?> save(@AuthenticationPrincipal CustomUserDetails userDetails) {
         OrderResponse responseDTO = orderService.saveOrderList(userDetails.getUser().getId());
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
@@ -30,7 +28,7 @@ public class OrderRestController {
 
     // (기능10) 주문 결과 확인 GET
     // /orders/{id}
-    @GetMapping("/orders/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<?> findById(@PathVariable int id, @AuthenticationPrincipal CustomUserDetails userDetails) {
         OrderResponse responseDTO = orderService.findById(id, userDetails.getUser().getId());
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
