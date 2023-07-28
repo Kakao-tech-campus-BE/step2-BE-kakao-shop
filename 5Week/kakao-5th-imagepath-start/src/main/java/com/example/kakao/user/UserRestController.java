b29ea44eb4de845f33e49ebd0436d85584028874
KimSehoon <shggm2000@gmail.com>
2023-07-28 13:54:50 +0900

    refactor: UserRestController
    @RequestMapping 추가

--- 5Week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/user/UserRestController.java
+++ 5Week/kakao-5th-imagepath-start/src/main/java/com/example/kakao/user/UserRestController.java
@@ -7,12 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/auth")
 public class UserRestController {
 
     private final UserService userService;
