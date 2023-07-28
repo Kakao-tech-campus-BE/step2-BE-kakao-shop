50713bae672443fd7a9a3d8013feb042d0a78ee5
zxc88kr <zxc88kr@naver.com>
2023-07-29 04:22:53 +0900

    fix: Add Param Annotation

--- 5week/src/main/java/com/example/kakao/cart/CartJPARepository.java
+++ 5week/src/main/java/com/example/kakao/cart/CartJPARepository.java
@@ -2,12 +2,13 @@ package com.example.kakao.cart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CartJPARepository extends JpaRepository<Cart, Integer> {
-    List<Cart> findAllByUserId(int userId);
+    List<Cart> findAllByUserId(@Param("userId") int userId);
 
     @Query("select c from Cart c where c.user.id = :userId order by c.option.id asc")
-    List<Cart> findByUserIdOrderByOptionIdAsc(int userId);
+    List<Cart> findByUserIdOrderByOptionIdAsc(@Param("userId") int userId);
 }
--- 5week/src/main/java/com/example/kakao/user/UserJPARepository.java
+++ 5week/src/main/java/com/example/kakao/user/UserJPARepository.java
@@ -1,9 +1,10 @@
 package com.example.kakao.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserJPARepository extends JpaRepository<User, Integer> {
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(@Param("email") String email);
 }
