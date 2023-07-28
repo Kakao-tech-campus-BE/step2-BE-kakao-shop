9c29642c12eba20ff401f71c61edadd1f873ce4e
jinc20 <wh5971@pusan.ac.kr>
2023-07-28 14:14:00 +0900

    OrderService 예외처리

--- 5week/kakao-5th-exhandler-start/src/main/java/com/example/kakao/order/OrderRestController.java
+++ 5week/kakao-5th-exhandler-start/src/main/java/com/example/kakao/order/OrderRestController.java
@@ -2,39 +2,28 @@ package com.example.kakao.order;
 
 import com.example.kakao._core.security.CustomUserDetails;
 import com.example.kakao._core.utils.ApiUtils;
-import com.example.kakao.cart.Cart;
-import com.example.kakao.cart.CartService;
-import com.example.kakao.order.item.Item;
-import com.example.kakao.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import java.util.List;
-
 @RequiredArgsConstructor
 @RestController
 public class OrderRestController {
-
-    private final CartService cartService;
     private final OrderService orderService;
 
 
     // (기능9) 결재하기 - (주문 인서트) POST
-    // /orders/save
     @PostMapping("orders/save")
     public ResponseEntity<?> save(Error errors, @AuthenticationPrincipal CustomUserDetails userDetails) {
-        OrderResponse.SaveDTO responseDTO = orderService.save(userDetails.getUser());
+        OrderResponse.SaveDTO responseDTO = orderService.saveOrder(userDetails.getUser());
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
 
     // (기능10) 주문 결과 확인 GET
-    // /orders/{id}
     @GetMapping("orders/{id}")
     public ResponseEntity<?> findById(Error errors, @AuthenticationPrincipal CustomUserDetails userDetails, @PathVariable int id) {
-        OrderResponse.FindByIdDTO responseDTO = orderService.findById(userDetails.getUser(), id);
+        OrderResponse.FindByIdDTO responseDTO = orderService.findOrderById(userDetails.getUser(), id);
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
 
--- 5week/kakao-5th-exhandler-start/src/main/java/com/example/kakao/order/OrderService.java
+++ 5week/kakao-5th-exhandler-start/src/main/java/com/example/kakao/order/OrderService.java
@@ -1,20 +1,18 @@
 package com.example.kakao.order;
 
+import com.example.kakao._core.errors.exception.Exception403;
 import com.example.kakao._core.errors.exception.Exception404;
+import com.example.kakao._core.errors.exception.Exception500;
 import com.example.kakao.cart.Cart;
 import com.example.kakao.cart.CartJPARepository;
 import com.example.kakao.order.item.Item;
 import com.example.kakao.order.item.ItemJPARepository;
-import com.example.kakao.product.ProductJPARepository;
-import com.example.kakao.product.option.OptionJPARepository;
 import com.example.kakao.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -25,32 +23,41 @@ public class OrderService {
     private final ItemJPARepository itemJPARepository;
 
     @Transactional
-    public OrderResponse.SaveDTO save(User user) {
+    public OrderResponse.SaveDTO saveOrder(User user) {
         List<Cart> carts = cartJPARepository.findByUserIdOrderByOptionIdAsc(user.getId());
-        List<Item> items = new ArrayList<>();
+        if (carts.isEmpty()) throw new Exception404("장바구니에서 주문할 상품이 없습니다.");
 
         Order order = Order.builder().user(user).build();
-        orderJPARepository.save(order);
-
-        for (Cart cart : carts) {
-            Item item = Item.builder()
-                    .option(cart.getOption())
-                    .order(order)
-                    .quantity(cart.getQuantity())
-                    .price(cart.getPrice())
-                    .build();
-            items.add(item);
+        try { orderJPARepository.saveAndFlush(order); } catch (Exception e) {
+            throw new Exception500("주문을 저장하는 과정에서 오류가 발생했습니다");
         }
 
-        itemJPARepository.saveAll(items);
-        cartJPARepository.deleteByUserId(user.getId());
+        List<Item> items = carts.stream()
+                .map(cart -> Item.builder()
+                        .order(order)
+                        .option(cart.getOption())
+                        .quantity(cart.getQuantity())
+                        .price(cart.getPrice())
+                        .build()).collect(Collectors.toList());
 
+        try { itemJPARepository.saveAllAndFlush(items); } catch (Exception e) {
+            throw new Exception500("주문 상품을 저장하는 과정에서 오류가 발생했습니다.");
+        }
+        try { cartJPARepository.deleteByUserId(user.getId()); } catch (Exception e) {
+            throw new Exception500("주문한 장바구니를 비우는 과정에서 오류가 발생했습니다.");
+        }
         return new OrderResponse.SaveDTO(order, items);
     }
 
-    public OrderResponse.FindByIdDTO findById(User user, int id) {
-        List<Item> items = itemJPARepository.findByOrderId(id);
+    public OrderResponse.FindByIdDTO findOrderById(User user, int id) {
+
         Order order = orderJPARepository.findById(id).orElseThrow(() -> new Exception404("해당 주문을 찾을 수 없습니다."));
+
+        if (order.getUser().getId() != user.getId()) throw new Exception403("주문에 접근 권한이 없습니다.");
+
+        List<Item> items = itemJPARepository.findByOrderId(id);
+        if (items.isEmpty()) throw new Exception404("해당 주문의 상품을 찾을 수 없습니다.");
+
         return new OrderResponse.FindByIdDTO(order, items);
     }
 }
