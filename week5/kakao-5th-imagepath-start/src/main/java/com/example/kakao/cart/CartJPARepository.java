f44b51781d427fceba3fc6d747ac84e97177f447
CheongEun <ce090404@naver.com>
2023-07-28 17:57:57 +0900

    base code 구현

--- week5/kakao-5th-imagepath-start/src/main/java/com/example/kakao/cart/CartJPARepository.java
+++ week5/kakao-5th-imagepath-start/src/main/java/com/example/kakao/cart/CartJPARepository.java
@@ -9,11 +9,17 @@ import java.util.Optional;
 
 
 public interface CartJPARepository extends JpaRepository<Cart, Integer> {
+
+    // @Query(value = "insert into cart_tb(user_id, option_id, quantity, price) values(:userId, :optionId, :quantity, :price)", nativeQuery = true)
+    // void mSave(@Param("userId") int userId, @Param("optionId") int optionId, @Param("quantity") int quantity, @Param("price") int price);
+
     List<Cart> findAllByUserId(int userId);
 
-    @Query("select c from Cart c where c.user.id = :userId order by c.option.id asc")
+    @Query("select c from Cart c join fetch c.option o join fetch o.product p where c.user.id = :userId order by c.option.id asc")
     List<Cart> findByUserIdOrderByOptionIdAsc(int userId);
 
+    // @Query("delete from Cart c where c.user.id = :userId")
+    // 위의 Query를 사용하면 500 error 발생..
     void deleteByUserId(int userId);
 
     @Query("select c from Cart c where c.option.id = :optionId and c.user.id = :userId")
