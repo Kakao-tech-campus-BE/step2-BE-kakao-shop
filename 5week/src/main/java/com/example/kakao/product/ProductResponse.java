902bee633793b830f09702c28ecfa49e26cfe6c2
zxc88kr <zxc88kr@naver.com>
2023-07-28 19:44:36 +0900

    fix: Delete Unused Code of Product

--- 5week/src/main/java/com/example/kakao/product/ProductResponse.java
+++ 5week/src/main/java/com/example/kakao/product/ProductResponse.java
@@ -35,41 +35,7 @@ public class ProductResponse {
         private int starCount; // 0~5
         private List<OptionDTO> options;
 
-        public FindByIdDTO(Product product, List<Option> optionList) {
-            this.id = product.getId();
-            this.productName = product.getProductName();
-            this.description = product.getDescription();
-            this.image = product.getImage();
-            this.price = product.getPrice();
-            this.starCount = 5; // 임시로 추가해둠 (요구사항에는 없음)
-            this.options = optionList.stream().map(OptionDTO::new).collect(Collectors.toList());
-        }
-
-        @Getter @Setter
-        public class OptionDTO {
-            private int id;
-            private String optionName;
-            private int price;
-
-            public OptionDTO(Option option) {
-                this.id = option.getId();
-                this.optionName = option.getOptionName();
-                this.price = option.getPrice();
-            }
-        }
-    }
-
-    @Getter @Setter
-    public static class FindByIdDTOv2 {
-        private int id;
-        private String productName;
-        private String description;
-        private String image;
-        private int price;
-        private int starCount; // 0~5
-        private List<OptionDTO> options;
-
-        public FindByIdDTOv2(List<Option> optionList) {
+        public FindByIdDTO(List<Option> optionList) {
             this.id = optionList.get(0).getProduct().getId();
             this.productName = optionList.get(0).getProduct().getProductName();
             this.description = optionList.get(0).getProduct().getDescription();
