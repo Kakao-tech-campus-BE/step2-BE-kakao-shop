ed14b1e1ea5e75b7f3225676e70370a218bfbbe9
Cheoroo <195822@jnu.ac.kr>
2023-07-28 23:46:18 +0900

    feat 글로벌 에외처리 에러 로그 기록하도록 수정

--- 5week/kakao-5th-imagepath-start-main/src/main/java/com/example/kakao/_core/errors/GlobalExceptionHandler.java
+++ 5week/kakao-5th-imagepath-start-main/src/main/java/com/example/kakao/_core/errors/GlobalExceptionHandler.java
@@ -7,8 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
 
     @ExceptionHandler(Exception400.class)
     public ResponseEntity<?> badRequest(Exception400 e){
@@ -32,11 +38,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception500.class)
     public ResponseEntity<?> serverError(Exception500 e){
+        log.error("Server error: ", e);  // 로그 기록
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> unknownServerError(Exception e){
+        log.error("Unknown server error: ", e);  // 로그 기록
         ApiUtils.ApiResult<?> apiResult = ApiUtils.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(apiResult, HttpStatus.INTERNAL_SERVER_ERROR);
     }
