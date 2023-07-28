86e96defd295156c09e15caf21a47a7189be91ac
Cheoroo <195822@jnu.ac.kr>
2023-07-28 23:47:50 +0900

    feat CartJPARepository.java 쿼리 업데이트

--- 5week/kakao-5th-imagepath-start-main/src/main/java/com/example/kakao/cart/CartJPARepository.java
+++ 5week/kakao-5th-imagepath-start-main/src/main/java/com/example/kakao/cart/CartJPARepository.java
@@ -1,6 +1,7 @@
 package com.example.kakao.cart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -9,12 +10,26 @@ import java.util.Optional;
 
 
 public interface CartJPARepository extends JpaRepository<Cart, Integer> {
-    List<Cart> findAllByUserId(int userId);
+
+    @Query("select c from Cart c join fetch c.option o join fetch o.product where c.user.id = :userId")
+    List<Cart> findAllByUserId(@Param("userId") int userId);
+
+    @Query("SELECT c FROM Cart c JOIN FETCH c.option o JOIN FETCH o.product p WHERE c.user.id = :userId")
+    List<Cart> findCartByUserId(@Param("userId") int userId);
 
     @Query("select c from Cart c where c.user.id = :userId order by c.option.id asc")
     List<Cart> findByUserIdOrderByOptionIdAsc(int userId);
 
+    //@Query("SELECT c FROM Cart c JOIN FETCH c.option o JOIN FETCH c.user u WHERE c.user.id = :userId ORDER BY c.option.id ASC")
+    //List<Cart> findWithOptionAndUserByUserIdOrderByOptionIdAsc(@Param("userId") int userId);
+    //메모리 먹는거 때매 더 느릴듯..
+
     void deleteByUserId(int userId);
+
+    @Modifying
+    @Query("UPDATE Cart c SET c.quantity = :quantity, c.price = :price WHERE c.id = :id")
+    void updateQuantityAndPriceById(@Param("id") int id, @Param("quantity") int quantity, @Param("price") int price);
+
 
     @Query("select c from Cart c where c.option.id = :optionId and c.user.id = :userId")
     Optional<Cart> findByOptionIdAndUserId(@Param("optionId") int optionId, @Param("userId") int userId);
