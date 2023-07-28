a2027f1810446de98f6972b6297c19cffbce1541
goberomsu <gobeumsu@gmail.com>
2023-07-28 10:56:37 +0900

    Fix : typo

--- week5/src/main/java/com/example/kakao/cart/CartRestController.java
+++ week5/src/main/java/com/example/kakao/cart/CartRestController.java
@@ -1,5 +1,7 @@
 package com.example.kakao.cart;
 
+import com.example.kakao._core.errors.GlobalExceptionHandler;
+import com.example.kakao._core.errors.exception.Exception400;
 import com.example.kakao._core.security.CustomUserDetails;
 import com.example.kakao._core.utils.ApiUtils;
 import com.example.kakao.order.OrderResponse;
@@ -15,17 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
 public class CartRestController {
 
     private final CartService cartService;
-    private final OrderService orderService;
+
     // (기능6) 장바구니 담기 POST
     // /carts/add
     @PostMapping("/carts/add")
     public ResponseEntity<?> addCartList(@RequestBody @Valid List<CartRequest.SaveDTO> requestDTOs, Errors errors, @AuthenticationPrincipal CustomUserDetails userDetails) {
+
         cartService.addCartList(requestDTOs, userDetails.getUser());
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(null);
         return ResponseEntity.ok(apiResult);
@@ -33,16 +37,10 @@ public class CartRestController {
 
     @GetMapping("/carts")
     public ResponseEntity<?> findAll(@AuthenticationPrincipal CustomUserDetails userDetails) {
+
         CartResponse.FindAllDTO responseDTO = cartService.findAll(userDetails.getUser());
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
         return ResponseEntity.ok(apiResult);
-    }
-
-    @GetMapping("/carts/v2")
-    public ResponseEntity<?> findAllv2(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        CartResponse.FindAllDTOv2 responseDTO = cartService.findAllv2(userDetails.getUser());
-
-        return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
 
 
@@ -50,6 +48,7 @@ public class CartRestController {
     // /carts/update
     @PostMapping("/carts/update")
     public ResponseEntity<?> update(@RequestBody @Valid List<CartRequest.UpdateDTO> requestDTOs, Errors errors, @AuthenticationPrincipal CustomUserDetails userDetails) {
+
         CartResponse.UpdateDTO responseDTO = cartService.update(requestDTOs,userDetails.getUser());
         ApiUtils.ApiResult<?> apiResult = ApiUtils.success(responseDTO);
         return ResponseEntity.ok(apiResult);
