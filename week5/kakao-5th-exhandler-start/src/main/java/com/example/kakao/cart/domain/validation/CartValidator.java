3151fab92b5f781b1e518cb0504d26b5f50347d8
kssumin <201566@jnu.ac.kr>
2023-07-29 00:00:49 +0900

    fix : cartRequest dto 명 변경

--- week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/domain/validation/CartValidator.java
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/domain/validation/CartValidator.java
@@ -1,7 +1,6 @@
 package com.example.kakao.cart.domain.validation;
 
 import com.example.kakao.cart.web.request.CartReqeust;
-import com.example.kakao.product.domain.service.ProductOptionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -14,21 +13,18 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Slf4j
 public class CartValidator {
-    private final ProductOptionRepository productOptionRepository;
 
-    public void validateCreateConstraint(final List<CartReqeust> cartReqeusts) {
-        isDuplicated(cartReqeusts);
+    public void validateCreateConstraint(final List<CartReqeust> cartRequests) {
+        isDuplicated(cartRequests);
 
     }
-    private void isDuplicated(List<CartReqeust>cartReqeusts){
-        Set<Long> notDuplicated = cartReqeusts.stream()
+    private void isDuplicated(List<CartReqeust>cartRequests){
+        Set<Long> notDuplicated = cartRequests.stream()
                 .map(CartReqeust::getOptionId)
                 .collect(Collectors.toSet());
 
-        if(notDuplicated.size()!= cartReqeusts.size()){
+        if(notDuplicated.size()!= cartRequests.size()){
             throw new IllegalArgumentException("장바구니 요청에 동일한 옵션이 들어있습니다.");
         }
     }
-
-
 }
