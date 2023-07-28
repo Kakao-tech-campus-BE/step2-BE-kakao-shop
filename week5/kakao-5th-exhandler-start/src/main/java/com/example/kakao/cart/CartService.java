c1d89e154af679977059e74c2a44f57bdd43d488
sjmjys954646 <sjmjys954646@gmail.com>
2023-07-28 13:04:04 +0900

    장바구니 수정

--- week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/CartService.java
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/CartService.java
@@ -26,10 +26,26 @@ public class CartService {
         List<Cart> cartList = cartJPARepository.findAllByUserId(user.getId());
 
         // 1. 유저 장바구니에 아무것도 없으면 예외처리
+        if(cartList.isEmpty())
+        {
+            throw new Exception400("업데이트할 장바구니가 비어있습니다.");
+        }
 
         // 2. cartId:1, cartId:1 이렇게 requestDTOs에 동일한 장바구니 아이디가 두번 들어오면 예외처리
+        Set<Integer> requestDTOSet = requestDTOs.stream().map(id -> id.getCartId()).collect(Collectors.toSet());
+        if(requestDTOSet.size() != requestDTOs.size())
+        {
+            throw new Exception400("동일한 장바구니 아이디가 있습니다.");
+        }
 
         // 3. 유저 장바구니에 없는 cartId가 들어오면 예외처리
+        for(Cart cart : cartList)
+        {
+            if(!requestDTOs.stream().anyMatch(id -> id.getCartId() == cart.getId()))
+            {
+                throw new Exception400("장바구니에 없는 cartId가 있습니다.");
+            }
+        }
 
         // 위에 3개를 처리하지 않아도 프로그램은 잘돌아간다. 예를 들어 1번을 처리하지 않으면 for문을 돌지 않고, cartList가 빈배열 []로 정상응답이 나감.
         for (Cart cart : cartList) {
