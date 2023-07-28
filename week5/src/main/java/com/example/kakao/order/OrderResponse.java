b6b200e6465f262ab234d0e8de9ce84e5d8f37de
suuding <tngus4789@naver.com>
2023-07-28 16:25:33 +0900

    feat : 주문하기(결제) 기능 구현

--- week5/src/main/java/com/example/kakao/order/OrderResponse.java
+++ week5/src/main/java/com/example/kakao/order/OrderResponse.java
@@ -1,6 +1,59 @@
 package com.example.kakao.order;
 
+import com.example.kakao.order.item.Item;
+import com.example.kakao.product.Product;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class OrderResponse {
 
+    @Getter
+    @Setter
+    public static class SaveDTO {
+        private int id;
+        private List<ProductDTO> products;
+        private int totalPrice;
 
+        public SaveDTO(Order order, List<Item> items) {
+            this.id = order.getId();
+            this.products = items.stream()
+                    .map(cart -> cart.getOption().getProduct()).distinct()
+                    .map(product -> new SaveDTO.ProductDTO(product, items))
+                    .collect(Collectors.toList());
+            this.totalPrice = items.stream()
+                    .mapToInt(cart -> cart.getOption().getPrice()*cart.getQuantity()).sum();
+        }
+
+        @Getter @Setter
+        public class ProductDTO {
+            private String productName;
+            private List<ItemDTO> items;
+
+            public ProductDTO(Product product, List<Item> items) {
+                this.productName = product.getProductName();
+                this.items = items.stream()
+                        .filter(cart -> cart.getOption().getProduct().getId()==product.getId())
+                        .map(ItemDTO::new)
+                        .collect(Collectors.toList());
+            }
+
+            @Getter @Setter
+            public class ItemDTO {
+                private int id;
+                private String optionName;
+                private int quantity;
+                private int price;
+
+                public ItemDTO(Item items) {
+                    this.id = items.getOption().getId();
+                    this.optionName = items.getOption().getOptionName();
+                    this.quantity = items.getQuantity();
+                    this.price = items.getOption().getPrice()*items.getQuantity();
+                }
+            }
+        }
+    }
 }
--- week5/src/main/java/com/example/kakao/order/OrderRestController.java
+++ week5/src/main/java/com/example/kakao/order/OrderRestController.java
@@ -1,11 +1,14 @@
 package com.example.kakao.order;
 
+import com.example.kakao._core.security.CustomUserDetails;
 import com.example.kakao._core.utils.ApiUtils;
 import com.example.kakao.order.item.Item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,11 +17,15 @@ import java.util.List;
 @RestController
 public class OrderRestController {
 
+    private final OrderService orderService;
 
     // (기능9) 결재하기 - (주문 인서트) POST
     // /orders/save
-    public void save() {
+    @PostMapping("/orders/save")
+    public ResponseEntity<?> save(@AuthenticationPrincipal CustomUserDetails userDetails) {
 
+        OrderResponse.SaveDTO responseDTO = orderService.save(userDetails.getUser());
+        return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
 
     // (기능10) 주문 결과 확인 GET
