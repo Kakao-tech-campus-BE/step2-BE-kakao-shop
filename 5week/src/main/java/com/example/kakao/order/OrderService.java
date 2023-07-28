08e1c98d68249c904d61331ad23b17f08cf4cb8c
yunzae <dsa05037@naver.com>
2023-07-28 19:32:47 +0900

    feat: Complete week5 assign

---
+++ 5week/src/main/java/com/example/kakao/order/OrderService.java
@@ -0,0 +1,62 @@
+package com.example.kakao.order;
+
+import com.example.kakao._core.errors.exception.Exception400;
+import com.example.kakao.cart.Cart;
+import com.example.kakao.cart.CartJPARepository;
+import com.example.kakao.order.item.Item;
+import com.example.kakao.order.item.ItemJPARepository;
+import com.example.kakao.user.User;
+import com.example.kakao.user.UserJPARepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class OrderService {
+    private final UserJPARepository userJPARepository;
+    private final OrderJPARepository orderJPARepository;
+    private final CartJPARepository cartJPARepository;
+    private final ItemJPARepository itemJPARepository;
+
+    public OrderService(UserJPARepository userJPARepository, OrderJPARepository orderJPARepository, CartJPARepository cartJPARepository, ItemJPARepository itemJPARepository) {
+        this.userJPARepository = userJPARepository;
+        this.orderJPARepository = orderJPARepository;
+        this.cartJPARepository = cartJPARepository;
+        this.itemJPARepository = itemJPARepository;
+    }
+
+    @Transactional
+    public OrderResponse.SaveDTO save(int userId) {
+        List<Cart> carts = cartJPARepository.mFindAllByUserId(userId); // 상품,옵션,카트,유저 페치조인
+        if (carts.isEmpty()){throw new Exception400("해당 유저의 장바구니가 비어있습니다.");}
+        Order order = Order.builder().user(carts.get(0).getUser()).build();
+        orderJPARepository.save(order);
+        List<Item> items = new ArrayList<>();
+        for (Cart cart : carts) {
+            Item item = Item.builder().option(cart.getOption()).quantity(cart.getQuantity()).order(order).price(cart.getPrice()).build();
+            items.add(item);
+        }
+        itemJPARepository.saveAll(items);
+        cartJPARepository.deleteAllByUserId(userId); // 주문 후 장바구니 비워주기
+        return new OrderResponse.SaveDTO(order, items);
+    }
+
+    public OrderResponse.FindByIdDTO findById(int orderId, int userId) {
+        List<Item> items = itemJPARepository.mFindAllByOrderId(orderId);
+        if (items.isEmpty()) {
+            throw new Exception400("주문목록이 없습니다.");
+        }
+        if (items.get(0).getOrder().getId() != userId) {
+            throw new Exception400("요청한 유저의 주문이 아닙니다.");
+        }
+        return new OrderResponse.FindByIdDTO(items.get(0).getOrder(), items);
+
+    }
+
+
+}
+
+
---
+++ 5week/src/test/java/com/example/kakao/product/ProductRestControllerTest.java
@@ -0,0 +1,116 @@
+package com.example.kakao.product;
+
+import com.example.kakao._core.errors.GlobalExceptionHandler;
+import com.example.kakao._core.security.SecurityConfig;
+import com.example.kakao._core.util.DummyEntity;
+import com.example.kakao.log.ErrorLogJPARepository;
+import com.example.kakao.product.option.Option;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+
+@Import({
+        GlobalExceptionHandler.class,
+        SecurityConfig.class
+})
+@WebMvcTest(controllers = {ProductRestController.class})
+class ProductRestControllerTest extends DummyEntity {
+
+
+    @MockBean
+    private ErrorLogJPARepository errorLogJPARepository;
+
+    @MockBean
+    ProductService productService;
+    @Autowired
+    private MockMvc mvc;
+
+
+    @Autowired
+    private ObjectMapper om;
+    @Test
+    @DisplayName("post - /products 전체 상품 조회")
+    void findAll() throws Exception {
+        // given
+        int page = 0;
+        // stub
+        List<Product> productDummy = productDummyList();
+        List<ProductResponse.FindAllDTO> responseDTO = productDummy.stream().skip(page*9).limit(9).map(p -> new ProductResponse.FindAllDTO(p)).collect(Collectors.toList());
+        Mockito.when(productService.findAll(anyInt())).thenReturn(responseDTO);
+        String DTOjson = om.writeValueAsString(responseDTO);
+        System.out.println("테스트 : "+DTOjson);
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .get("/products")
+                        .param("page", Objects.toString(page))
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : "+responseBody);
+
+        // then
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response[0].productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response[0].image").value("/images/1.jpg"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response[0].price").value(1000));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.size()").value(9));
+
+
+
+
+    }
+    @Test
+    @DisplayName("get - /products/{id} 개별 상품 조회")
+    void findById() throws Exception {
+        // given
+        String productid = "1";
+        // stub
+        List<Product> productDummy = productDummyList();
+        List<Option> optionDummy = optionDummyList(productDummy);
+        List<Option> filterdOptions = optionDummy.stream().filter(o-> o.getProduct().getProductName().equals("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전")).collect(Collectors.toList());
+        ProductResponse.FindByIdDTO responseDTO = new ProductResponse.FindByIdDTO(productDummy.get(0),filterdOptions);
+        Mockito.when(productService.findById(anyInt())).thenReturn(responseDTO);
+        String DTOjson = om.writeValueAsString(responseDTO);
+        System.out.println("테스트 : "+DTOjson);
+        // when
+        ResultActions result = mvc.perform(
+                MockMvcRequestBuilders
+                        .get("/products/"+productid)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : "+responseBody);
+
+        // then
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.image").value("/images/1.jpg"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.price").value(1000));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.starCount").value(5));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.options[0].optionName").value("01. 슬라이딩 지퍼백 크리스마스에디션 4종"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.response.options.size()").value(5));
+
+    }
+
+
+
+
+}
