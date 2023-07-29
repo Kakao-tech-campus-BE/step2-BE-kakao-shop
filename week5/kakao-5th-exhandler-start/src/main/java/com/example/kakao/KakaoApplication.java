06c9f42043fe4a70eae4a911a7b7554d62e99d02
kssumin <201566@jnu.ac.kr>
2023-07-29 20:15:38 +0900

    feat : initial data 세팅

--- week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/KakaoApplication.java
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/KakaoApplication.java
@@ -1,9 +1,9 @@
 package com.example.kakao;
 
-import com.example.kakao.product.Product;
-import com.example.kakao.product.ProductJPARepository;
-import com.example.kakao.product.option.Option;
-import com.example.kakao.product.option.OptionJPARepository;
+import com.example.kakao.product.entity.ProductEntity;
+import com.example.kakao.product.entity.ProductOptionEntity;
+import com.example.kakao.product.repository.ProductJpaRepository;
+import com.example.kakao.product.repository.ProductOptionJpaRepository;
 import com.example.kakao.user.User;
 import com.example.kakao.user.UserJPARepository;
 import org.springframework.boot.CommandLineRunner;
@@ -25,7 +25,7 @@ public class KakaoApplication {
 
 	@Profile("local")
 	@Bean
-	CommandLineRunner localServerStart(UserJPARepository userJPARepository, ProductJPARepository productJPARepository, OptionJPARepository optionJPARepository, PasswordEncoder passwordEncoder){
+	CommandLineRunner localServerStart(UserJPARepository userJPARepository, ProductJpaRepository productJPARepository, ProductOptionJpaRepository optionJPARepository, PasswordEncoder passwordEncoder){
 		return args -> {
 			userJPARepository.saveAll(Arrays.asList(newUser("ssarmango", passwordEncoder)));
 			productJPARepository.saveAll(Arrays.asList(
@@ -46,54 +46,54 @@ public class KakaoApplication {
 					newProduct("[LIVE][5%쿠폰] 홈카페 Y3.3 캡슐머신 베이직 세트", 15, 148000)
 			));
 			optionJPARepository.saveAll(Arrays.asList(
-					newOption(Product.builder().id(1).build(), "01. 슬라이딩 지퍼백 크리스마스에디션 4종", 10000),
-					newOption(Product.builder().id(1).build(), "02. 슬라이딩 지퍼백 플라워에디션 5종", 10900),
-					newOption(Product.builder().id(1).build(), "고무장갑 베이지 S(소형) 6팩", 9900),
-					newOption(Product.builder().id(1).build(), "뽑아쓰는 키친타올 130매 12팩", 16900),
-					newOption(Product.builder().id(1).build(), "2겹 식빵수세미 6매", 8900),
-					newOption(Product.builder().id(2).build(), "22년산 햇단밤 700g(한정판매)", 9900),
-					newOption(Product.builder().id(2).build(), "22년산 햇단밤 1kg(한정판매)", 14500),
-					newOption(Product.builder().id(2).build(), "밤깎기+다회용 구이판 세트", 5500),
-					newOption(Product.builder().id(3).build(), "JR310 (유선 전용) - 블루", 29900),
-					newOption(Product.builder().id(3).build(), "JR310BT (무선 전용) - 레드", 49900),
-					newOption(Product.builder().id(3).build(), "JR310BT (무선 전용) - 그린", 49900),
-					newOption(Product.builder().id(3).build(), "JR310BT (무선 전용) - 블루", 49900),
-					newOption(Product.builder().id(3).build(), "T510BT (무선 전용) - 블랙", 52900),
-					newOption(Product.builder().id(3).build(), "T510BT (무선 전용) - 화이트", 52900),
-					newOption(Product.builder().id(4).build(), "선택01_바른곡물효소 누룽지맛 2박스", 17900), //15
-					newOption(Product.builder().id(4).build(), "선택02_바른곡물효소누룽지맛 6박스", 50000),
-					newOption(Product.builder().id(4).build(), "선택03_바른곡물효소3박스+유산균효소3박스", 50000),
-					newOption(Product.builder().id(4).build(), "선택04_바른곡물효소3박스+19종유산균3박스", 50000),
-					newOption(Product.builder().id(5).build(), "01. 말랑컷팅장족 100g", 4900),
-					newOption(Product.builder().id(5).build(), "02. 말랑컷팅장족 300g", 12800),
-					newOption(Product.builder().id(5).build(), "03. 눌린장족 100g", 4900),
-					newOption(Product.builder().id(6).build(), "굳지않는 쑥 앙금 절편 1050g", 15900),
-					newOption(Product.builder().id(6).build(), "굳지않는 흑미 앙금 절편 1050g", 15900),
-					newOption(Product.builder().id(6).build(), "굳지않는 흰 가래떡 1050g", 15900),
-					newOption(Product.builder().id(7).build(), "이너딜리티 1박스", 26800), //25
-					newOption(Product.builder().id(7).build(), "이너딜리티 2박스+사은품 2종", 49800),
-					newOption(Product.builder().id(8).build(), "제나벨 PDRN 자생크림 1+1", 25900),
-					newOption(Product.builder().id(9).build(), "플레이스테이션 VR2 호라이즌 번들", 839000),
-					newOption(Product.builder().id(9).build(), "플레이스테이션 VR2", 797000),
-					newOption(Product.builder().id(10).build(), "홍가리비2kg(50미이내)", 8900), //30
-					newOption(Product.builder().id(11).build(), "궁채 절임 1kg", 6900),
-					newOption(Product.builder().id(11).build(), "양념 깻잎 1kg", 8900),
-					newOption(Product.builder().id(11).build(), "된장 깻잎 1kg", 8900),
-					newOption(Product.builder().id(11).build(), "간장 깻잎 1kg", 7900),
-					newOption(Product.builder().id(11).build(), "고추 무침 1kg", 8900),
-					newOption(Product.builder().id(11).build(), "파래 무침 1kg", 9900),
-					newOption(Product.builder().id(12).build(), "01_순수소프트 27m 30롤 2팩", 28900),
-					newOption(Product.builder().id(12).build(), "02_벚꽃 프리미엄 27m 30롤 2팩", 32900),
-					newOption(Product.builder().id(13).build(), "(증정) 초미세모 칫솔 12개 x 2개", 11900),
-					newOption(Product.builder().id(13).build(), "(증정) 잇몸케어 치약 100G 3개 x 2개", 16900),
-					newOption(Product.builder().id(13).build(), "(증정) 구취케어 치약 100G 3개 x 2개", 16900),
-					newOption(Product.builder().id(13).build(), "(증정)화이트케어 치약 100G 3개 x 2개", 19900),
-					newOption(Product.builder().id(13).build(), "(증정) 어린이 칫솔 12EA", 9900),
-					newOption(Product.builder().id(14).build(), "[가정용] 샤인머스켓 1kg 2수내외", 9900),
-					newOption(Product.builder().id(14).build(), "[특품] 샤인머스켓 1kg 1-2수", 12900), //45
-					newOption(Product.builder().id(14).build(), "[특품] 샤인머스켓 2kg 2-3수", 23900),
-					newOption(Product.builder().id(15).build(), "화이트", 148000),
-					newOption(Product.builder().id(15).build(), "블랙", 148000)
+					newOption(ProductEntity.builder().productId(1L).build(), "02. 슬라이딩 지퍼백 플라워에디션 5종", 10900),
+					newOption(ProductEntity.builder().productId(1L).build(), "고무장갑 베이지 S(소형) 6팩", 9900),
+					newOption(ProductEntity.builder().productId(1L).build(), "뽑아쓰는 키친타올 130매 12팩", 16900),
+					newOption(ProductEntity.builder().productId(1L).build(), "2겹 식빵수세미 6매", 8900),
+					newOption(ProductEntity.builder().productId(1L).build(), "01. 슬라이딩 지퍼백 크리스마스에디션 4종", 10000),
+					newOption(ProductEntity.builder().productId(2L).build(), "22년산 햇단밤 700g(한정판매)", 9900),
+					newOption(ProductEntity.builder().productId(2L).build(), "22년산 햇단밤 1kg(한정판매)", 14500),
+					newOption(ProductEntity.builder().productId(2L).build(), "밤깎기+다회용 구이판 세트", 5500),
+					newOption(ProductEntity.builder().productId(3L).build(), "JR310 (유선 전용) - 블루", 29900),
+					newOption(ProductEntity.builder().productId(3L).build(), "JR310BT (무선 전용) - 레드", 49900),
+					newOption(ProductEntity.builder().productId(3L).build(), "JR310BT (무선 전용) - 그린", 49900),
+					newOption(ProductEntity.builder().productId(3L).build(), "JR310BT (무선 전용) - 블루", 49900),
+					newOption(ProductEntity.builder().productId(3L).build(), "T510BT (무선 전용) - 블랙", 52900),
+					newOption(ProductEntity.builder().productId(3L).build(), "T510BT (무선 전용) - 화이트", 52900),
+					newOption(ProductEntity.builder().productId(4L).build(), "선택01_바른곡물효소 누룽지맛 2박스", 17900), //15
+					newOption(ProductEntity.builder().productId(4L).build(), "선택02_바른곡물효소누룽지맛 6박스", 50000),
+					newOption(ProductEntity.builder().productId(4L).build(), "선택03_바른곡물효소3박스+유산균효소3박스", 50000),
+					newOption(ProductEntity.builder().productId(4L).build(), "선택04_바른곡물효소3박스+19종유산균3박스", 50000),
+					newOption(ProductEntity.builder().productId(5L).build(), "01. 말랑컷팅장족 100g", 4900),
+					newOption(ProductEntity.builder().productId(5L).build(), "02. 말랑컷팅장족 300g", 12800),
+					newOption(ProductEntity.builder().productId(5L).build(), "03. 눌린장족 100g", 4900),
+					newOption(ProductEntity.builder().productId(6L).build(), "굳지않는 쑥 앙금 절편 1050g", 15900),
+					newOption(ProductEntity.builder().productId(6L).build(), "굳지않는 흑미 앙금 절편 1050g", 15900),
+					newOption(ProductEntity.builder().productId(6L).build(), "굳지않는 흰 가래떡 1050g", 15900),
+					newOption(ProductEntity.builder().productId(7L).build(), "이너딜리티 1박스", 26800), //25
+					newOption(ProductEntity.builder().productId(7L).build(), "이너딜리티 2박스+사은품 2종", 49800),
+					newOption(ProductEntity.builder().productId(8L).build(), "제나벨 PDRN 자생크림 1+1", 25900),
+					newOption(ProductEntity.builder().productId(9L).build(), "플레이스테이션 VR2 호라이즌 번들", 839000),
+					newOption(ProductEntity.builder().productId(9L).build(), "플레이스테이션 VR2", 797000),
+					newOption(ProductEntity.builder().productId(10L).build(), "홍가리비2kg(50미이내)", 8900), //30
+					newOption(ProductEntity.builder().productId(11L).build(), "궁채 절임 1kg", 6900),
+					newOption(ProductEntity.builder().productId(11L).build(), "양념 깻잎 1kg", 8900),
+					newOption(ProductEntity.builder().productId(11L).build(), "된장 깻잎 1kg", 8900),
+					newOption(ProductEntity.builder().productId(11L).build(), "간장 깻잎 1kg", 7900),
+					newOption(ProductEntity.builder().productId(11L).build(), "고추 무침 1kg", 8900),
+					newOption(ProductEntity.builder().productId(11L).build(), "파래 무침 1kg", 9900),
+					newOption(ProductEntity.builder().productId(12L).build(), "01_순수소프트 27m 30롤 2팩", 28900),
+					newOption(ProductEntity.builder().productId(12L).build(), "02_벚꽃 프리미엄 27m 30롤 2팩", 32900),
+					newOption(ProductEntity.builder().productId(13L).build(), "(증정) 초미세모 칫솔 12개 x 2개", 11900),
+					newOption(ProductEntity.builder().productId(13L).build(), "(증정) 잇몸케어 치약 100G 3개 x 2개", 16900),
+					newOption(ProductEntity.builder().productId(13L).build(), "(증정) 구취케어 치약 100G 3개 x 2개", 16900),
+					newOption(ProductEntity.builder().productId(13L).build(), "(증정)화이트케어 치약 100G 3개 x 2개", 19900),
+					newOption(ProductEntity.builder().productId(13L).build(), "(증정) 어린이 칫솔 12EA", 9900),
+					newOption(ProductEntity.builder().productId(14L).build(), "[가정용] 샤인머스켓 1kg 2수내외", 9900),
+					newOption(ProductEntity.builder().productId(14L).build(), "[특품] 샤인머스켓 1kg 1-2수", 12900), //45
+					newOption(ProductEntity.builder().productId(14L).build(), "[특품] 샤인머스켓 2kg 2-3수", 23900),
+					newOption(ProductEntity.builder().productId(15L).build(), "화이트", 148000),
+					newOption(ProductEntity.builder().productId(15L).build(), "블랙", 148000)
 			));
 		};
 	}
@@ -107,8 +107,8 @@ public class KakaoApplication {
 				.build();
 	}
 
-	private Product newProduct(String productName, int imageNumber, int price){
-		return Product.builder()
+	private ProductEntity newProduct(String productName, int imageNumber, int price){
+		return ProductEntity.builder()
 				.productName(productName)
 				.description("")
 				.image("/images/"+imageNumber+".jpg")
@@ -116,9 +116,9 @@ public class KakaoApplication {
 				.build();
 	}
 
-	private Option newOption(Product product, String optionName, int price){
-		return Option.builder()
-				.product(product)
+	private ProductOptionEntity newOption(ProductEntity product, String optionName, int price){
+		return ProductOptionEntity.builder()
+				.productEntity(product)
 				.optionName(optionName)
 				.price(price)
 				.build();
