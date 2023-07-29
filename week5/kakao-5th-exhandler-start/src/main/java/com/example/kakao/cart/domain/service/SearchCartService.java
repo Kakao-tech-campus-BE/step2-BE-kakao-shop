e8ad5f978be0bbc905a5218d462365d041037e68
kssumin <201566@jnu.ac.kr>
2023-07-30 02:39:04 +0900

    feat : 책임에 따라 기존의 CartService를 SearchCartService로 분리

---
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/domain/service/SearchCartService.java
@@ -0,0 +1,46 @@
+package com.example.kakao.cart.domain.service;
+
+import com.example.kakao._core.errors.exception.Exception404;
+import com.example.kakao.cart.domain.converter.CartConverter;
+import com.example.kakao.cart.domain.converter.CashierConverter;
+import com.example.kakao.cart.domain.model.Cart;
+import com.example.kakao.cart.domain.model.Cashier;
+import com.example.kakao.cart.web.converter.CartFindAllResponseConverter;
+import com.example.kakao.cart.web.response.CartFindAllResponse;
+import com.example.kakao.user.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
+public class SearchCartService {
+    private final CartRepository cartRepository;
+
+    public CartFindAllResponse getCartsByUser(User user) {
+        List<Cart> carts = cartRepository.findByUser(user)
+                .stream()
+                .map(CartConverter::from)
+                .collect(Collectors.toList());
+
+        if (carts.isEmpty()) {
+            throw new Exception404("장바구니에 아무것도 없습니다.");
+        }
+
+        Cashier cashier = Objects.requireNonNull(CashierConverter.from(carts));
+        int totalPrice = calculateTotalPrice(cashier);
+
+        return CartFindAllResponseConverter.from(cashier, totalPrice);
+    }
+
+    private int calculateTotalPrice(Cashier cashier) {
+        return cashier.calculateTotalPrice();
+    }
+}
--- week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/web/CartRestController.java
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/web/CartRestController.java
@@ -2,71 +2,36 @@ package com.example.kakao.cart.web;
 
 import com.example.kakao._core.security.CustomUserDetails;
 import com.example.kakao._core.utils.ApiUtils;
-import com.example.kakao.cart.domain.service.CartService;
-import com.example.kakao.cart.web.request.CartReqeust;
-import com.example.kakao.cart.web.request.CartUpdateRequest;
-import com.example.kakao.cart.web.response.CartChangedOptionResponse;
+import com.example.kakao.cart.domain.service.SearchCartService;
 import com.example.kakao.cart.web.response.CartFindAllResponse;
-import com.example.kakao.cart.web.response.CartUpdateResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/carts")
 public class CartRestController {
-    private final CartService cartService;
+    private final SearchCartService searchCartService;
 
     @GetMapping
     public ResponseEntity<ApiUtils.ApiResult<CartFindAllResponse>> findAll(@AuthenticationPrincipal CustomUserDetails user) {
-        return ResponseEntity.ok(ApiUtils.success(cartService.getCartsByUser(user.getUser())));
+        return ResponseEntity.ok(ApiUtils.success(searchCartService.getCartsByUser(user.getUser())));
     }
 
-    @PostMapping
-    public ResponseEntity<ApiUtils.ApiResult> saveCarts(@AuthenticationPrincipal CustomUserDetails user, @Valid @RequestBody List<CartReqeust>cartSaveRequests) {
-        cartService.addCarts(user.getUser(), cartSaveRequests);
-        return ResponseEntity.ok(ApiUtils.successWithoutData());
-    }
+//    @PostMapping
+//    public ResponseEntity<ApiUtils.ApiResult> saveCarts(@AuthenticationPrincipal CustomUserDetails user, @Valid @RequestBody List<CartReqeust>cartSaveRequests) {
+//        cartService.addCarts(user.getUser(), cartSaveRequests);
+//        return ResponseEntity.ok(ApiUtils.successWithoutData());
+//    }
 
-    @PutMapping
-    public ResponseEntity<ApiUtils.ApiResult<CartUpdateResponse>> updateCarts(@AuthenticationPrincipal CustomUserDetails user, @Valid @RequestBody CartUpdateRequest cartUpdateRequest) {
-
-        List<CartChangedOptionResponse> changedOptions = new ArrayList<>();
-
-        CartChangedOptionResponse singleChangedOption = CartChangedOptionResponse.builder()
-                .cartId(4L)
-                .optionId(1L)
-                .optionName("01. 슬라이딩 지퍼백 크리스마스에디션 4종")
-                .quantity(10)
-                .price(100000)
-                .build();
-
-        CartChangedOptionResponse singleChangedOption2 = CartChangedOptionResponse.builder()
-                .cartId(5L)
-                .optionId(2L)
-                .optionName("02. 슬라이딩 지퍼백 플라워에디션 5종")
-                .quantity(10)
-                .price(109000)
-                .build();
-
-        changedOptions.add(singleChangedOption);
-        changedOptions.add(singleChangedOption2);
-
-        int totalPrice = singleChangedOption.getPrice() + singleChangedOption2.getPrice();
-
-
-        CartUpdateResponse cartUpdateResponse = CartUpdateResponse.builder()
-                .cartChangedOptionResponses(changedOptions)
-                .totalPrice(totalPrice)
-                .build();
-
-        return new ResponseEntity<>(ApiUtils.success(cartUpdateResponse), HttpStatus.OK);
-
-    }
+//    @PutMapping
+//    public ResponseEntity<ApiUtils.ApiResult<CartUpdateResponse>> updateCarts(@AuthenticationPrincipal CustomUserDetails user, @Valid @RequestBody List<CartReqeust> cartUpdateRequest) {
+//        cartService.updateCarts(user.getUser(), cartUpdateRequest);
+//
+//        return new ResponseEntity<>(ApiUtils.success(cartUpdateResponse), HttpStatus.OK);
+//
+//    }
 }
