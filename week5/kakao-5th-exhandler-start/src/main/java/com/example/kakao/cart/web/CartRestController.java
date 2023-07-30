82930c4ecb18e87a3fc5cc47c1522023654910cc
kssumin <201566@jnu.ac.kr>
2023-07-30 23:56:08 +0900

    fix : controller 반환 형식 변경

--- week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/web/CartRestController.java
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/web/CartRestController.java
@@ -2,7 +2,6 @@ package com.example.kakao.cart.web;
 
 import com.example.kakao._core.security.CustomUserDetails;
 import com.example.kakao._core.utils.ApiResult;
-import com.example.kakao._core.utils.ResponseBody;
 import com.example.kakao.cart.domain.service.SaveCartUsecase;
 import com.example.kakao.cart.domain.service.SearchCartUseCase;
 import com.example.kakao.cart.domain.service.UpdateCartUseCase;
@@ -11,7 +10,6 @@ import com.example.kakao.cart.web.request.CartUpdateRequest;
 import com.example.kakao.cart.web.response.CartFindAllResponse;
 import com.example.kakao.cart.web.response.CartUpdateResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,18 +25,18 @@ public class CartRestController {
     private final UpdateCartUseCase updateCartUseCase;
 
     @GetMapping
-    public ResponseEntity<ApiResult<CartFindAllResponse>> findAll(@AuthenticationPrincipal CustomUserDetails user) {
-        return ResponseEntity.ok(ResponseBody.success(searchCartUseCase.execute(user.getUser())));
+    public ApiResult<CartFindAllResponse> findAll(@AuthenticationPrincipal CustomUserDetails user) {
+        return ApiResult.success(searchCartUseCase.execute(user.getUser()));
     }
 
     @PostMapping
-    public ResponseEntity<ApiResult<CartFindAllResponse>> saveCarts(@AuthenticationPrincipal CustomUserDetails user, @Valid @RequestBody List<CartSaveReqeust> cartSaveRequests) {
+    public ApiResult<CartFindAllResponse> saveCarts(@AuthenticationPrincipal CustomUserDetails user, @Valid @RequestBody List<CartSaveReqeust> cartSaveRequests) {
         saveCartUsecase.execute(user.getUser(), cartSaveRequests);
-        return ResponseEntity.ok(ResponseBody.success());
+        return ApiResult.success();
     }
 
     @PutMapping
-    public ResponseEntity<ApiResult<CartUpdateResponse>> updateCarts(@AuthenticationPrincipal CustomUserDetails user, @Valid @RequestBody List<CartUpdateRequest> requests) {
-        return ResponseEntity.ok(ResponseBody.success(updateCartUseCase.execute(user.getUser(), requests)));
+    public ApiResult<CartUpdateResponse> updateCarts(@AuthenticationPrincipal CustomUserDetails user, @Valid @RequestBody List<CartUpdateRequest> requests) {
+        return ApiResult.success(updateCartUseCase.execute(user.getUser(), requests));
     }
 }
--- week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/order/web/OrderRestController.java
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/order/web/OrderRestController.java
@@ -2,11 +2,9 @@ package com.example.kakao.order.web;
 
 import com.example.kakao._core.security.CustomUserDetails;
 import com.example.kakao._core.utils.ApiResult;
-import com.example.kakao._core.utils.ResponseBody;
 import com.example.kakao.order.domain.service.SaveOrderUseCase;
 import com.example.kakao.order.web.response.OrderResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +16,7 @@ public class OrderRestController {
         private final SaveOrderUseCase saveOrderUseCase;
 
         @PostMapping
-        public ResponseEntity<ApiResult<OrderResponse>> createOrder(@AuthenticationPrincipal CustomUserDetails user) {
-            return ResponseEntity.ok(ResponseBody.success(saveOrderUseCase.execute(user.getUser())));
+        public ApiResult<OrderResponse> createOrder(@AuthenticationPrincipal CustomUserDetails user) {
+            return ApiResult.success(saveOrderUseCase.execute(user.getUser()));
         }
 }
