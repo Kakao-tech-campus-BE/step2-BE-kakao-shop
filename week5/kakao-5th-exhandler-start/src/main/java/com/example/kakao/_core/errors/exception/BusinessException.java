d1522cbeab86164a2dd5ea501dc5b7276410465c
kssumin <201566@jnu.ac.kr>
2023-07-30 17:21:21 +0900

    feat : ErrorCode를 파라미터로 받는 exception 구현

---
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/_core/errors/exception/BusinessException.java
@@ -0,0 +1,10 @@
+package com.example.kakao._core.errors.exception;
+
+public class BusinessException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
