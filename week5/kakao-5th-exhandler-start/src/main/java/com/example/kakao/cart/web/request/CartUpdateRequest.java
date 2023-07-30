13aa13b775bef1b66c2e1ac22dfb668fb63f0d08
kssumin <201566@jnu.ac.kr>
2023-07-30 11:35:52 +0900

    fix : api명세에 따라 cartId를 request로 받도록 변경

--- week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/web/request/CartUpdateRequest.java
+++ week5/kakao-5th-exhandler-start/src/main/java/com/example/kakao/cart/web/request/CartUpdateRequest.java
@@ -5,12 +5,11 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class CartUpdateRequest {
-    private List<CartReqeust> cartUpdateRequest;
+    private Long cartId;
+    private int quantity;
 }
