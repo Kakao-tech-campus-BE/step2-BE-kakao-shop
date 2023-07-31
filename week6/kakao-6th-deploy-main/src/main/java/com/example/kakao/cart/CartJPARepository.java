01a21293870a400a6a0bfd8546f46120effb71bc
INUK-ai <arheon2469@gmail.com>
2023-07-31 20:25:53 +0900

    feat: Cart, Order 기능 가져오기
    - 5주차 과제에서 기능 7, 8, 9, 10을 가져옴

--- week6/kakao-6th-deploy-main/src/main/java/com/example/kakao/cart/CartJPARepository.java
+++ week6/kakao-6th-deploy-main/src/main/java/com/example/kakao/cart/CartJPARepository.java
@@ -1,6 +1,7 @@
 package com.example.kakao.cart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -9,13 +10,19 @@ import java.util.Optional;
 
 
 public interface CartJPARepository extends JpaRepository<Cart, Integer> {
-    @Query("select c from Cart c join fetch c.option o join fetch o.product where c.user.id =:userId")
-    List<Cart> findAllByUserId(int userId);
 
     @Query("select c from Cart c join fetch c.user u join fetch c.option o join fetch o.product where c.user.id = :userId order by c.option.id asc")
     List<Cart> findByUserIdOrderByOptionIdAsc(int userId);
 
+    @Query("select c from Cart c join fetch c.option o where c.user.id = :userId")
+    List<Cart> mFindAllByUserId(int userId);
+
+    @Modifying(clearAutomatically = false)
+    @Query("delete from Cart c where c.user.id = :userId")
     void deleteByUserId(int userId);
+
+    @Query("select c from Cart c join fetch c.option where c.user.id = :userId")
+    List<Cart> findByUserId(@Param("userId") int userId);
 
     @Query("select c from Cart c where c.option.id = :optionId and c.user.id = :userId")
     Optional<Cart> findByOptionIdAndUserId(@Param("optionId") int optionId, @Param("userId") int userId);
--- week6/kakao-6th-deploy-main/src/main/java/com/example/kakao/cart/CartService.java
+++ week6/kakao-6th-deploy-main/src/main/java/com/example/kakao/cart/CartService.java
@@ -1,5 +1,6 @@
 package com.example.kakao.cart;
 
+import com.example.kakao._core.errors.exception.Exception400;
 import com.example.kakao._core.errors.exception.Exception404;
 import com.example.kakao.product.option.Option;
 import com.example.kakao.product.option.OptionJPARepository;
@@ -8,7 +9,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -20,53 +24,70 @@ public class CartService {
 
     @Transactional
     public void addCartList(List<CartRequest.SaveDTO> requestDTOs, User sessionUser) {
-        // 1. 동일한 옵션이 들어오면 예외처리
-        // [ { optionId:1, quantity:5 }, { optionId:1, quantity:10 } ]
 
-        // 2. cartJPARepository.findByOptionIdAndUserId() 조회 -> 존재하면 장바구니에 수량을 추가하는 업데이트를 해야함. (더티체킹하기)
+        Set<Integer> checkOptionId = new HashSet<>();
 
-        // 3. [2번이 아니라면] 유저의 장바구니에 담기
         for (CartRequest.SaveDTO requestDTO : requestDTOs) {
             int optionId = requestDTO.getOptionId();
             int quantity = requestDTO.getQuantity();
+
+            if(!checkOptionId.add(optionId)) {
+                throw new Exception400("동일한 상품이 중복으로 담겨있습니다. : " + optionId);
+            }
+
             Option optionPS = optionJPARepository.findById(optionId)
-                    .orElseThrow(() -> new Exception404("해당 옵션을 찾을 수 없습니다 : " + optionId));
+                    .orElseThrow(() -> new Exception404("해당 상품을 찾을 수 없습니다 : " + optionId));
             int price = optionPS.getPrice() * quantity;
-            Cart cart = Cart.builder().user(sessionUser).option(optionPS).quantity(quantity).price(price).build();
-            cartJPARepository.save(cart);
+
+            Optional<Cart> findExistingCart = cartJPARepository.findByOptionIdAndUserId(optionId, sessionUser.getId());
+            if(findExistingCart.isPresent()) {
+                Cart existingCart = findExistingCart.get();
+                existingCart.update(existingCart.getQuantity() + quantity, existingCart.getPrice() + price);
+            } else {
+                Cart cart = Cart.builder().user(sessionUser).option(optionPS).quantity(quantity).price(price).build();
+                cartJPARepository.save(cart);
+            }
         }
     }
 
     public CartResponse.FindAllDTO findAll(User user) {
         List<Cart> cartList = cartJPARepository.findByUserIdOrderByOptionIdAsc(user.getId());
-        // Cart에 담긴 옵션이 3개이면, 2개는 바나나 상품, 1개는 딸기 상품이면 Product는 2개인 것이다.
-        return new CartResponse.FindAllDTO(cartList);
-    }
 
-    public CartResponse.FindAllDTOv2 findAllv2(User user) {
-        List<Cart> cartList = cartJPARepository.findByUserIdOrderByOptionIdAsc(user.getId());
-        return new CartResponse.FindAllDTOv2(cartList);
+        return new CartResponse.FindAllDTO(cartList);
     }
 
     @Transactional
     public CartResponse.UpdateDTO update(List<CartRequest.UpdateDTO> requestDTOs, User user) {
-        List<Cart> cartList = cartJPARepository.findAllByUserId(user.getId());
+        List<Cart> cartList = cartJPARepository.mFindAllByUserId(user.getId());
 
-        // 1. 유저 장바구니에 아무것도 없으면 예외처리
+        if(cartList.isEmpty()) {
+            throw new Exception400("장바구니가 비어있습니다.");
+        }
 
-        // 2. cartId:1, cartId:1 이렇게 requestDTOs에 동일한 장바구니 아이디가 두번 들어오면 예외처리
+        Set<Integer> checkCartId = new HashSet<>();
 
-        // 3. 유저 장바구니에 없는 cartId가 들어오면 예외처리
+        for(CartRequest.UpdateDTO updateDTO : requestDTOs) {
+            int cartId = updateDTO.getCartId();
 
-        // 위에 3개를 처리하지 않아도 프로그램은 잘돌아간다. 예를 들어 1번을 처리하지 않으면 for문을 돌지 않고, cartList가 빈배열 []로 정상응답이 나감.
-        for (Cart cart : cartList) {
-            for (CartRequest.UpdateDTO updateDTO : requestDTOs) {
-                if (cart.getId() == updateDTO.getCartId()) {
+            if (!checkCartId.add(cartId)) {
+                throw new Exception400("동일한 상품이 중복으로 담겨있습니다 : " + cartId);
+            }
+
+            boolean cartExist = false;
+
+            for(Cart cart : cartList) {
+                if(cart.getId() == cartId) {
                     cart.update(updateDTO.getQuantity(), cart.getOption().getPrice() * updateDTO.getQuantity());
+                    cartExist = true;
+                    break;
                 }
+            }
+
+            if(!cartExist) {
+                throw new Exception400("해당 상품은 장바구니에 존재하지 않습니다 : " + cartId);
             }
         }
 
         return new CartResponse.UpdateDTO(cartList);
-    } // 더티체킹
+    }
 }
