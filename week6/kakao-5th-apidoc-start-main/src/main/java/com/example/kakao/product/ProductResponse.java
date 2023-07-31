a0db9703cd573847ce23d1f8953b63b9ee168e2b
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-07-31 22:25:25 +0900

    feat : apply week5 code

--- week6/kakao-5th-apidoc-start-main/src/main/java/com/example/kakao/product/ProductResponse.java
+++ week6/kakao-5th-apidoc-start-main/src/main/java/com/example/kakao/product/ProductResponse.java
@@ -1,23 +1,22 @@
 package com.example.kakao.product;
 
 import com.example.kakao.product.option.Option;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ProductResponse {
     @Getter
-    @Setter
     public static class FindAllDTO {
-
         private int id;
         private String productName;
         private String description;
         private String image;
         private int price;
 
+        @Builder
         public FindAllDTO(Product product) {
             this.id = product.getId();
             this.productName = product.getProductName();
@@ -27,72 +26,38 @@ public class ProductResponse {
         }
     }
 
-@Getter @Setter
-public static class FindByIdDTO {
-
-    private int id;
-    private String productName;
-    private String description;
-    private String image;
-    private int price;
-    private int starCount; // 0~5
-    private List<OptionDTO> options;
-
-    public FindByIdDTO(Product product, List<Option> optionList) {
-        this.id = product.getId();
-        this.productName = product.getProductName();
-        this.description = product.getDescription();
-        this.image = product.getImage();
-        this.price = product.getPrice();
-        this.starCount = 5; // 임시로 추가해둠 (요구사항에는 없음)
-        this.options = optionList.stream().map(OptionDTO::new).collect(Collectors.toList());
-    }
-
-    @Getter @Setter
-    public class OptionDTO {
+    @Getter
+    public static class FindByIdDTO {
         private int id;
-        private String optionName;
+        private String productName;
+        private String description;
+        private String image;
         private int price;
+        private int starCount; // 0~5
+        private List<OptionDTO> options;
 
-        public OptionDTO(Option option) {
-            this.id = option.getId();
-            this.optionName = option.getOptionName();
-            this.price = option.getPrice();
+        @Builder
+        public FindByIdDTO(List<Option> optionsList) {
+            this.id = optionsList.get(0).getProduct().getId();
+            this.productName = optionsList.get(0).getProduct().getProductName();
+            this.description = optionsList.get(0).getProduct().getDescription();
+            this.image = optionsList.get(0).getProduct().getImage();
+            this.price = optionsList.get(0).getProduct().getPrice();
+            this.starCount = 5;
+            this.options = optionsList.stream().map(OptionDTO::new).collect(Collectors.toList());
+        }
+
+        @Getter
+        public class OptionDTO {
+            int id;
+            String optionName;
+            int price;
+
+            public OptionDTO(Option option) {
+                this.id = option.getId();
+                this.optionName = option.getOptionName();
+                this.price = option.getPrice();
+            }
         }
     }
-}
-
-@Getter @Setter
-public static class FindByIdDTOv2 {
-    private int id;
-    private String productName;
-    private String description;
-    private String image;
-    private int price;
-    private int starCount; // 0~5
-    private List<OptionDTO> options;
-
-    public FindByIdDTOv2(List<Option> optionList) {
-        this.id = optionList.get(0).getProduct().getId();
-        this.productName = optionList.get(0).getProduct().getProductName();
-        this.description = optionList.get(0).getProduct().getDescription();
-        this.image = optionList.get(0).getProduct().getImage();
-        this.price = optionList.get(0).getProduct().getPrice();
-        this.starCount = 5;
-        this.options = optionList.stream().map(OptionDTO::new).collect(Collectors.toList());
-    }
-
-    @Getter @Setter
-    public class OptionDTO {
-        private int id;
-        private String optionName;
-        private int price;
-
-        public OptionDTO(Option option) {
-            this.id = option.getId();
-            this.optionName = option.getOptionName();
-            this.price = option.getPrice();
-        }
-    }
-}
 }
--- week6/kakao-5th-apidoc-start-main/src/main/java/com/example/kakao/product/ProductService.java
+++ week6/kakao-5th-apidoc-start-main/src/main/java/com/example/kakao/product/ProductService.java
@@ -17,42 +17,33 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Service
 public class ProductService {
-    private final ProductJPARepository productRepository;
-    private final OptionJPARepository optionRepository;
-
-    public ProductResponse.FindByIdDTOv2 findByIdv2(int id) {
-        List<Option> optionListPS = optionRepository.findByProductIdJoinProduct(id);
-        if(optionListPS.size() == 0){
-            throw new Exception404("해당 상품을 찾을 수 없습니다 : "+id);
-        }
-        return new ProductResponse.FindByIdDTOv2(optionListPS);
-    }
-
-    public ProductResponse.FindByIdDTO findById(int id) {
-        Product productPS = productRepository.findById(id).orElseThrow(
-                () -> new Exception404("해당 상품을 찾을 수 없습니다 : "+id)
-        );
-        List<Option> optionListPS = optionRepository.findByProductId(productPS.getId());
-        return new ProductResponse.FindByIdDTO(productPS, optionListPS);
-    }
-
-
+    private final ProductJPARepository productJPARepository;
+    private final OptionJPARepository optionJPARepository;
 
     public List<ProductResponse.FindAllDTO> findAll(int page) {
         // 1. 페이지 객체 만들기
-        Pageable pageable = PageRequest.of(page,9);
+        Pageable pageable = PageRequest.of(page, 9);
 
         // 2. DB 조회하기
-        Page<Product> pageContent = productRepository.findAll(pageable);
+        Page<Product> pageContent = productJPARepository.findAll(pageable);
 
         // 3. DTO 만들기
-        // 페이지 객체의 content는 List이다.
-        // List를 stream()으로 변환 -> 자바 오브젝트의 타입이 없어진다. (강물에 흩뿌린다)
-        // map으로 순회하면서 값을 변한한다. (가공)
-        // 가공된 데이터를 다시 자바 오브젝트로 변환한다.
+        // 페이지 객체의 content는 List이다
+        // List를 stream()으로 변환 -> 자바 오브젝트의 타입이 없어진다.
+        // map으로 순회하면서 값을 변환
+        // 가공된 데이터를 다시 자바 오브젝트로 변환한다
         List<ProductResponse.FindAllDTO> responseDTOs = pageContent.getContent().stream()
                 .map(product -> new ProductResponse.FindAllDTO(product))
                 .collect(Collectors.toList());
+
         return responseDTOs;
+    }
+
+    public ProductResponse.FindByIdDTO findById(int id) {
+        List<Option> optionList = optionJPARepository.findByProductIdJoinProduct(id);
+        if (optionList.isEmpty()) {
+            throw new Exception404("해당 상품을 찾을 수 없습니다. : " + id);
+        }
+        return new ProductResponse.FindByIdDTO(optionList);
     }
 }
