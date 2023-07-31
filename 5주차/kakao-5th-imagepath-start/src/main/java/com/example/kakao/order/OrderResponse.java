11f2b6bcf8d1263016e20f07d2df6280aaffa933
yuseonkim <yusun715@icloud.com>
2023-08-01 02:06:03 +0900

    orderResponseDTO 수정

--- 5주차/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/OrderResponse.java
+++ 5주차/kakao-5th-imagepath-start/src/main/java/com/example/kakao/order/OrderResponse.java
@@ -1,15 +1,9 @@
 package com.example.kakao.order;
 
-import com.example.kakao.cart.Cart;
-import com.example.kakao.cart.CartResponse;
 import com.example.kakao.order.item.Item;
 import com.example.kakao.product.Product;
-import com.example.kakao.product.option.Option;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
-import org.aspectj.weaver.ast.Or;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -27,21 +21,21 @@ public class OrderResponse {
             this.id = items.get(0).getOrder().getId();
             this.products = items.stream().map(item -> item.getOption().getProduct()).distinct()
                     .map(product -> new ProductDTO(product,items)).collect(Collectors.toList());
-            this.totalPrice = items.stream().mapToInt(item -> item.getPrice() * item.getQuantity()).sum();
+            this.totalPrice = items.stream().mapToInt(item -> item.getPrice()).sum();
         }
 
         @Getter
         @Setter
         public class ProductDTO {
-            private int id;
             private String productName;
             private List<ItemDTO> items;
 
             public ProductDTO(Product product,List<Item> items){
-                this.id = product.getId();
                 this.productName = product.getProductName();
-                this.items = items.stream().map(item -> new ItemDTO(item.getId(),item.getOption().getOptionName(),
-                        item.getQuantity(),item.getPrice())).collect(Collectors.toList());
+                this.items = items.stream()
+                        .filter(item -> item.getOption().getProduct().getId() == product.getId())
+                        .map(item -> new ItemDTO(item.getId(),item.getOption().getOptionName(),item.getQuantity(),item.getPrice()))
+                        .collect(Collectors.toList());
             }
 
             @Getter
@@ -74,21 +68,21 @@ public class OrderResponse {
             this.id = items.get(0).getOrder().getId();
             this.products = items.stream().map(item -> item.getOption().getProduct()).distinct()
                     .map(product -> new ProductDTO(product,items)).collect(Collectors.toList());
-            this.totalPrice = items.stream().mapToInt(item -> item.getPrice() * item.getQuantity()).sum();
+            this.totalPrice = items.stream().mapToInt(item -> item.getPrice()).sum();
         }
 
         @Getter
         @Setter
         public class ProductDTO {
-            private int id;
             private String productName;
             private List<ItemDTO> items;
 
             public ProductDTO(Product product,List<Item> items){
-                this.id = product.getId();
                 this.productName = product.getProductName();
-                this.items = items.stream().map(item -> new ItemDTO(item.getId(),item.getOption().getOptionName(),
-                        item.getQuantity(),item.getPrice())).collect(Collectors.toList());
+                this.items = items.stream()
+                        .filter(item -> item.getOption().getProduct().getId() == product.getId())
+                        .map(item -> new FindByIdDTO.ProductDTO.ItemDTO(item.getId(),item.getOption().getOptionName(),item.getQuantity(),item.getPrice()))
+                        .collect(Collectors.toList());
             }
 
             @Getter
