d134be0e4f3282be3c0e251df2fdfd36ea1e607b
Cookie <pon05114@naver.com>
2023-08-01 23:16:27 +0900

    feat: Product Integration Test
    - findById_error_test() 추가

--- 6주차과제/kakao-6th-deploy/src/test/java/com/example/kakao/product/ProductRestControllerTest.java
+++ 6주차과제/kakao-6th-deploy/src/test/java/com/example/kakao/product/ProductRestControllerTest.java
@@ -66,4 +66,27 @@ public class ProductRestControllerTest extends MyRestDoc {
         resultActions.andExpect(jsonPath("$.response.price").value(1000));
         resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
     }
+
+    @Test
+    public void findById_error_test() throws Exception {
+        // given teardown.sql
+        int id = 0;
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                get("/products/" + id)
+        );
+
+        // console
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : "+responseBody);
+
+        // verify
+        resultActions.andExpect(jsonPath("$.success").value("false"));
+        resultActions.andExpect(jsonPath("$.error.message").value("해당 상품을 찾을 수 없습니다 : " + id));
+        resultActions.andExpect(jsonPath("$.error.status").value("404"));
+
+
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);// API DOCS부분
+    }
 }
