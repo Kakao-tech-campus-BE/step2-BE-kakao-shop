f5f0ad664857e1b448a73d9c9a3028fe8110d96a
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-08-01 20:03:22 +0900

    Update GlobalExceptionHandler.java
    Error entity가 구현되어 있기에 기능을 완성하였습니다.
    
    회원의 ip, 접속정보등을 getArr로 가져왔습니다.

--- 6week/kakao-6th-deploy/src/main/java/com/example/kakao/_core/errors/GlobalExceptionHandler.java
+++ 6week/kakao-6th-deploy/src/main/java/com/example/kakao/_core/errors/GlobalExceptionHandler.java
@@ -2,41 +2,66 @@ package com.example.kakao._core.errors;
 
 import com.example.kakao._core.errors.exception.*;
 import com.example.kakao._core.utils.ApiUtils;
+import com.example.kakao.log.ErrorLog;
+import com.example.kakao.log.ErrorLogJPARepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @Autowired
+    private ErrorLogJPARepository errorLogJPARepository;
+
+    private void logError(HttpServletRequest request, Exception e) {
+        ErrorLog errorLog = ErrorLog.builder()
+                .userIp(request.getRemoteAddr())
+                .userAgent(request.getHeader("User-Agent"))
+                .message(e.getMessage())
+                .build();
+        errorLogJPARepository.save(errorLog);
+    }
+
+
     @ExceptionHandler(Exception400.class)
-    public ResponseEntity<?> badRequest(Exception400 e){
+    public ResponseEntity<?> badRequest(Exception400 e, HttpServletRequest request){
+        logError(request, e);
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception401.class)
-    public ResponseEntity<?> unAuthorized(Exception401 e){
+    public ResponseEntity<?> unAuthorized(Exception401 e, HttpServletRequest request){
+        logError(request, e);
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception403.class)
-    public ResponseEntity<?> forbidden(Exception403 e){
+    public ResponseEntity<?> forbidden(Exception403 e, HttpServletRequest request){
+        logError(request, e);
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception404.class)
-    public ResponseEntity<?> notFound(Exception404 e){
+    public ResponseEntity<?> notFound(Exception404 e, HttpServletRequest request){
+        logError(request, e);
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception500.class)
-    public ResponseEntity<?> serverError(Exception500 e){
+    public ResponseEntity<?> serverError(Exception500 e, HttpServletRequest request){
+        logError(request, e);
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> unknownServerError(Exception e){
+    public ResponseEntity<?> unknownServerError(Exception e, HttpServletRequest request){
+        logError(request, e);
         ApiUtils.ApiResult<?> apiResult = ApiUtils.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(apiResult, HttpStatus.INTERNAL_SERVER_ERROR);
     }
