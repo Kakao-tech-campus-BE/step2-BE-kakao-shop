f648256883f043e118c0d4a6df2f482be9547dcd
sososo0 <sh0000@pusan.ac.kr>
2023-08-02 00:00:07 +0900

    feat: Add findAll_test() in ProductRestControllerTest

--- 5week/kakao-5th-assignment/src/test/java/com/example/kakao/_core/product/ProductRestControllerTest.java
+++ 5week/kakao-5th-assignment/src/test/java/com/example/kakao/_core/product/ProductRestControllerTest.java
@@ -4,26 +4,40 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+@Sql(value = "classpath:db/teardown.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class ProductRestControllerTest {
 
     @Autowired
     private MockMvc mvc;
-
     @Test
     public void findAll_test() throws Exception {
         // given
         Integer page = 0;
         // when
-        mvc.perform(get("/products"));
+        ResultActions resultActions = mvc.perform(get("/products").param("page", page.toString()));
+
+        //eye 응답되는 json data를 직접 확인해볼 수 있음
+        //String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        //System.out.println("테스트 : "+responseBody);
 
         // then
-
+        resultActions.andExpect(jsonPath("$.success").value("true"));
+        resultActions.andExpect(jsonPath("$.response[0].id").value(1));
+        resultActions.andExpect(jsonPath("$.response[0].productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"));
+        resultActions.andExpect(jsonPath("$.response[0].description").value(""));
+        resultActions.andExpect(jsonPath("$.response[0].image").value("/images/1.jpg"));
+        resultActions.andExpect(jsonPath("$.response[0].price").value(1000));
     }
 
 }
