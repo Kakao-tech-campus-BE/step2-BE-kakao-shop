7500f92fffb5f89e8b8e6e66d0712f28f27dc647
sososo0 <sh0000@pusan.ac.kr>
2023-08-02 02:30:02 +0900

    feat: Add findAll_test() in CartRestController

--- 5week/kakao-5th-assignment/src/test/java/com/example/kakao/_core/cart/CartRestControllerTest.java
+++ 5week/kakao-5th-assignment/src/test/java/com/example/kakao/_core/cart/CartRestControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
@@ -53,14 +54,40 @@ public class CartRestControllerTest {
         ResultActions resultActions = mvc.perform(post("/carts/add").content(requestBody).contentType(MediaType.APPLICATION_JSON));
 
         // eye
-        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
-        System.out.println("테스트 : "+responseBody);
+        //String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        //System.out.println("테스트 : "+responseBody);
 
         //then
         resultActions.andExpect(jsonPath("$.success").value("true"));
 
     }
 
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
+    public void findAll_test() throws Exception {
+        // given teardown
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                get("/carts")
+        );
+
+        // eye
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // verify
+        resultActions.andExpect(jsonPath("$.success").value("true"));
+        resultActions.andExpect(jsonPath("$.response.products[0].id").value(1));
+        resultActions.andExpect(jsonPath("$.response.products[0].productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"));
+        resultActions.andExpect(jsonPath("$.response.products[0].carts[0].id").value(1));
+        resultActions.andExpect(jsonPath("$.response.products[0].carts[0].option.id").value(1));
+        resultActions.andExpect(jsonPath("$.response.products[0].carts[0].option.optionName").value("01. 슬라이딩 지퍼백 크리스마스에디션 4종"));
+        resultActions.andExpect(jsonPath("$.response.products[0].carts[0].option.price").value(10000));
+        resultActions.andExpect(jsonPath("$.response.products[0].carts[0].quantity").value(5));
+        resultActions.andExpect(jsonPath("$.response.products[0].carts[0].price").value(50000));
+        resultActions.andExpect(jsonPath("$.response.totalPrice").value(310900));
+    }
 
 
 }
