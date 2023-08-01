33d2f59568912f59e691c9c2f5fff775f45bee96
sososo0 <sh0000@pusan.ac.kr>
2023-08-02 00:21:07 +0900

    feat: Add findById_test() in ProductControllerTest

--- 5week/kakao-5th-assignment/src/test/java/com/example/kakao/_core/product/ProductRestControllerTest.java
+++ 5week/kakao-5th-assignment/src/test/java/com/example/kakao/_core/product/ProductRestControllerTest.java
@@ -40,4 +40,28 @@ public class ProductRestControllerTest {
         resultActions.andExpect(jsonPath("$.response[0].price").value(1000));
     }
 
+    @Test
+    public void findById_test() throws Exception {
+        // given teardown.sql
+        int id = 1;
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                get("/products/" + id)
+        );
+
+        // console
+        //String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        //System.out.println("테스트 : "+responseBody);
+
+        // verify
+        resultActions.andExpect(jsonPath("$.success").value("true"));
+        resultActions.andExpect(jsonPath("$.response.id").value(1));
+        resultActions.andExpect(jsonPath("$.response.productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"));
+        resultActions.andExpect(jsonPath("$.response.description").value(""));
+        resultActions.andExpect(jsonPath("$.response.image").value("/images/1.jpg"));
+        resultActions.andExpect(jsonPath("$.response.price").value(1000));
+    }
+
+
 }
