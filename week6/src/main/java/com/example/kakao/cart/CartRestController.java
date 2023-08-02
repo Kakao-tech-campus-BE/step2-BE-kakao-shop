3f791a19bf6b1ec3d631d2fec273c57f45365e47
Starlight258 <audwl03071@gmail.com>
2023-08-02 13:52:08 +0900

    feat: API Docs 예외처리 케이스 추가

--- week6/src/main/java/com/example/kakao/cart/CartRestController.java
+++ week6/src/main/java/com/example/kakao/cart/CartRestController.java
@@ -68,8 +68,6 @@ public class CartRestController {
      */
     // (기능8) 주문하기 - (주문화면에서 장바구니 수정하기)
     // /carts/update
-    // (기능8) 주문하기 - (주문화면에서 장바구니 수정하기)
-    // /carts/update
     @PostMapping("/carts/update")
     public ResponseEntity<?> update(@RequestBody @Valid List<CartRequest.UpdateDTO> requestDTOs, Errors errors, @AuthenticationPrincipal CustomUserDetails userDetails) {
         CartResponse.UpdateDTO responseDTO = cartService.update(requestDTOs,userDetails.getUser());
--- week6/src/test/java/com/example/kakao/cart/CartRestControllerTest.java
+++ week6/src/test/java/com/example/kakao/cart/CartRestControllerTest.java
@@ -21,6 +21,7 @@ import java.util.List;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureRestDocs(uriScheme = "http", uriHost = "localhost", uriPort = 8080)
 @ActiveProfiles("test")
@@ -30,6 +31,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class CartRestControllerTest extends MyRestDoc {
     @Autowired
     private ObjectMapper om;
+
+    @Autowired
+    private CartJPARepository cartJPARepository;
 
     @WithUserDetails(value = "ssarmango@nate.com") //시큐리티 인증
     @DisplayName("장바구니 담기(저장)")
@@ -58,6 +62,84 @@ public class CartRestControllerTest extends MyRestDoc {
         // verify
         resultActions.andExpect(jsonPath("$.success").value("true"));
         resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+    @WithUserDetails(value = "ssarmango@nate.com") //시큐리티 인증
+    @DisplayName("장바구니 담기(저장) 실패 - 중복된 옵션 입력")
+    @Test
+    public void addCartList_test_if_duplicated_option() throws Exception {
+        // given -> optionId [1,2,16]이 teardown.sql을 통해 들어가 있음
+        // [ { optionId:3, quantity:5 }, { optionId:3, quantity:5 } ]
+        List<CartRequest.SaveDTO> requestDTOs = new ArrayList<>();
+        CartRequest.SaveDTO item = new CartRequest.SaveDTO();
+        item.setOptionId(3);
+        item.setQuantity(5);
+        requestDTOs.add(item);
+        //동일한 옵션
+        CartRequest.SaveDTO item2 = new CartRequest.SaveDTO();
+        item2.setOptionId(3);
+        item2.setQuantity(5);
+        requestDTOs.add(item2);
+
+        String requestBody = om.writeValueAsString(requestDTOs);
+        System.out.println("요청 데이터 : " + requestBody);
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                post("/carts/add")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        );
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // verify
+        resultActions.andExpect(jsonPath("$.success").value("false"));
+        resultActions.andExpect(status().isBadRequest()); //400번 에러
+
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+    @WithUserDetails(value = "ssarmango@nate.com") //시큐리티 인증
+    @DisplayName("장바구니 담기(저장) - 이미 존재하는 옵션 입력(업데이트)")
+    @Test
+    public void addCartList_test_if_existed_option() throws Exception {
+        // given -> optionId [1,2,16]이 teardown.sql을 통해 들어가 있음
+        List<CartRequest.SaveDTO> requestDTOs = new ArrayList<>();
+        CartRequest.SaveDTO item = new CartRequest.SaveDTO();
+        item.setOptionId(1);
+        item.setQuantity(5);
+        requestDTOs.add(item);
+
+        String requestBody = om.writeValueAsString(requestDTOs);
+        System.out.println("요청 데이터 : " + requestBody);
+
+        // when
+        //장바구니 담기
+        ResultActions resultActions = mvc.perform(
+                post("/carts/add")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        );
+        //장바구니 조회
+        ResultActions resultActions2 = mvc.perform(
+                get("/carts")
+        );
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("장바구니 담기 : " + responseBody);
+        String responseBody2 = resultActions2.andReturn().getResponse().getContentAsString();
+        System.out.println("장바구니 조회 결과 : " + responseBody2);
+
+        // verify
+        //장바구니 담기
+        resultActions.andExpect(jsonPath("$.success").value("true"));
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+
+        //장바구니 조회(장바구니 업데이트 결과)
+        resultActions2.andExpect(jsonPath("$.response.products[0].productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"));
+        resultActions2.andExpect(jsonPath("$.response.products[0].carts[0].id").value(1));
+        resultActions2.andExpect(jsonPath("$.response.products[0].carts[0].quantity").value(10)); //5개 더 추가되어 10개
     }
 
     @WithUserDetails(value = "ssarmango@nate.com") //UserDetailService의 loadByUsername 실행, email으로 user를 DB 조회
@@ -120,6 +202,105 @@ public class CartRestControllerTest extends MyRestDoc {
         resultActions.andExpect(jsonPath("$.response.carts[0].optionName").value("01. 슬라이딩 지퍼백 크리스마스에디션 4종"));
         resultActions.andExpect(jsonPath("$.response.carts[0].quantity").value(10));
         resultActions.andExpect(jsonPath("$.response.carts[0].price").value(100000));
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @DisplayName("주문하기(장바구니 수정) 실패 - 장바구니가 비었을때")
+    @Test
+    public void update_test_if_cart_empty() throws Exception {
+        // given -> cartId [1번 5개,2번 1개,3번 5개]가 teardown.sql을 통해 들어가 있음
+        //장바구니 비우기
+        cartJPARepository.deleteAll();
+        //dto 생성
+        List<CartRequest.UpdateDTO> requestDTOs = new ArrayList<>();
+        CartRequest.UpdateDTO item = new CartRequest.UpdateDTO();
+        item.setCartId(1);
+        item.setQuantity(10);
+        requestDTOs.add(item);
+
+        String requestBody = om.writeValueAsString(requestDTOs);
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                post("/carts/update")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        );
+
+        // eye
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // verify
+        resultActions.andExpect(jsonPath("$.success").value("false"));
+        resultActions.andExpect(status().isNotFound()); //404번 에러
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @DisplayName("주문하기(장바구니 수정) 실패 - 중복된 장바구니 입력")
+    @Test
+    public void update_test_if_duplicated_cart() throws Exception {
+        // given -> cartId [1번 5개,2번 1개,3번 5개]가 teardown.sql을 통해 들어가 있음
+        //dto 생성
+        List<CartRequest.UpdateDTO> requestDTOs = new ArrayList<>();
+        CartRequest.UpdateDTO item = new CartRequest.UpdateDTO();
+        item.setCartId(1);
+        item.setQuantity(10);
+        requestDTOs.add(item);
+        //중복된 장바구니
+        CartRequest.UpdateDTO item2 = new CartRequest.UpdateDTO();
+        item2.setCartId(1);
+        item2.setQuantity(10);
+        requestDTOs.add(item);
+
+        String requestBody = om.writeValueAsString(requestDTOs);
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                post("/carts/update")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        );
+
+        // eye
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // verify
+        resultActions.andExpect(jsonPath("$.success").value("false"));
+        resultActions.andExpect(status().isBadRequest()); //400번 에러
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @DisplayName("주문하기(장바구니 수정) 실패 - 존재하지 않은 장바구니 업데이트 시도")
+    @Test
+    public void update_test_if_notexisted_cart() throws Exception {
+        // given -> cartId [1번 5개,2번 1개,3번 5개]가 teardown.sql을 통해 들어가 있음
+        List<CartRequest.UpdateDTO> requestDTOs = new ArrayList<>();
+        CartRequest.UpdateDTO item = new CartRequest.UpdateDTO();
+        item.setCartId(4); //존재하지 않은 cartId
+        item.setQuantity(5);
+        requestDTOs.add(item);
+
+        String requestBody = om.writeValueAsString(requestDTOs);
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                post("/carts/update")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        );
+
+        // eye
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // verify
+        resultActions.andExpect(jsonPath("$.success").value("false"));
+        resultActions.andExpect(status().isNotFound()); //404번 에러
         resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
     }
 
