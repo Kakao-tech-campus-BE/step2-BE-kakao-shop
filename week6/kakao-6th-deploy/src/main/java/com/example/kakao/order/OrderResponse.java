6abf4e118046d8a6fd67125cf6306c13ab67c707
chaee813 <chaee813@gmail.com>
2023-08-02 23:25:54 +0900

    inital commit

---
+++ week6/kakao-6th-deploy/src/main/java/com/example/kakao/order/OrderResponse.java
@@ -0,0 +1,134 @@
+package com.example.kakao.order;
+
+import com.example.kakao.order.item.Item;
+import com.example.kakao.product.Product;
+import com.example.kakao.product.option.Option;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OrderResponse {
+    @Getter
+    @Setter
+    @ToString
+    public static class SaveDTO {
+        private int id;
+        private List<ProductDTO> products;
+        private int totalPrice;
+
+        public SaveDTO(Order order, List<Item> itemList) {
+            this.id = order.getId();
+            this.products = itemList.stream()
+                    // 중복되는 상품 걸러내기
+                    .map(item -> item.getOption().getProduct()).distinct()
+                    .map(product -> new OrderResponse.SaveDTO.ProductDTO(product, itemList)).collect(Collectors.toList());
+            this.totalPrice = itemList.stream().mapToInt(item -> item.getPrice()).sum();
+        }
+
+        @Getter
+        @Setter
+        public class ProductDTO {
+            private String productName;
+            private List<ItemDTO> items;
+            public ProductDTO(Product product, List<Item> itemList) {
+                this.productName = product.getProductName();
+                // 현재 상품과 동일한 장바구니 내역만 담기
+                this.items = itemList.stream()
+                        .filter(item -> item.getOption().getProduct().getId() == product.getId())
+                        .map(ItemDTO::new)
+                        .collect(Collectors.toList());
+            }
+
+            @Getter @Setter
+            public class ItemDTO {
+                private int id;
+                private String optionName;
+                private int quantity;
+                private int price;
+
+                public ItemDTO(Item item) {
+                    this.id = item.getId();
+                    this.optionName = item.getOption().getOptionName();
+                    this.quantity = item.getQuantity();
+                    this.price = item.getOption().getPrice() * this.quantity;
+                }
+                @Getter
+                @Setter
+                public class OptionDTO {
+                    private int id;
+                    private String optionName;
+                    private int price;
+
+                    public OptionDTO(Option option) {
+                        this.id = option.getId();
+                        this.optionName = option.getOptionName();
+                        this.price = option.getPrice();
+                    }
+                }
+            }
+        }
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    public static class findByIdDTO {
+        private int id;
+        private List<ProductDTO> products;
+        private int totalPrice;
+
+        public findByIdDTO(int orderId, List<Item> itemList) {
+            this.id = orderId; //order.getId();
+            this.products = itemList.stream()
+                    // 중복되는 상품 걸러내기
+                    .map(item -> item.getOption().getProduct()).distinct()
+                    .map(product -> new OrderResponse.findByIdDTO.ProductDTO(product, itemList)).collect(Collectors.toList());
+            this.totalPrice = itemList.stream().mapToInt(item -> item.getPrice()).sum();
+        }
+        @Getter
+        @Setter
+        public class ProductDTO {
+            private String productName;
+            private List<findByIdDTO.ProductDTO.ItemDTO> items;
+            public ProductDTO(Product product, List<Item> itemList) {
+                this.productName = product.getProductName();
+                // 현재 상품과 동일한 장바구니 내역만 담기
+                this.items = itemList.stream()
+                        .filter(item -> item.getOption().getProduct().getId() == product.getId())
+                        .map(findByIdDTO.ProductDTO.ItemDTO::new)
+                        .collect(Collectors.toList());
+            }
+
+            @Getter @Setter
+            public class ItemDTO {
+                private int id;
+                private String optionName;
+                private int quantity;
+                private int price;
+
+                public ItemDTO(Item item) {
+                    this.id = item.getId();
+                    this.optionName = item.getOption().getOptionName();
+                    this.quantity = item.getQuantity();
+                    this.price = item.getOption().getPrice() * this.quantity;
+                }
+                @Getter
+                @Setter
+                public class OptionDTO {
+                    private int id;
+                    private String optionName;
+                    private int price;
+
+                    public OptionDTO(Option option) {
+                        this.id = option.getId();
+                        this.optionName = option.getOptionName();
+                        this.price = option.getPrice();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ week6/kakao-6th-deploy/src/main/java/com/example/kakao/order/OrderService.java
@@ -0,0 +1,64 @@
+package com.example.kakao.order;
+
+import com.example.kakao._core.errors.exception.Exception400;
+import com.example.kakao.cart.Cart;
+import com.example.kakao.cart.CartJPARepository;
+import com.example.kakao.order.item.Item;
+import com.example.kakao.order.item.ItemJPARepository;
+import com.example.kakao.product.option.Option;
+import com.example.kakao.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class OrderService {
+
+    private final CartJPARepository cartJPARepository;
+    private final OrderJPARepository orderJPARepository;
+    private final ItemJPARepository itemJPARepository;
+
+    @Transactional
+    public OrderResponse.SaveDTO saveOrder(User sesssionUser) {
+        List<Cart> cartList = cartJPARepository.findAllByUserId(sesssionUser.getId());
+        // 장바구니 비어 있을 때 예외 처리
+        if (cartList.isEmpty()) {
+            throw new Exception400("장바구니가 비어있습니다");
+        }
+        else {
+            // 비어있지 않다면
+            Order order = Order.builder().user(sesssionUser).build();
+            Order orderPS = orderJPARepository.save(order);
+            List<Item> itemList = new ArrayList();
+            for (Cart cart : cartList) {
+                Option optionPS = cart.getOption();
+                int quantity = cart.getQuantity();
+                int price = cart.getPrice();
+                Item item = Item.builder().option(optionPS).order(orderPS).
+                        quantity(quantity).price(price).build();
+                itemList.add(item);
+            }
+            List<Item> itemListPS = itemJPARepository.saveAll(itemList);
+            cartJPARepository.deleteAll();
+            return new OrderResponse.SaveDTO(orderPS, itemListPS);
+        }
+    }
+
+    @Transactional
+    public OrderResponse.findByIdDTO findById(int orderId, User sesssionUser) {
+        Optional<Order> orderPS = orderJPARepository.findById(orderId);
+        if (orderPS.isPresent()) {
+            List<Item> itemListPS = itemJPARepository.mfindByOrderId(orderId);
+            return new OrderResponse.findByIdDTO(orderId, itemListPS);
+        }
+        else {
+            throw new Exception400("존재하지 않는 주문 번호");
+        }
+    }
+}
