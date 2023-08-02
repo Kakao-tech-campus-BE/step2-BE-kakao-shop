bcee192aa80fc05b3fd6b858b898af7fa29530e0
chaee813 <chaee813@gmail.com>
2023-08-02 23:38:50 +0900

    Cart 실패 테스트 추가

--- week6/kakao-6th-deploy/src/test/java/com/example/kakao/cart/CartRestControllerTest.java
+++ week6/kakao-6th-deploy/src/test/java/com/example/kakao/cart/CartRestControllerTest.java
@@ -59,6 +59,35 @@ public class CartRestControllerTest extends MyRestDoc {
 
     @WithUserDetails(value = "ssarmango@nate.com")
     @Test
+    public void addCartList_fail_test() throws Exception {
+        // 존재하지 않는 옵션을 추가할 때의 실패 테스트
+        // given
+        List<CartRequest.SaveDTO> requestDTOs = new ArrayList<>();
+        CartRequest.SaveDTO item = new CartRequest.SaveDTO();
+        item.setOptionId(100);
+        item.setQuantity(2);
+        requestDTOs.add(item);
+
+        String requestBody = om.writeValueAsString(requestDTOs);
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                post("/carts/add")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        );
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // verify
+        resultActions.andExpect(jsonPath("$.success").value("false"));
+        resultActions.andExpect(jsonPath("$.error.message").value("해당 옵션을 찾을 수 없습니다 : 100"));
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
     public void findAll_test() throws Exception {
         // given teardown
 
@@ -115,6 +144,101 @@ public class CartRestControllerTest extends MyRestDoc {
         resultActions.andExpect(jsonPath("$.response.carts[0].optionName").value("01. 슬라이딩 지퍼백 크리스마스에디션 4종"));
         resultActions.andExpect(jsonPath("$.response.carts[0].quantity").value(10));
         resultActions.andExpect(jsonPath("$.response.carts[0].price").value(100000));
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+    @WithUserDetails(value = "ssarmango2@nate.com")
+    @Test
+    public void update_fail1_test() throws Exception {
+        // 장바구니가 비어있을 때의 실패 테스트
+        // teardown으로 매번 장바구니에.. 데이터를 넣어주는데 비어있을 때의 테스트를 어떻게 하지?
+        // given
+        List<CartRequest.UpdateDTO> requestDTOs = new ArrayList<>();
+        CartRequest.UpdateDTO item = new CartRequest.UpdateDTO();
+        item.setCartId(1);
+        item.setQuantity(10);
+        requestDTOs.add(item);
+
+        String requestBody = om.writeValueAsString(requestDTOs);
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                post("/carts/update")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        );
+
+        // eye
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // verify
+        resultActions.andExpect(jsonPath("$.success").value("false"));
+        resultActions.andExpect(jsonPath("$.error.message").value("장바구니가 비어있습니다."));
+    }
+
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
+    public void update_fail2_test() throws Exception {
+        // 같은 아이템의 수량을 여러번 업데이트할 때의 실패 테스트
+        // given
+        List<CartRequest.UpdateDTO> requestDTOs = new ArrayList<>();
+        CartRequest.UpdateDTO item1 = new CartRequest.UpdateDTO();
+        item1.setCartId(1);
+        item1.setQuantity(10);
+        requestDTOs.add(item1);
+
+        CartRequest.UpdateDTO item2 = new CartRequest.UpdateDTO();
+        item2.setCartId(1);
+        item2.setQuantity(15);
+        requestDTOs.add(item2);
+
+
+        String requestBody = om.writeValueAsString(requestDTOs);
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                post("/carts/update")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        );
+
+        // eye
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // verify
+        resultActions.andExpect(jsonPath("$.success").value("false"));
+        resultActions.andExpect(jsonPath("$.error.message").value("상품 수량은 요청당 한 번만 변경할 수 있습니다."));
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
+    public void update_fail3_test() throws Exception {
+        // 장바구니에 존재하지 않는 아이템을 업데이트할 때의 실패 테스트
+        // given
+        List<CartRequest.UpdateDTO> requestDTOs = new ArrayList<>();
+        CartRequest.UpdateDTO item = new CartRequest.UpdateDTO();
+        item.setCartId(10);
+        item.setQuantity(10);
+        requestDTOs.add(item);
+
+        String requestBody = om.writeValueAsString(requestDTOs);
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                post("/carts/update")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        );
+
+        // eye
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // verify
+        resultActions.andExpect(jsonPath("$.success").value("false"));
+        resultActions.andExpect(jsonPath("$.error.message").value("장바구니에 존재하지 않는 아이템입니다."));
         resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
     }
 
