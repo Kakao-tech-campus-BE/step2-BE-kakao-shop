089b3ad4066399708c58a80e1b146ac5ffe2e8a9
sososo0 <sh0000@pusan.ac.kr>
2023-08-02 12:32:23 +0900

    refactor: Modify exception handling in Cart

--- 5week/kakao-5th-assignment/src/main/java/com/example/kakao/cart/CartRequest.java
+++ 5week/kakao-5th-assignment/src/main/java/com/example/kakao/cart/CartRequest.java
@@ -5,15 +5,20 @@ import lombok.NonNull;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.Size;
+
 public class CartRequest {
 
     @Getter @Setter @ToString
     public static class SaveDTO {
         @NonNull
         private int optionId;
+
+        @Size(min=1, max = 1000, message = "잘못된 수량 요청입니다.")
         @NonNull
         private int quantity;
 
+        @Size(min=1, max=10000000, message = "잘못된 가격 요청입니다.")
         // price 추가했습니다.
         private int price;
     }
