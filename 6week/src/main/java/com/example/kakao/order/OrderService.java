06bd3bacc28a8bdd775b7b39f343f63a161b82b0
hoayii <lhh@hoyaiui-MacBookAir.local>
2023-08-03 01:47:36 +0900

    week6_ver1.4

--- 6week/src/main/java/com/example/kakao/order/OrderService.java
+++ 6week/src/main/java/com/example/kakao/order/OrderService.java
@@ -61,5 +61,6 @@ public class OrderService {
     public void clearOrder() {
         itemJPARepository.deleteAll();
         orderJPARepository.deleteAll();
+        cartJPARepository.deleteAll();
     }
 }
