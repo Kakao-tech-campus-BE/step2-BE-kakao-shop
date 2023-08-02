f2d0665df48399c1636a12d0a5be045d4b851345
sososo0 <sh0000@pusan.ac.kr>
2023-08-03 00:28:54 +0900

    test: Modify CartRestControllerTest

--- 6week/kakao-6th-assignment/src/test/java/com/example/kakao/_core/cart/CartRestControllerTest.java
+++ 6week/kakao-6th-assignment/src/test/java/com/example/kakao/_core/cart/CartRestControllerTest.java
@@ -1,7 +1,7 @@
 package com.example.kakao._core.cart;
 
 import com.example.kakao._core.MyRestDoc;
-import com.example.kakao.cart.CartRequest;
+import com.example.kakao.cart.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +22,7 @@ import java.util.List;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureRestDocs(uriScheme = "http", uriHost = "localhost", uriPort = 8080)
 @Sql(value = "classpath:db/teardown.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
@@ -35,6 +36,9 @@ public class CartRestControllerTest extends MyRestDoc {
 
     @Autowired
     private ObjectMapper om;
+
+    @Autowired
+    private CartJPARepository cartJPARepository;
 
     @WithUserDetails(value = "ssarmango@nate.com") // ssarmango@nate.com으로 DB에 있는지 조회 -> 있으면 통과
     @Test
@@ -55,20 +59,64 @@ public class CartRestControllerTest extends MyRestDoc {
         // when
         //mvc.perform(post("/carts/add").content("optionId=3&quantity=5").contentType(MediaType.APPLICATION_FORM_URLENCODED));
         //json data를 보내는 것을 알려줌
-        ResultActions resultActions = mvc.perform(post("/carts/add").content(requestBody).contentType(MediaType.APPLICATION_JSON));
+        ResultActions resultActions = mvc.perform(post("/carts/add")
+                .content(requestBody).contentType(MediaType.APPLICATION_JSON)
+        );
 
         // eye
         //String responseBody = resultActions.andReturn().getResponse().getContentAsString();
         //System.out.println("테스트 : "+responseBody);
 
         //then
-        resultActions.andExpect(jsonPath("$.success").value("true"));
+        resultActions.andExpect(status().isOk()).andExpect(jsonPath("$.success").value("true"));
         resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
     }
 
     @WithUserDetails(value = "ssarmango@nate.com")
     @Test
+    public void addCartList_duplicateOptionId_test() throws Exception {
+
+        // given
+        List<CartRequest.SaveDTO> requestDTOs = new ArrayList<>();
+
+        CartRequest.SaveDTO item1 = new CartRequest.SaveDTO();
+        item1.setOptionId(1);
+        item1.setQuantity(5);
+        item1.setPrice(10000);
+        requestDTOs.add(item1);
+
+        CartRequest.SaveDTO item2 = new CartRequest.SaveDTO();
+        item2.setOptionId(1);
+        item2.setQuantity(10);
+        item2.setPrice(20000);
+        requestDTOs.add(item2);
+
+        String requestBody = om.writeValueAsString(requestDTOs);
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/carts/add")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // eye
+        //String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        //System.out.println("테스트 : " + responseBody);
+
+        // then
+
+        resultActions.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.response").isEmpty())
+                .andExpect(jsonPath("$.error.message").value("중복된 옵션 요청입니다. : "+ 1));
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+
+    }
+
+
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
     public void findAll_test() throws Exception {
+
         // given teardown
 
         // when
@@ -77,28 +125,31 @@ public class CartRestControllerTest extends MyRestDoc {
         );
 
         // eye
-        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        //String responseBody = resultActions.andReturn().getResponse().getContentAsString();
         //System.out.println("테스트 : " + responseBody);
 
         // verify
-        resultActions.andExpect(jsonPath("$.success").value("true"));
-        resultActions.andExpect(jsonPath("$.response.products[0].id").value(1));
-        resultActions.andExpect(jsonPath("$.response.products[0].productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"));
-        resultActions.andExpect(jsonPath("$.response.products[0].carts[0].id").value(1));
-        resultActions.andExpect(jsonPath("$.response.products[0].carts[0].option.id").value(1));
-        resultActions.andExpect(jsonPath("$.response.products[0].carts[0].option.optionName").value("01. 슬라이딩 지퍼백 크리스마스에디션 4종"));
-        resultActions.andExpect(jsonPath("$.response.products[0].carts[0].option.price").value(10000));
-        resultActions.andExpect(jsonPath("$.response.products[0].carts[0].quantity").value(5));
-        resultActions.andExpect(jsonPath("$.response.products[0].carts[0].price").value(50000));
-        resultActions.andExpect(jsonPath("$.response.totalPrice").value(310900));
+        resultActions.andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value("true"))
+                .andExpect(jsonPath("$.response.products[0].id").value(1))
+                .andExpect(jsonPath("$.response.products[0].productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"))
+                .andExpect(jsonPath("$.response.products[0].carts[0].id").value(1))
+                .andExpect(jsonPath("$.response.products[0].carts[0].option.id").value(1))
+                .andExpect(jsonPath("$.response.products[0].carts[0].option.optionName").value("01. 슬라이딩 지퍼백 크리스마스에디션 4종"))
+                .andExpect(jsonPath("$.response.products[0].carts[0].option.price").value(10000))
+                .andExpect(jsonPath("$.response.products[0].carts[0].quantity").value(5))
+                .andExpect(jsonPath("$.response.products[0].carts[0].price").value(50000))
+                .andExpect(jsonPath("$.response.totalPrice").value(310900));
         resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
     }
+
 
     @WithUserDetails(value = "ssarmango@nate.com")
     @Test
     public void update_test() throws Exception {
         // given -> cartId [1번 5개,2번 1개,3번 5개]가 teardown.sql을 통해 들어가 있음
         List<CartRequest.UpdateDTO> requestDTOs = new ArrayList<>();
+
         CartRequest.UpdateDTO item = new CartRequest.UpdateDTO();
         item.setCartId(1);
         item.setQuantity(10);
@@ -116,7 +167,7 @@ public class CartRestControllerTest extends MyRestDoc {
         );
 
         // eye
-        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        //String responseBody = resultActions.andReturn().getResponse().getContentAsString();
         //System.out.println("테스트 : " + responseBody);
 
         // verify
@@ -128,5 +179,76 @@ public class CartRestControllerTest extends MyRestDoc {
         resultActions.andExpect(jsonPath("$.response.carts[0].price").value(100000));
         resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
     }
+
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
+    public void update_duplicateCartId_test() throws Exception {
+        // given duplicate cartId in the request
+        List<CartRequest.UpdateDTO> requestDTOs = new ArrayList<>();
+
+        CartRequest.UpdateDTO item1 = new CartRequest.UpdateDTO();
+        item1.setCartId(1);
+        item1.setQuantity(10);
+        item1.setPrice(100000);
+        requestDTOs.add(item1);
+
+        CartRequest.UpdateDTO item2 = new CartRequest.UpdateDTO();
+        item2.setCartId(1);
+        item2.setQuantity(5);
+        item2.setPrice(50000);
+        requestDTOs.add(item2);
+
+        String requestBody = om.writeValueAsString(requestDTOs);
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/carts/update")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON_VALUE));
+
+        // eye
+        //String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        //System.out.println("테스트 : " + responseBody);
+
+        resultActions.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.response").isEmpty())
+                .andExpect(jsonPath("$.error.message").value("중복된 장바구니 아이디 요청입니다. : "+ 1));
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+
+    }
+
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
+    public void update_cartNotFound_test() throws Exception {
+
+        // given cartId not found in the user's cart list
+        List<CartRequest.UpdateDTO> requestDTOs = new ArrayList<>();
+
+        CartRequest.UpdateDTO item1 = new CartRequest.UpdateDTO();
+        item1.setCartId(10);
+        item1.setQuantity(10);
+        item1.setPrice(100000);
+        requestDTOs.add(item1);
+
+        String requestBody = om.writeValueAsString(requestDTOs);
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/carts/update")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON_VALUE));
+
+        // eye
+        //String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        //System.out.println("테스트 : " + responseBody);
+
+        // then
+        resultActions.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.response").isEmpty())
+                .andExpect(jsonPath("$.error.message").value("없는 장바구니 아이디 요청입니다. : "+ 10));
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+
 
 }
