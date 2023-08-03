bf7f9a1d8891c505f4a350d12ed26d340281b1f2
MinseokGo <rhalstjr1999@naver.com>
2023-08-03 23:52:05 +0900

    test: product integration test

---
+++ week6/kakao-6th-deploy/src/test/java/com/example/kakao/product/ProductIntegrationTest.java
@@ -0,0 +1,137 @@
+package com.example.kakao.product;
+
+import com.example.kakao.MyRestDoc;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureRestDocs(uriScheme = "http", uriHost = "localhost", uriPort = 8080)
+@ActiveProfiles("test")
+@Sql("classpath:db/teardown.sql")
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class ProductIntegrationTest extends MyRestDoc {
+
+    @Test
+    @DisplayName("전체 상품 조회 테스트")
+    void findAll_test() throws Exception {
+        // given teardown.sql
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                get("/products")
+        );
+
+        // console
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // verify
+        resultActions.andExpect(jsonPath("$.success").value("true"));
+        resultActions.andExpect(jsonPath("$.response[0].id").value(1));
+        resultActions.andExpect(jsonPath("$.response[0].productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"));
+        resultActions.andExpect(jsonPath("$.response[0].description").value(""));
+        resultActions.andExpect(jsonPath("$.response[0].image").value("/images/1.jpg"));
+        resultActions.andExpect(jsonPath("$.response[0].price").value(1000));
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+    @Test
+    @DisplayName("개별 상품 조회 테스트")
+    void findById_test() throws Exception {
+        // given teardown.sql
+        int id = 1;
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                        get("/products/" + id)
+        );
+
+        // console
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : "+responseBody);
+
+        // verify
+        resultActions.andExpect(jsonPath("$.success").value("true"));
+        resultActions.andExpect(jsonPath("$.response.id").value(1));
+        resultActions.andExpect(jsonPath("$.response.productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"));
+        resultActions.andExpect(jsonPath("$.response.description").value(""));
+        resultActions.andExpect(jsonPath("$.response.image").value("/images/1.jpg"));
+        resultActions.andExpect(jsonPath("$.response.price").value(1000));
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 페이지 테스트")
+    void notExist_findAll_test() throws Exception {
+        //given
+        int id = -1;
+
+        //when
+        ResultActions resultActions = mvc.perform(
+                        get("/products")
+                        .param("page", String.valueOf(id))
+        );
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        //then
+        // 4xx 에러 검증 - 404
+        resultActions.andExpect(status().is4xxClientError())
+                .andDo(print())
+                .andDo(MockMvcResultHandlers.print())
+                .andDo(document);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 상품 조회 테스트")
+    void notExist_findById_test() throws Exception {
+        //given
+        int id = 200;
+
+        //when
+        ResultActions result = mvc.perform(
+                        get("/products/{id}", id)
+        );
+
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        //then
+        // 4xx 에러 검증 - 404
+        result.andExpect(status().is4xxClientError())
+                .andDo(print())
+                .andDo(MockMvcResultHandlers.print())
+                .andDo(document);
+    }
+
+//    @Test
+//    @DisplayName("잘못된 HTTP 메서드로 서버에 요청 테스트")
+//    void notAllowed_httpMethod_test() throws Exception {
+//        //given
+//        int id = 1;
+//
+//        //when
+//        ResultActions result = mvc.perform(
+//                        post("/products")
+//                        .param("page", String.valueOf(id))
+//        );
+//
+//        //then
+//        // 4xx 에러 검증 - 405
+//        result.andExpect(status().is4xxClientError()).andDo(print());
+//    }
+}
