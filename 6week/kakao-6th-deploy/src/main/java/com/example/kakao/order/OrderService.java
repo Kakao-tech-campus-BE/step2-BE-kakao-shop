5eeab84ff4b03e9bba188a31574ed88729deb8ae
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2023-08-03 16:22:49 +0900

    Refactor OrderService
    메서드 분리, private, public 분리하여 배치

--- 6week/kakao-6th-deploy/src/main/java/com/example/kakao/order/OrderService.java
+++ 6week/kakao-6th-deploy/src/main/java/com/example/kakao/order/OrderService.java
@@ -30,11 +30,36 @@ public class OrderService {
         if (cartList.isEmpty()){
             throw new Exception404("장바구니가 비어있습니다.");
         }
+        Order newOrder = createOrderForUser(user);
+        int totalPrice = saveItemsFromCart(cartList, newOrder);
 
+        /*유저 장바구니 전부 삭제
+        강사님이 제공해주신 메서드이지만 이런 경우에는 id 마다 따로 삭제를 진행해주어야 해서 deleteAll을 사용함
+        cartJPARepository.deleteByUserId(user.getId());*/
+        cartJPARepository.deleteAll(cartList);
+
+        return new OrderResponse.FindAllDTO(newOrder.getId(), cartList, totalPrice);
+    }
+
+    public OrderResponse.FindByIdDTO findById (int orderId, User user) {
+        // 권한 체크
+        validateUserAccessToOrder(orderId, user);
+        // 주문 내 상품 목록 가져오기
+        List<Item> itemList = itemJPARepository.findWithOptionAndProductByOrderId(orderId);
+        // 총 주문 금액 계산
+        // Order을 만들 당시에 TotalPrice를 저장하는 변수를 만드는것이 좋을것 같다.
+        int totalPrice = calculateTotalPrice(itemList);
+
+        return new OrderResponse.FindByIdDTO(orderId, itemList, totalPrice);
+    }
+    private Order createOrderForUser(User user) {
         Order newOrder = Order.builder()
                 .user(user).build();
-        orderJPARepository.save(newOrder);
 
+        return orderJPARepository.save(newOrder);
+    }
+
+    private int saveItemsFromCart(List<Cart> cartList, Order newOrder) {
         int totalPrice = 0;
 
         List<Item> items = new ArrayList<>();
@@ -48,38 +73,22 @@ public class OrderService {
             items.add(item);
             totalPrice += cart.getPrice();
         }
-        // 쿼리 단 하나
         itemJPARepository.saveAll(items);
-        /*유저 장바구니 전부 삭제
-        강사님이 제공해주신 메서드이지만 이런 경우에는 id 마다 따로 삭제를 진행해주어야 해서 deleteAll을 사용함
-        cartJPARepository.deleteByUserId(user.getId());*/
 
-        // 쿼리 단 하나
-        cartJPARepository.deleteAll(cartList);
-        System.out.println(totalPrice);
-        return new OrderResponse.FindAllDTO(newOrder.getId(), cartList, totalPrice);
+        return totalPrice;
     }
 
-    public OrderResponse.FindByIdDTO findById (int orderId, User user) {
-        // 권한 체크
+    private void validateUserAccessToOrder(int orderId, User user) {
         boolean hasAccess = orderJPARepository.existsByIdAndUserId(orderId, user.getId());
         if (!hasAccess) {
             throw new Exception401("해당 주문이 유효하지 않거나 접근권한이 없습니다.");
         }
+    }
 
-        // 주문 내 상품 목록 가져오기
-        List<Item> itemList = itemJPARepository.findWithOptionAndProductByOrderId(orderId);
-
-        // 총 주문 금액 계산
-        // Order을 만들 당시에 TotalPrice를 저장하는 변수를 만드는것이 좋을것 같다.
-        int totalPrice = itemList.stream()
+    private int calculateTotalPrice(List<Item> itemList) {
+        return itemList.stream()
                 .mapToInt(item -> item.getPrice())
                 .sum();
-
-        // 응답 DTO 생성
-        OrderResponse.FindByIdDTO responseDTO = new OrderResponse.FindByIdDTO(orderId, itemList, totalPrice);
-
-        return responseDTO;
     }
 
 
