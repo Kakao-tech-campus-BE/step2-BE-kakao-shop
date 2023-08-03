410424a12c7734f70c22b4cdaa6789f71505737d
Cheoroo <195822@jnu.ac.kr>
2023-08-03 12:41:03 +0900

    update 6주차 과제 세팅

--- 6week/kakao-6th-deploy-main/src/main/java/com/example/kakao/order/OrderRestController.java
+++ 6week/kakao-6th-deploy-main/src/main/java/com/example/kakao/order/OrderRestController.java
@@ -1,30 +1,31 @@
 package com.example.kakao.order;
 
+import com.example.kakao._core.security.CustomUserDetails;
 import com.example.kakao._core.utils.ApiUtils;
-import com.example.kakao.order.item.Item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class OrderRestController {
 
+    private final OrderService orderService;
 
     // (기능9) 결재하기 - (주문 인서트) POST
     // /orders/save
-    public void save() {
-
+    @PostMapping("/orders/save")
+    public ResponseEntity<ApiUtils.ApiResult<?>> save(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        OrderResponse.FindByIdDTO responseDTO = orderService.save(userDetails.getUser());
+        return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
 
     // (기능10) 주문 결과 확인 GET
     // /orders/{id}
-    public void findById() {
-
+    @GetMapping("/orders/{id}")
+    public ResponseEntity<ApiUtils.ApiResult<?>> findById(@PathVariable int id, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        OrderResponse.FindByIdDTO responseDTO = orderService.findById(id, userDetails.getUser());
+        return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
-
 }
