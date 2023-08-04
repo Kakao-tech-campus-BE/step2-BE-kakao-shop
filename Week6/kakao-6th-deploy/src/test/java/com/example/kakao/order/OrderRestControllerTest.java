68111a8087469d9accecca05c1261ecdfa3e8d1a
cosmos_1885 <dmsrkql104321@gmail.com>
2023-08-04 12:27:26 +0900

    feat: create OrderRestControllerTest

---
+++ Week6/kakao-6th-deploy/src/test/java/com/example/kakao/order/OrderRestControllerTest.java
@@ -0,0 +1,96 @@
+package com.example.kakao.order;
+
+import com.example.kakao.MyRestDoc;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@AutoConfigureRestDocs(uriScheme = "http", uriHost = "localhost", uriPort = 8080)
+@ActiveProfiles("test")
+@Sql(value = "classpath:db/teardown.sql")
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class OrderRestControllerTest extends MyRestDoc {
+    @Autowired
+    private ObjectMapper om;
+
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
+    public void save_test() throws Exception {
+        // given
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                post("/orders/save")
+        );
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트: " + responseBody);
+
+        // then
+        resultActions.andExpect(jsonPath("$.success").value("true"));
+        resultActions.andExpect(jsonPath("$.response.id").value(2));
+        resultActions.andExpect(jsonPath("$.response.products[0].productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"));
+        resultActions.andExpect(jsonPath("$.response.products[0].items[0].id").value(4));
+        resultActions.andExpect(jsonPath("$.response.products[0].items[0].optionName").value("01. 슬라이딩 지퍼백 크리스마스에디션 4종"));
+        resultActions.andExpect(jsonPath("$.response.products[0].items[0].price").value(50000));
+        resultActions.andExpect(jsonPath("$.response.totalPrice").value(310900));
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
+    public void findById_test() throws Exception {
+        // given
+        int id = 1;
+
+        // when
+        ResultActions result = mvc.perform(
+                get("/orders/" + id)
+        );
+
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트: " + responseBody);
+
+        // then
+        result.andExpect(jsonPath("$.success").value("true"));
+        result.andExpect(jsonPath("$.response.id").value(1));
+        result.andExpect(jsonPath("$.response.products[0].productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"));
+        result.andExpect(jsonPath("$.response.products[0].items[0].id").value(1));
+        result.andExpect(jsonPath("$.response.products[0].items[0].optionName").value("01. 슬라이딩 지퍼백 크리스마스에디션 4종"));
+        result.andExpect(jsonPath("$.response.products[0].items[0].price").value(50000));
+        result.andExpect(jsonPath("$.response.totalPrice").value(50000));
+    }
+
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
+    public void findById_fail_test() throws Exception {
+        // given
+        int id = 2;
+
+        // when
+        ResultActions result = mvc.perform(
+                get("/orders/" + id)
+        );
+
+        String responseBody = result.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트: " + responseBody);
+
+        // then
+        result.andExpect(jsonPath("$.success").value("false"));
+        result.andExpect(jsonPath("$.error.message").value("주문 내역이 존재하지 않습니다 : " + id));
+        result.andExpect(jsonPath("$.error.status").value(400));
+    }
+}
